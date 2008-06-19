5b1ad36e6dde57a8dbd4b447657836dfb6a28b83
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2008-06-19 11:25:39 +0000

    Removed typedefs etc.

--- redblack.c
+++ redblack.c
@@ -1,40 +1,20 @@
-typedef int ElementType;
-#define NegInfinity (-10000)
-
-#ifndef _RedBlack_H
-#define _RedBlack_H
-
-struct RedBlackNode;
-typedef struct RedBlackNode *Position;
-typedef struct RedBlackNode *RedBlackTree;
-
-RedBlackTree MakeEmpty( RedBlackTree T );
-Position Find( ElementType X, RedBlackTree T );
-Position FindMin( RedBlackTree T );
-Position FindMax( RedBlackTree T );
-RedBlackTree Initialize( void );
-RedBlackTree Insert( ElementType X, RedBlackTree T );
-RedBlackTree Remove( ElementType X, RedBlackTree T );
-ElementType Retrieve( Position P );
-void PrintTree( RedBlackTree T );
-
-typedef enum ColorType { Red, Black } ColorType;
+// typedef enum ColorType { Red = 0, Black = 1 } ColorType;
 
 struct RedBlackNode
 {
-    ElementType  Element;
-    RedBlackTree Left;
-    RedBlackTree Right;
-    ColorType    Color;
+    int  Element;
+    struct RedBlackNode * Left;
+    struct RedBlackNode * Right;
+    int    Color;
 };
 
-static Position NullNode = NULL;  /* Needs initialization */
+static struct RedBlackNode * NullNode = NULL;  /* Needs initialization */
 
 /* Initialization procedure */
-RedBlackTree
+struct RedBlackNode *
 Initialize( void )
 {
-    RedBlackTree T;
+    struct RedBlackNode * T;
 
     if( NullNode == NULL )
     {
@@ -42,7 +22,7 @@ Initialize( void )
         if( NullNode == NULL )
             FatalError( "Out of space!!!" );
         NullNode->Left = NullNode->Right = NullNode;
-        NullNode->Color = Black;
+        NullNode->Color = (1 /* Black */);
         NullNode->Element = 12345;
     }
 
@@ -50,15 +30,15 @@ Initialize( void )
     T = malloc( sizeof( struct RedBlackNode ) );
     if( T == NULL )
         FatalError( "Out of space!!!" );
-    T->Element = NegInfinity;
+    T->Element = (-10000);
     T->Left = T->Right = NullNode;
-    T->Color = Black;
+    T->Color = (1 /* Black */);
 
     return T;
 }
 
 void
-Output( ElementType Element )
+Output( int Element )
 {
     printf( "%d\n", Element );
 }
@@ -67,7 +47,7 @@ Output( ElementType Element )
 /* and skip header */
 
 static void
-DoPrint( RedBlackTree T )
+DoPrint( struct RedBlackNode * T )
 {
     if( T != NullNode )
     {
@@ -78,13 +58,13 @@ DoPrint( RedBlackTree T )
 }
 
 void
-PrintTree( RedBlackTree T )
+PrintTree( struct RedBlackNode * T )
 {
     DoPrint( T->Right );
 }
 
-static RedBlackTree
-MakeEmptyRec( RedBlackTree T )
+static struct RedBlackNode *
+MakeEmptyRec( struct RedBlackNode * T )
 {
     if( T != NullNode )
     {
@@ -95,15 +75,15 @@ MakeEmptyRec( RedBlackTree T )
     return NullNode;
 }
 
-RedBlackTree
-MakeEmpty( RedBlackTree T )
+struct RedBlackNode *
+MakeEmpty( struct RedBlackNode * T )
 {
     T->Right = MakeEmptyRec( T->Right );
     return T;
 }
 
-Position
-Find( ElementType X, RedBlackTree T )
+struct RedBlackNode *
+Find( int X, struct RedBlackNode * T )
 {
     if( T == NullNode )
         return NullNode;
@@ -116,8 +96,8 @@ Find( ElementType X, RedBlackTree T )
         return T;
 }
 
-Position
-FindMin( RedBlackTree T )
+struct RedBlackNode *
+FindMin( struct RedBlackNode * T )
 {
     T = T->Right;
     while( T->Left != NullNode )
@@ -126,8 +106,8 @@ FindMin( RedBlackTree T )
     return T;
 }
 
-Position
-FindMax( RedBlackTree T )
+struct RedBlackNode *
+FindMax( struct RedBlackNode * T )
 {
     while( T->Right != NullNode )
         T = T->Right;
@@ -139,10 +119,10 @@ FindMax( RedBlackTree T )
 /* Perform a rotate between a node (K2) and its left child */
 /* Update heights, then return new root */
 
-static Position
-SingleRotateWithLeft( Position K2 )
+static struct RedBlackNode *
+SingleRotateWithLeft( struct RedBlackNode * K2 )
 {
-    Position K1;
+    struct RedBlackNode * K1;
 
     K1 = K2->Left;
     K2->Left = K1->Right;
@@ -155,10 +135,10 @@ SingleRotateWithLeft( Position K2 )
 /* Perform a rotate between a node (K1) and its right child */
 /* Update heights, then return new root */
 
-static Position
-SingleRotateWithRight( Position K1 )
+static struct RedBlackNode *
+SingleRotateWithRight( struct RedBlackNode * K1 )
 {
-    Position K2;
+    struct RedBlackNode * K2;
 
     K2 = K1->Right;
     K1->Right = K2->Left;
@@ -171,8 +151,8 @@ SingleRotateWithRight( Position K1 )
 /* (whose parent is passed as a parameter) */
 /* The child is deduced by examining Item */
 
-static Position
-Rotate( ElementType Item, Position Parent )
+static struct RedBlackNode *
+Rotate( int Item, struct RedBlackNode * Parent )
 {
 
     if( Item < Parent->Element )
@@ -185,28 +165,28 @@ Rotate( ElementType Item, Position Parent )
             SingleRotateWithRight( Parent->Right );
 }
 
-static Position X, P, GP, GGP;
+static struct RedBlackNode * X, P, GP, GGP;
 
 static
-void HandleReorient( ElementType Item, RedBlackTree T )
+void HandleReorient( int Item, struct RedBlackNode * T )
 {
-    X->Color = Red;        /* Do the color flip */
-    X->Left->Color = Black;
-    X->Right->Color = Black;
+    X->Color = (0 /* Red */);        /* Do the color flip */
+    X->Left->Color = (1 /* Black */);
+    X->Right->Color = (1 /* Black */);
 
-    if( P->Color == Red )  /* Have to rotate */
+    if( P->Color == (0 /* Red */) )  /* Have to rotate */
     {
-        GP->Color = Red;
+        GP->Color = (0 /* Red */);
         if( (Item < GP->Element) != (Item < P->Element) )
             P = Rotate( Item, GP );  /* Start double rotate */
         X = Rotate( Item, GGP );
-        X->Color = Black;
+        X->Color = (1 /* Black */);
     }
-    T->Right->Color = Black;  /* Make root black */
+    T->Right->Color = (1 /* Black */);  /* Make root black */
 }
 
-RedBlackTree
-Insert( ElementType Item, RedBlackTree T )
+struct RedBlackNode *
+Insert( int Item, struct RedBlackNode * T )
 {
     X = P = GP = T;
     NullNode->Element = Item;
@@ -217,7 +197,7 @@ Insert( ElementType Item, RedBlackTree T )
             X = X->Left;
         else
             X = X->Right;
-        if( X->Left->Color == Red && X->Right->Color == Red )
+        if( X->Left->Color == (0 /* Red */) && X->Right->Color == (0 /* Red */) )
             HandleReorient( Item, T );
     }
 
@@ -239,8 +219,8 @@ Insert( ElementType Item, RedBlackTree T )
     return T;
 }
 
-RedBlackTree
-Remove( ElementType Item, RedBlackTree T )
+struct RedBlackNode *
+Remove( int Item, struct RedBlackNode * T )
 {
     printf( "Remove is unimplemented\n" );
     if( Item )
@@ -248,8 +228,8 @@ Remove( ElementType Item, RedBlackTree T )
     return T;
 }
 
-ElementType
-Retrieve( Position P )
+int
+Retrieve( struct RedBlackNode * P )
 {
     return P->Element;
 }
