447e77d0b66e3e3cb8a541e21df2756c60c5a631
Cedric Cuypers <cedric.cuypers@student.kuleuven.be>
2009-04-15 13:47:17 +0000

    * Fixed unsoundness issue of Thread_.java and it's contract

--- src/rt/wrappers/Thread_.java
+++ src/rt/wrappers/Thread_.java
@@ -1,15 +1,23 @@
 public class Thread_{
   Thread t;
+  Exception e;
   public Thread_(Runnable run){
     t=new Thread(run);
   }
   void start(){
-    t.start();
+    try{
+      t.run();
+    }catch(RuntimeException e1){
+      e=e1;
+    }
   }
   void join(){
-    try{t.join();}
-	catch(InterruptedException e){
-	throw new RuntimeException("Thread was interrupted.");
-	}
+    try{
+      t.join();
+    }catch(InterruptedException e){
+      throw new RuntimeException("Thread was interrupted.");
+    }
+    if(e!=null)
+      throw new RuntimeException("An exception occurred");
   }
 }
