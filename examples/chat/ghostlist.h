b15bd018042a47c9e5122edeb6d02852b38e5c6f
Jan Smans <jan.smans@cs.kuleuven.be>
2009-07-02 16:18:08 +0000

    - fixed unsound lemma in ghostlist

--- examples/chat/ghostlist.h
+++ examples/chat/ghostlist.h
@@ -23,10 +23,6 @@ lemma void ghost_list_remove(int id, void *d);
 lemma void ghost_list_member_handle_lemma();
     requires [?f1]ghost_list(?id, ?ds) &*& [?f2]ghost_list_member_handle(id, ?d);
     ensures [f1]ghost_list(id, ds) &*& [f2]ghost_list_member_handle(id, d) &*& contains(ds, d) == true;
-
-lemma void ghost_list_create_member_handle(int id, void *x);
-    requires [?f1]ghost_list(id, ?xs) &*& contains(xs, x) == true;
-    ensures [f1]ghost_list(id, xs) &*& [_]ghost_list_member_handle(id, x);
 @*/
 
 #endif
