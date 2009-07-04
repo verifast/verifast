62d63b7c14fdc4d02a9b1fb12401dbd4034b0973
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2009-07-04 08:05:23 +0000

    owicki-gries.c: some minor simplifications

--- examples/tutorial/owicki-gries.c
+++ examples/tutorial/owicki-gries.c
@@ -8,25 +8,9 @@ struct counter {
 };
 
 /*@
-predicate counter(struct counter* c, int v)
-  requires c->x |-> v &*& malloc_block_counter(c) &*& [1/2]c->contrib1 |-> ?c1 &*& [1/2]c->contrib2 |-> ?c2 &*& v == c1 + c2;
-  
-predicate_ctor counter_ctor(struct counter* c)()
-  requires counter(c, _); 
-  
-predicate session(struct session* s)
-  requires [1/2]s->isFirst |-> ?b &*& [1/2]s->lock |-> ?l &*& [1/2]s->counter |-> ?c &*& malloc_block_session(s) &*&
-           [1/4]lock_permission(l, counter_ctor(c)) &*& b == true ? [1/2] c->contrib1 |-> 0 : [1/2] c->contrib2 |-> 0;
 
-predicate session_done(struct session* s)
-  requires [1/2]s->isFirst |-> ?b &*& [1/2]s->lock |-> ?l &*& [1/2]s->counter |-> ?c &*& malloc_block_session(s) &*&
-           [1/4]lock_permission(l, counter_ctor(c)) &*& b == true ? [1/2] c->contrib1 |-> 1 : [1/2] c->contrib2 |-> 1;
-
-predicate_family_instance thread_run_data(inc)(void* data)
-  requires session(data);
-  
-predicate_family_instance thread_run_post(inc)(void* data)
-  requires session_done(data);
+predicate_ctor counter(struct counter* c)()
+  requires c->x |-> ?v &*& [1/2]c->contrib1 |-> ?c1 &*& [1/2]c->contrib2 |-> ?c2 &*& v == c1 + c2;
 
 @*/
 
@@ -36,34 +20,49 @@ struct session {
   struct counter* counter;
 };
 
-struct session* create_session(struct lock* l, struct counter* c, bool b)
-  //@ requires [1/4]lock_permission(l, counter_ctor(c)) &*& b == true ? [1/2] c->contrib1 |-> 0 : [1/2]c->contrib2 |-> 0;
-  //@ ensures session(result) &*& [1/2]result->isFirst |->b &*& [1/2]result->counter |->c &*& [1/2]result->lock |->l;
+/*@
+
+predicate session(struct session *session; bool isFirst, struct lock *lock, struct counter *counter) =
+  session->isFirst |-> isFirst &*& session->lock |-> lock &*& session->counter |-> counter &*& malloc_block_session(session);
+
+@*/
+
+
+struct session* create_session(struct lock* lock, struct counter* counter, bool isFirst)
+  //@ requires true;
+  //@ ensures session(result, isFirst, lock, counter);
 {
   struct session* s = malloc(sizeof(struct session));
-  if(s == 0) { abort(); }
-  s->isFirst = b;
-  s->lock = l;
-  s->counter = c;
-  //@ split_fraction session_isFirst(s, b);
-  //@ split_fraction session_counter(s, c);
-  //@ split_fraction session_lock(s, l);
-  //@ close session(s);  
+  if (s == 0) { abort(); }
+  s->isFirst = isFirst;
+  s->lock = lock;
+  s->counter = counter;
+  //@ close session(s, isFirst, lock, counter);
   return s;
 }
 
+/*@
+
+predicate_family_instance thread_run_pre(inc)(struct session* s)
+  requires [1/2]session(s, ?isFirst, ?lock, ?counter) &*&
+           [1/4]lock_permission(lock, counter(counter)) &*& isFirst ? [1/2]counter->contrib1 |-> 0 : [1/2]counter->contrib2 |-> 0;
+  
+predicate_family_instance thread_run_post(inc)(struct session* s)
+  requires [1/2]session(s, ?isFirst, ?lock, ?counter) &*&
+           [1/4]lock_permission(lock, counter(counter)) &*& isFirst ? [1/2]counter->contrib1 |-> 1 : [1/2]counter->contrib2 |-> 1;
+
+@*/
+
 void inc(void* data) //@ : thread_run
-  //@ requires thread_run_data(inc)(data);
+  //@ requires thread_run_pre(inc)(data);
   //@ ensures thread_run_post(inc)(data);
 {
-  //@ open thread_run_data(inc)(data);
   struct session* session = data;
-  //@ open session(data);
+  //@ open thread_run_pre(inc)(session);
+  //@ open [1/2]session(session, ?isFirst, ?lock, ?counter);
   lock_acquire(session->lock);
-  //@ open_lock_invariant();
   struct counter* c = session->counter;
-  //@ open counter_ctor(c)();
-  //@ open counter(c, ?v);
+  //@ open counter(c)();
   c->x = c->x + 1;
   if(session->isFirst) {
     //@ merge_fractions counter_contrib1(c, _);
@@ -74,11 +73,9 @@ void inc(void* data) //@ : thread_run
     c->contrib2 = c->contrib2 + 1;
     //@ split_fraction counter_contrib2(c, _);
   }
-  //@ close counter(c, v + 1);
-  //@ close counter_ctor(c)();
-  //@ close_lock_invariant(counter_ctor(c));
-  lock_release(session->lock);  
-  //@ close session_done(data);
+  //@ close counter(c)();
+  lock_release(session->lock);
+  //@ close [1/2]session(session, isFirst, lock, counter);
   //@ close thread_run_post(inc)(data);
 }
 
@@ -93,50 +90,43 @@ int main()
   c->contrib2 = 0;
   //@ split_fraction counter_contrib1(c, _);
   //@ split_fraction counter_contrib2(c, _);
-  //@ close counter(c, 0);
-  
-  //@ close counter_ctor(c)();
-  //@ close_lock_invariant(counter_ctor(c));
-  
+  //@ close counter(c)();
+
+  //@ close create_lock_ghost_argument(counter(c));
   struct lock* l = create_lock();
   
-  //@ split_lock_permission(l);
-  //@ split_lock_permission(l);
+  //@ split_fraction lock_permission(l, _);
+  //@ split_fraction lock_permission(l, _);
   
   struct session* s1 = create_session(l, c, true);
-  struct session* s2 = create_session(l, c, false);
-  
-  //@ close thread_run_data(inc)(s1);
+  //@ split_fraction session(s1, _, _, _);
+  //@ close thread_run_pre(inc)(s1);
   struct thread* t1 = thread_start(inc, s1);
   
-  //@ close thread_run_data(inc)(s2);
+  struct session* s2 = create_session(l, c, false);
+  //@ split_fraction session(s2, _, _, _);
+  //@ close thread_run_pre(inc)(s2);
   struct thread* t2 = thread_start(inc, s2);
   
   thread_join(t1);
   //@ open thread_run_post(inc)(s1);
-  //@ open session_done(s1);
-  //@ merge_fractions session_isFirst(s1, _);
-  //@ merge_fractions session_counter(s1, _);
-  //@ merge_fractions session_lock(s1, _);
+  //@ merge_fractions session(s1, _, _, _);
+  //@ open session(s1, _, _, _);
   
   thread_join(t2);
   //@ open thread_run_post(inc)(s2);
-  //@ open session_done(s2);
-  //@ merge_fractions session_isFirst(s2, _);
-  //@ merge_fractions session_counter(s2, _);
-  //@ merge_fractions session_lock(s2, _);
+  //@ merge_fractions session(s2, _, _, _);
+  //@ open session(s2, _, _, _);
 
-  //@ merge_lock_permission(l);
-  //@ merge_lock_permission(l);
+  //@ merge_fractions lock_permission(l, _);
+  //@ merge_fractions lock_permission(l, _);
   lock_dispose(l);
-  //@ open_lock_invariant();
-  //@ open counter_ctor(c)();
-  //@ open counter(c, _);
+  //@ open counter(c)();
   //@ merge_fractions counter_contrib1(c, _);
   //@ merge_fractions counter_contrib2(c, _);
   
   int tmp = c->x;
-  assert tmp == 2; 
+  assert(tmp == 2);
   
   free(s1);
   free(s2);
--- examples/tutorial/threading.h
+++ examples/tutorial/threading.h
@@ -4,33 +4,16 @@
 struct lock;
 
 /*@
-predicate_family thread_run_data(void *run)(void *data);
+predicate_family thread_run_pre(void *run)(void *data);
 
 predicate_family thread_run_post(void *run)(void *data);
 
-predicate lock_invariant(predicate() p);
+predicate create_lock_ghost_argument(predicate() p) = true;
 
-lemma void open_lock_invariant();
-    requires lock_invariant(?p);
-    ensures p();
-
-lemma void close_lock_invariant(predicate() p);
-    requires p();
-    ensures lock_invariant(p);
-
-predicate lock_permission(struct lock *lock, predicate() p);
+predicate lock_permission(struct lock *lock; predicate() p);
 
 predicate unlock_permission(struct lock *lock, predicate() p, real f);
 
-lemma void split_lock_permission(struct lock *lock);
-    requires [?f]lock_permission(lock, ?p);
-    ensures [f/2]lock_permission(lock, p) &*& [f/2]lock_permission(lock, p);
-    
-lemma void merge_lock_permission(struct lock *lock);
-    requires [?f]lock_permission(lock, ?p) &*& [?g]lock_permission(lock, p);
-    ensures [f + g]lock_permission(lock, p);
-    
-   
 predicate thread(struct thread *thread, void *thread_run, void *data);
 
 @*/
@@ -38,11 +21,11 @@ predicate thread(struct thread *thread, void *thread_run, void *data);
 struct thread;
 
 typedef void thread_run(void *data);
-    //@ requires thread_run_data(this)(data);
+    //@ requires thread_run_pre(this)(data);
     //@ ensures thread_run_post(this)(data);
 
 struct thread* thread_start(void *run, void *data);
-    //@ requires is_thread_run(run) == true &*& thread_run_data(run)(data);
+    //@ requires is_thread_run(run) == true &*& thread_run_pre(run)(data);
     //@ ensures thread(result, run, data);
     
 void thread_join(struct thread *t);
@@ -50,20 +33,19 @@ void thread_join(struct thread *t);
     //@ ensures thread_run_post(run)(data);
 
 struct lock *create_lock();
-    //@ requires lock_invariant(?p);
+    //@ requires create_lock_ghost_argument(?p) &*& p();
     //@ ensures lock_permission(result, p);
 
-void lock_acquire(struct lock *lock);   // TODO: Make the lock implementation non-reentrant; otherwise, this contract is unsound.
+void lock_acquire(struct lock *lock);  // Assumes that the lock is non-reentrant.
     //@ requires [?f]lock_permission(lock, ?p);
-    //@ ensures unlock_permission(lock, p, f) &*& lock_invariant(p);
+    //@ ensures unlock_permission(lock, p, f) &*& p();
 
 void lock_release(struct lock *lock);
-    //@ requires unlock_permission(lock, ?p, ?f) &*& lock_invariant(p);
+    //@ requires unlock_permission(lock, ?p, ?f) &*& p();
     //@ ensures [f]lock_permission(lock, p);
     
 struct lock* lock_dispose(struct lock* l);
     //@ requires lock_permission(l, ?p);
-    //@ ensures lock_invariant(p);
-    
+    //@ ensures p();
 
 #endif
