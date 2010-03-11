85960c867c12e8835ab2f0fd255992c18e6e9583
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2010-03-11 16:59:48 +0000

    Added a dummy implementation of GSourceView2 for the MacOS build

---
+++ src/macos/GSourceView2.ml
@@ -0,0 +1,22 @@
+class source_buffer buffer =
+  object
+    inherit GText.buffer buffer#as_buffer
+  end
+
+class source_view (source_buffer: source_buffer) view () =
+  object
+    inherit GText.view view#as_view
+    val mutable show_line_numbers = false
+    val mutable draw_spaces: [ `SPACE | `TAB ] list = []
+    method source_buffer = source_buffer
+    method show_line_numbers = show_line_numbers
+    method set_show_line_numbers (enable:bool) = show_line_numbers <- enable
+    method draw_spaces = draw_spaces
+    method set_draw_spaces flags = draw_spaces <- flags
+    method set_tab_width (size: int) = ()
+  end
+
+let source_view ?source_buffer ~packing () =
+  let buffer = match source_buffer with None -> GText.buffer () | Some sbuf -> (sbuf :> GText.buffer) in
+  let view = GText.view ~packing ~buffer () in
+  new source_view (new source_buffer buffer) view ()
