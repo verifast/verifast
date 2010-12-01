4498e604659a6f63f63c466506aee3680750ec74
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2010-12-01 11:11:11 +0000

    - Added example barrier.c, which verifies a barrier synchronization construct + an example client program (stolen from Aquinas Hobor's paper) - Added queue.c and http.c demo programs

---
+++ examples/tutorial/http.c
@@ -0,0 +1,77 @@
+#include "stdlib.h"
+#include "stringBuffers.h"
+#include "sockets.h"
+#include "threading.h"
+
+static struct string_buffer *requests;
+static struct mutex *requestsMutex;
+
+/*@
+
+predicate lock_invariant() =
+    pointer(&requests, ?requests_) &*& string_buffer(requests_);
+
+predicate_family_instance
+        thread_run_data(handle_connection)(struct socket *socket) =
+    socket(socket, ?reader, ?writer) &*&
+    reader(reader) &*& writer(writer) &*&
+    [_]pointer(&requestsMutex, ?mutex) &*&
+    [_]mutex(mutex, lock_invariant);
+
+@*/
+
+void handle_connection(struct socket *socket) //@ : thread_run
+    //@ requires thread_run_data(handle_connection)(socket);
+    //@ ensures true;
+{
+    //@ open thread_run_data(handle_connection)(socket);
+    
+    struct reader *reader = socket_get_reader(socket);
+    struct writer *writer = socket_get_writer(socket);
+
+    struct string_buffer *buffer = create_string_buffer();
+    reader_read_line(reader, buffer);
+    mutex_acquire(requestsMutex);
+    //@ open lock_invariant();
+    string_buffer_append_string_buffer(requests, buffer);
+    string_buffer_append_string(requests, "\r\n");
+    string_buffer_clear(buffer);
+    string_buffer_append_string_buffer(buffer, requests);
+    //@ close lock_invariant();
+    mutex_release(requestsMutex);
+
+    writer_write_string(writer,
+        "HTTP/1.0 200 OK\r\n\r\n"
+        "<html><head><title>Request Viewer</title></head>"
+        "<body><p>Requests:</p><pre>\r\n");
+    writer_write_string_buffer(writer, buffer);
+    writer_write_string(writer, "</pre></body></html>\r\n");
+    socket_close(socket);
+    string_buffer_dispose(buffer);
+}
+
+int main() //@ : main_full(http)
+    //@ requires module(http, true);
+    //@ ensures true;
+{
+    //@ open_module();
+    requests = create_string_buffer();
+    //@ close lock_invariant();
+    //@ close create_mutex_ghost_arg(lock_invariant);
+    requestsMutex = create_mutex();
+    //@ leak pointer(&requestsMutex, ?mutex) &*& mutex(mutex, _);
+    
+    struct server_socket *server = create_server_socket(80);
+    for (;;)
+       /*@
+       invariant
+           server_socket(server) &*&
+           [_]pointer(&requestsMutex, mutex) &*&
+           [_]mutex(mutex, lock_invariant);
+       @*/
+    {
+        struct socket *socket = server_socket_accept(server);
+        //@ close thread_run_data(handle_connection)(socket);
+        thread_start(handle_connection, socket);
+    }
+}
---
+++ examples/tutorial/queue.c
@@ -0,0 +1,127 @@
+#include "stdlib.h"
+
+/*@
+inductive ilist = inil | icons(int, ilist);
+
+fixpoint ilist iappend(ilist xs, ilist ys) {
+    switch (xs) {
+        case inil: return ys;
+        case icons(h, t): return icons(h, iappend(t, ys));
+    }
+}
+@*/
+
+struct node {
+    struct node *next;
+    int value;
+};
+
+/*@
+predicate lseg(struct node *first, struct node *last, ilist values) =
+    first == last ?
+        values == inil
+    :
+        first->next |-> ?next &*& first->value |-> ?value &*&
+        malloc_block_node(first) &*&
+        lseg(next, last, ?values0) &*&
+        values == icons(value, values0);
+
+lemma void lseg_add(struct node *first)
+    requires lseg(first, ?last, ?values) &*&
+        last->next |-> ?next &*& last->value |-> ?value &*&
+        malloc_block_node(last) &*&
+        next->next |-> ?nextNext;
+    ensures lseg(first, next, iappend(values, icons(value, inil)))
+        &*& next->next |-> nextNext;
+{
+    open lseg(first, last, values);
+    if (first == last) {
+        close lseg(next, next, inil);
+    } else {
+        lseg_add(first->next);
+    }
+    close lseg(first, next, _);
+}
+@*/
+
+struct queue {
+    struct node *first;
+    struct node *last;
+};
+
+/*@
+predicate queue(struct queue *q, ilist values) =
+    q->first |-> ?first &*& q->last |-> ?last &*&
+    malloc_block_queue(q) &*&
+    lseg(first, last, values) &*&
+    last->next |-> _ &*& last->value |-> _ &*&
+    malloc_block_node(last);
+@*/
+
+struct queue *create()
+    //@ requires true;
+    //@ ensures queue(result, inil);
+{
+    struct queue *q = malloc(sizeof(struct queue));
+    if (q == 0) abort();
+    struct node *n = malloc(sizeof(struct node));
+    if (n == 0) abort();
+    q->first = n;
+    q->last = n;
+    //@ close lseg(n, n, inil);
+    //@ close queue(q, inil);
+    return q;
+}
+
+void enqueue(struct queue *q, int x)
+    //@ requires queue(q, ?vs);
+    //@ ensures queue(q, iappend(vs, icons(x, inil)));
+{
+    //@ open queue(q, vs);
+    struct node *n = malloc(sizeof(struct node));
+    if (n == 0) abort();
+    q->last->next = n;
+    q->last->value = x;
+    q->last = n;
+    //@ lseg_add(q->first);
+    //@ close queue(q, _);
+}
+
+int dequeue(struct queue *q)
+    //@ requires queue(q, ?vs) &*& vs != inil;
+    //@ ensures queue(q, ?vs0) &*& vs == icons(result, vs0);
+{
+    //@ open queue(q, _);
+    struct node *n = q->first;
+    //@ open lseg(n, _, _);
+    int result = n->value;
+    q->first = n->next;
+    free(n);
+    //@ close queue(q, _);
+    return result;
+}
+
+void dispose(struct queue *q)
+    //@ requires queue(q, inil);
+    //@ ensures true;
+{
+    //@ open queue(q, inil);
+    //@ open lseg(_, _, _);
+    free(q->first);
+    free(q);
+}
+
+int main()
+    //@ requires true;
+    //@ ensures true;
+{
+    struct queue *q = create();
+    enqueue(q, 10);
+    enqueue(q, 20);
+    int x1 = dequeue(q);
+    assert(x1 == 10);
+    int x2 = dequeue(q);
+    assert(x2 == 20);
+    dispose(q);
+    return 0;
+}
