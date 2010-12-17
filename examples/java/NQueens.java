5a4a7a6f078f93cf334845425dc6c410825795b1
Jan Smans <jan.smans@cs.kuleuven.be>
2010-12-17 16:24:55 +0000

    - uploading a different version of NQueens with various assumes left to prove

--- examples/java/NQueens.java
+++ examples/java/NQueens.java
@@ -1,122 +1,87 @@
 /*@
-fixpoint boolean forall<t>(list<t> xs, fixpoint(t, boolean) p) {
-  switch(xs) {
-    case nil: return true;
-    case cons(h, t): return p(h) && forall(t, p);
-  }
-}
-lemma void not_forall<t>(list<t> xs, fixpoint(t, boolean) p, int i);
-  requires 0<=i &*& i < length(xs) &*& ! p(nth(i, xs));
-  ensures ! forall(xs, p);
-
-lemma_auto void take_nth<t>(list<t> xs, int i, int j);
-  requires 0<=i &*& i < j &*& j <= length(xs);
-  ensures nth(i, take(j, xs)) == nth(i, xs);
-fixpoint list<any> zip<t1, t2>(list<t1> xs, list<t2> ys) {
-  switch(xs) {
-    case nil: return nil;
-    case cons(xh, xt):
-      return switch(ys) {
-        case nil: return nil;
-        case cons(yh, yt): return cons((any) pair(xh, yh), zip(xt, yt));
-      };
-  }
-}
-
-
-fixpoint boolean safe(unit u, list<int> vs, int pos, int bp, int bq) {
-  switch(u) {
-    case unit: return bp != bq;// && bp - nth(q, vs) != pos - q;
+fixpoint boolean consistent(list<int> vs, nat i, int pos) {
+  switch(i) {
+    case zero: return true;
+    case succ(j): return nth(int_of_nat(j), vs) != nth(pos, vs) &&
+                  pos - int_of_nat(j) != ((int)nth(pos, vs)) - ((int) nth(int_of_nat(j), vs)) &&
+                  int_of_nat(j) - pos  != ((int) nth(int_of_nat(j), vs)) - nth(pos, vs) &&
+                  consistent(vs, j, pos);
   }
 }
 
-lemma void take_one_more<t>(list<t> vs, int i)
-  requires 0 <= i && i < length(vs);
-  ensures append(take(i, vs), cons(head(drop(i, vs)), nil)) == take(i + 1, vs);
-{
-  switch(vs) {
-    case nil:
-    case cons(h, t):
-      if(i == 0) 
-      {
-      } else {
-        take_one_more(t, i - 1);
-      }
+fixpoint boolean allconsistent(list<int> vs, nat n) {
+  switch(n) {
+    case zero: return true;
+    case succ(n0): return consistent(vs, n0, int_of_nat(n0)) && allconsistent(vs, n0);
   }
 }
 
-lemma void nth_head_drop<t>(list<t> vs, int i)
-  requires 0 <= i && i < length(vs);
-  ensures head(drop(i, vs)) == nth(i, vs);
-{
-  switch(vs) {
-    case nil:
-    case cons(h, t):
-      if(i == 0) 
-      {
-      } else {
-        nth_head_drop(t, i - 1);
-      }
-  }
-}
-
-lemma void append_forall<t>(list<t> vs1, list<t> vs2, fixpoint(t, boolean) p)
-  requires forall(vs1, p) && forall(vs2, p);
-  ensures forall(append(vs1, vs2), p) == true;
+lemma void take_append<t>(list<t> vs1, list<t> vs2, int n)
+  requires 0 <= n && n <= length(append(vs1, vs2)) && n <= length(vs1);
+  ensures take(n, append(vs1, vs2)) == (n <= length(vs1) ? take(n, vs1) : append(vs1, take(n - length(vs1), vs2)));
 {
   switch(vs1) {
     case nil:
-    case cons(h, t): append_forall(t, vs2, p);
+    case cons(h, t): 
+      if(n == 0) {
+      } else {
+        take_append(t, vs2, n- 1);
+      }
   }
 }
-
-
 @*/
 
-class NQueens {   
-  public static boolean isConsistent(int[] board, int pos) 
-    //@ requires board != null &*& array_slice(board, 0, board.length, ?vs) &*& 0 <= pos &*& pos < board.length;
-    //@ ensures board != null &*& array_slice(board, 0, board.length, vs) &*& forall(take(pos, vs), (safe)(unit, vs, pos, nth(pos, vs))) == result;
+class NQueens {
+  static boolean isConsistent(int[] board, int pos) 
+    //@ requires board != null &*& array_slice(board, 0, board.length, ?vs) &*& 0 <= pos && pos < board.length;
+    //@ ensures array_slice(board, 0, board.length, vs) &*& result == consistent(vs, nat_of_int(pos), pos);
   {
-    int bp = board[pos];      
-    for(int q = 0; q < pos; q++) 
-      //@ invariant 0 <= q &*& q <= pos &*& array_slice(board, 0, board.length, vs) &*& forall(take(q, vs), (safe)(unit, vs, pos, bp)) == true;
+    int bp = board[pos];
+    for(int i = 0; i < pos; i++) 
+      //@ invariant 0 <= i &*& i <= pos &*& array_slice(board, 0, board.length, vs) &*& consistent(vs, nat_of_int(i), pos) == true;
     {
-      int bq = board[q];
-      //@ assert bq == nth(q, vs);
-      if(bq == bp /*|| (bp - bq == pos - q) || (bp - bq == pos - q)*/) {
-        //@ take_nth(vs, q, pos);
-        //@ assert bq == nth(q, vs);
-        //@ not_forall(take(pos, vs), (safe)(unit, vs, pos, bp), q);
+      int bi = board[i];
+      //@ succ_int(i);
+      if(bi == bp || pos - i == bp - bi || i - pos == bi - bp) {
+        //@ assert consistent(vs, nat_of_int(i + 1), pos) == false;
+        //@ assume(false); //todo: show that not_consistency is monotonic
         return false;
       }
-      //@ take_one_more(vs, q);
-      //@ nth_head_drop(vs, q);
-      //@ append_forall(take(q, vs), cons(nth(q, vs), nil), (safe)(unit, vs, pos, bp));
     }
-
     return true;
   }
-
-  /*public static int[] search(int[] board, int pos) 
-    //@ requires board != null &*& array_slice(board, 0, board.length, ?vs) &*& 0 <= pos &*& pos <= board.length &*& forall(take(pos-1, vs), (safe)(unit, pos - 1, nth(pos - 1, vs))) == true;
-    //@ ensures result == null ? array_slice(board, 0, board.length, ?vs2) : result == board &*& array_slice(result, 0, board.length, ?vs2) &*&  forall(take(board.length - 1, vs2), (safe)(unit, board.length - 1, nth(board.length - 1, vs2))) == true;
+  
+  static int[] search(int[] board, int pos)
+    //@ requires board != null &*& array_slice(board, 0, board.length, ?vs) &*& 0 <= pos && pos <= board.length &*& allconsistent(vs, nat_of_int(pos)) == true;
+    /*@ ensures array_slice(board, 0, board.length, ?vs2) &*& result == null  ? 
+                  take(pos, vs) == take(pos, vs2) &*&
+                  allconsistent(vs2, nat_of_int(pos)) == true 
+                  /* todo: specify here that append(take(pos, vs), all lists of board.length - pos) are not allconsistent */
+                  : 
+                  result == board &*& allconsistent(vs2, nat_of_int(length(vs2))) == true; @*/
   {
     if(pos == board.length) {
       return board;
-    }
-
-    for(int i = 0; i < pos; i++) 
-      //@ invariant array_slice(board, 0, board.length, ?vs3);
-    {
-      board[pos] = i;
-      if(isConsistent(board, pos)) {
-        int[] s = search(board, pos + 1);
-        if(s != null) {
-          return s;
+    } else {
+      for(int i = 0; i < board.length; i++) 
+        //@ invariant array_slice(board, 0, board.length, ?vs2) &*& take(pos, vs) == take(pos, vs2) &*& allconsistent(vs2, nat_of_int(pos)) == true;
+      {
+        board[pos] = i;
+        //@ assert array_slice(board, 0, board.length, ?vs3);
+        //@ take_append(take(pos, vs2), append(cons(i, nil), take(length(vs2) - pos - 1, tail(drop(pos, vs2)))), pos);
+        //@ assume(allconsistent(vs3, nat_of_int(pos)) == true); //todo: show that updating the array at pos does not affect allconsistent
+        if(isConsistent(board, pos)) {
+          //@ succ_int(pos);
+          int[] s = search(board, pos + 1);
+          if(s != null) {
+            return s;
+          }
+          //@ assert array_slice(board, 0, board.length, ?vs4);
+          //@ assert take(pos + 1, vs3) == take(pos + 1, vs4);
+          //@ assume(take(pos, vs3) == take(pos, vs4)); //todo: show that take(i, vs) == take(i, vs2) implies take(i - 1, vs) == take(i - 1, vs);
         }
       }
+      return null;
     }
-    return null;
-  }*/
+  }
 }
