9ed8928697d1106bc902e1a2ec405dacd20e50e9
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2011-08-04 14:49:17 +0000

    Improved the readability of the example plugin

--- examples/plugin/trivial_plugin.ml
+++ examples/plugin/trivial_plugin.ml
@@ -1,33 +1,35 @@
 open Plugins
 open Plugins2
 
-let (create_typechecked_assertion, match_typechecked_assertion) = DynType.create_ctor ()
+let gt (Some x) = x
 
-let (create_state, match_state) = DynType.create_ctor ()
+let mk_dtasn, gt_dtasn = DynType.create_ctor ()
+let mk_ptasn tasn = TypecheckedPluginAssertion (mk_dtasn tasn)
+let gt_ptasn (TypecheckedPluginAssertion dtasn) = gt (gt_dtasn dtasn)
+
+let mk_dstate, gt_dstate = DynType.create_ctor ()
+let mk_pstate state = PluginState (mk_dstate state)
+let gt_pstate (PluginState dstate) = gt (gt_dstate dstate)
 
 let typecheck_assertion tenv text =
   try
-    let value = int_of_string text in
-    (TypecheckedPluginAssertion (create_typechecked_assertion value), [])
+    mk_ptasn (int_of_string text), []
   with
     Failure "int_of_string" -> raise (PluginStaticError (0, String.length text, "Integer literal expected"))
 
 let create_instance ctxt =
   object
-    method empty_state = PluginState (create_state 0)
-    method check_leaks (PluginState state) = let (Some s) = match_state state in if s <> 0 then Some "Function leaks trivial resource" else None
+    method empty_state = mk_pstate 0
+    method check_leaks pstate = if gt_pstate pstate <> 0 then Some "Function leaks trivial resource" else None
     method produce_assertion: 'a. plugin_state -> 'term environment -> typechecked_plugin_assertion -> (plugin_state -> 'term environment -> 'a) -> 'a =
-      fun (PluginState s) env (TypecheckedPluginAssertion asn) cont ->
-      let Some s = match_state s in
-      let Some asn = match_typechecked_assertion asn in
-      cont (PluginState (create_state (s + asn))) env
+      fun pstate env ptasn cont ->
+      cont (mk_pstate (gt_pstate pstate + gt_ptasn ptasn)) env
     method consume_assertion: 'a. plugin_state -> 'term environment -> typechecked_plugin_assertion -> (plugin_state -> 'term environment -> 'a) -> 'a =
-      fun (PluginState s) env (TypecheckedPluginAssertion asn) cont ->
-      let Some s = match_state s in
-      let Some asn = match_typechecked_assertion asn in
-      if s < asn then raise (PluginConsumeError (0, 1, "Insufficient trivial resource"));
-      cont (PluginState (create_state (s - asn))) env
-    method string_of_state (PluginState state) = let (Some s) = match_state state in string_of_int s
+      fun pstate env ptasn cont ->
+      let state, tasn = gt_pstate pstate, gt_ptasn ptasn in
+      if state < tasn then raise (PluginConsumeError (0, 1, "Insufficient trivial resource"));
+      cont (mk_pstate (state - tasn)) env
+    method string_of_state pstate = string_of_int (gt_pstate pstate)
   end
 
 let plugin = object
