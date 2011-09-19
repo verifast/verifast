26b30d8c578d9b49b0d9a4dd6f65a7d07ccd5137
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2011-09-19 18:56:29 +0000

    The vfide launch script for MacOS should now be robust against paths with spaces

--- src/vfide-mac.sh
+++ src/vfide-mac.sh
@@ -3,4 +3,4 @@ MYRELDIR=`dirname $0`
 MYABSDIR=`cd $MYRELDIR; pwd`
 export DYLD_LIBRARY_PATH=$MYABSDIR/../lib:$DYLD_LIBRARY_PATH
 export GDK_PIXBUF_MODULE_FILE=$MYABSDIR/../etc/gtk-2.0/gdk-pixbuf.loaders
-"$MYABSDIR/vfide-core" $*
+"$MYABSDIR/vfide-core" "$@"
