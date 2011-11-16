bdb4a6fe5c7f9134c278a581f3f44e7ae7f3a920
Jan Smans <jan.smans@cs.kuleuven.be>
2011-11-16 18:13:24 +0000

    adding assignment (tiny_httpd), todo: - test on linux (separator is / instead of \) - add more text to assignment description (e.g. distinguish must-solve and extra)

--- bin/stdio.h
+++ bin/stdio.h
@@ -9,12 +9,12 @@ struct file* fopen(char* filename, char* mode); // todo: check that mode is a va
   /*@ requires [?f]chars(filename, ?fcs) &*& [?g]chars(mode, ?mcs) &*& mem('\0', fcs) == true &*& mem('\0', mcs) == true &*&
                (length(mcs) == 2 || length(mcs) == 3) &*&
                (nth(0, mcs) == 'r' || nth(0, mcs) == 'w' || nth(0, mcs) == 'a') &*&
-               (length(mcs) == 3 ? nth(1, mcs) == '+' : true);         
+               (length(mcs) == 3 ? nth(1, mcs) == '+' || nth(1, mcs) == 'b' : true);         
   @*/          
   //@ ensures [f]chars(filename, fcs) &*& [g]chars(mode, mcs) &*& result == 0 ? true : file(result);
   
 int fread(void* buffer, int size, int n, struct file* fp);
-  //@ requires chars(buffer, ?cs) &*& 0<=size &*& 0<=n &*& size * n <= length(cs) &*& file(fp);
+  //@ requires chars(buffer, ?cs) &*& 0<=size &*& 0<=n &*& size * n <= length(cs) &*& file(fp); // TODO!
   //@ ensures chars(buffer, ?cs2) &*& length(cs2) == length(cs) &*& file(fp) &*& result <= n;
   
 int fwrite(void* buffer, int size, int n, struct file* fp);
@@ -32,7 +32,11 @@ int puts(char* format);
 int printf(char* format, int arg);
   //@ requires [?f]chars(format, ?cs) &*& cs == cons('%', cons('i', cons('\0', nil)));
   //@ ensures [f]chars(format, cs);
-
+  
+int feof(struct file* fp);
+  //@ requires file(fp);
+  //@ ensures file(fp);
+  
 struct file* fclose(struct file* fp); 
   //@ requires file(fp);
   //@ ensures true;
