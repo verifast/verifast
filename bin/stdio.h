39eb0401f2a5228ab93b32ff56e50fafb078dbb3
Jan Smans <jan.smans@cs.kuleuven.be>
2011-11-23 14:55:45 +0000

    update of webserver assignment

--- bin/stdio.h
+++ bin/stdio.h
@@ -1,11 +1,13 @@
 #ifndef STDIO_H
 #define STDIO_H
 
+typedef struct file FILE;
+
 struct file;
 
 //@ predicate file(struct file* fp);
 
-struct file* fopen(char* filename, char* mode); // todo: check that mode is a valid mode string
+FILE* fopen(char* filename, char* mode); // todo: check that mode is a valid mode string
   /*@ requires [?f]chars(filename, ?fcs) &*& [?g]chars(mode, ?mcs) &*& mem('\0', fcs) == true &*& mem('\0', mcs) == true &*&
                (length(mcs) == 2 || length(mcs) == 3) &*&
                (nth(0, mcs) == 'r' || nth(0, mcs) == 'w' || nth(0, mcs) == 'a') &*&
@@ -13,15 +15,15 @@ struct file* fopen(char* filename, char* mode); // todo: check that mode is a va
   @*/          
   //@ ensures [f]chars(filename, fcs) &*& [g]chars(mode, mcs) &*& result == 0 ? true : file(result);
   
-int fread(void* buffer, int size, int n, struct file* fp);
+int fread(void* buffer, int size, int n, FILE* fp);
   //@ requires chars(buffer, ?cs) &*& 0<=size &*& 0<=n &*& size * n <= length(cs) &*& file(fp); // TODO!
   //@ ensures chars(buffer, ?cs2) &*& length(cs2) == length(cs) &*& file(fp) &*& result <= n;
   
-int fwrite(void* buffer, int size, int n, struct file* fp);
+int fwrite(void* buffer, int size, int n, FILE* fp);
   //@ requires chars(buffer, ?cs) &*& 0<=size &*& 0<=n &*& size * n <= length(cs) &*& file(fp);
   //@ ensures chars(buffer, cs) &*& file(fp);
   
-char* fgets(char* buffer, int n, struct file* fp);
+char* fgets(char* buffer, int n, FILE* fp);
   //@ requires chars(buffer, ?cs) &*& length(cs) == n &*& file(fp);
   //@ ensures chars(buffer, ?cs2) &*& length(cs2) == n &*& file(fp) &*& result == 0 ? true : mem('\0', cs2) == true;
 
@@ -33,11 +35,11 @@ int printf(char* format, int arg);
   //@ requires [?f]chars(format, ?cs) &*& cs == cons('%', cons('i', cons('\0', nil)));
   //@ ensures [f]chars(format, cs);
   
-int feof(struct file* fp);
+int feof(FILE* fp);
   //@ requires file(fp);
   //@ ensures file(fp);
   
-struct file* fclose(struct file* fp); 
+int fclose(FILE* fp); 
   //@ requires file(fp);
   //@ ensures true;
 
