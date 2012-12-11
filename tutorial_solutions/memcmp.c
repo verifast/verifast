8ed5233bacae0c8dedef41d5fd6cc4921bb9296a
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2012-12-11 12:30:36 +0000

    Tutorial: Added a section introducing predicate 'chars'

---
+++ tutorial_solutions/memcmp.c
@@ -0,0 +1,25 @@
+int memcmp(char *p1, char *p2, int count)
+    //@ requires [?f1]p1[0..count] |-> ?cs1 &*& [?f2]p2[0..count] |-> ?cs2;
+    //@ ensures [f1]p1[0..count] |-> cs1 &*& [f2]p2[0..count] |-> cs2 &*& true == ((result == 0) == (cs1 == cs2));
+{
+    int result = 0;
+    for (int i = 0; ; i++)
+        //@ requires [f1]p1[i..count] |-> ?xs1 &*& [f2]p2[i..count] |-> ?xs2 &*& result == 0;
+        //@ ensures [f1]p1[old_i..count] |-> xs1 &*& [f2]p2[old_i..count] |-> xs2 &*& true == ((result == 0) == (xs1 == xs2));
+    {
+        //@ open chars(p1 + i, _, _);
+        //@ open chars(p2 + i, _, _);
+        if (i == count) {
+            //@ close [f1]chars(p1 + i, 0, _);
+            //@ close [f2]chars(p2 + i, 0, _);
+            break;
+        }
+        if (p1[i] < p2[i]) {
+            result = -1; break;
+        }
+        if (p1[i] > p2[i]) {
+            result = 1; break;
+        }
+    }
+    return result;
+}
---
+++ tutorial_solutions/memcpy.c
@@ -0,0 +1,45 @@
+void memcpy(char *dest, char *src, int count)
+    //@ requires chars(dest, count, _) &*& [?f]chars(src, count, ?cs);
+    //@ ensures chars(dest, count, cs) &*& [f]chars(src, count, cs);
+{
+    for (int i = 0; ; i++)
+        //@ requires chars(dest + i, count - i, _) &*& [f]chars(src + i, count - i, ?cs0);
+        //@ ensures chars(dest + old_i, count - old_i, cs0) &*& [f]chars(src + old_i, count - old_i, cs0);
+    {
+        //@ open chars(dest + i, _, _);
+        //@ open chars(src + i, _, _);
+        if (i == count) {
+            //@ close [f]chars(src + i, 0, _);
+            break;
+        }
+        dest[i] = src[i];
+    }
+}
+
+void memcpy1(char *dest, char *src, int count) // Identical but uses array slice syntax.
+    //@ requires dest[0..count] |-> _ &*& [?f]src[0..count] |-> ?cs;
+    //@ ensures dest[0..count] |-> cs &*& [f]src[0..count] |-> cs;
+{
+    for (int i = 0; ; i++)
+        //@ requires dest[i..count] |-> _ &*& [f]src[i..count] |-> ?cs0;
+        //@ ensures dest[old_i..count] |-> cs0 &*& [f]src[old_i..count] |-> cs0;
+    {
+        //@ open chars(dest + i, _, _);
+        //@ open chars(src + i, _, _);
+        if (i == count) {
+            //@ close [f]chars(src + i, 0, _);
+            break;
+        }
+        dest[i] = src[i];
+    }
+}
+
+void test()
+    //@ requires true;
+    //@ ensures true;
+{
+    char buffer1[7] = "Hello!";
+    char buffer2[7];
+    memcpy(buffer2, buffer1, 7);
+    assert(buffer2[5] == '!');
+}
