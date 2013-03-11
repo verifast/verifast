9b3fbabc941321c8137b1ec3cf9ad91b0a36ffd5
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2013-03-11 13:41:12 +0000

    - Cleaned up problem3.c - Synced problem3-alternative.c with problem3.c

--- examples/fm2012/problem3-alternative.c
+++ examples/fm2012/problem3-alternative.c
@@ -1,146 +1,121 @@
-struct tree {
-    Tree left;
-    int data;
-    Tree right;
-};
-
+struct tree { Tree left; int data; Tree right; };
 typedef struct tree *Tree;
 
 /*@
+inductive tree = empty | node(Tree, tree, int, tree);
 
-inductive tree_ = empty | node(tree_, Tree, int, tree_);
-
-fixpoint tree_ delete_min(tree_ t) {
+fixpoint tree delete_min(tree t) {
     switch (t) {
         case empty: return empty;
-        case node(t1, p, v, t2): return t1 == empty ? t2 : node(delete_min(t1), p, v, t2);
+        case node(p, t1, v, t2): return t1 == empty ? t2 : node(p, delete_min(t1), v, t2);
+    }
+}
+fixpoint int min_value(tree vs) {
+    switch (vs) {
+        case empty: return 0;
+        case node(p, t1, v, t2): return t1 == empty ? v : min_value(t1);
+    }
+}
+fixpoint Tree p_(tree t) {
+    switch (t) {
+        case empty: return 0;
+        case node(p, l, v, r): return p;
     }
 }
 
-predicate Tree(Tree t; tree_ vs) =
+predicate Tree(Tree t; tree vs) =
     t == 0 ?
         vs == empty
     :
-        t != 0 &*& t->left |-> ?l &*& Tree(l, ?vsl) &*&
-        t->right |-> ?r &*& Tree(r, ?vsr) &*&
-        t->data |-> ?v &*& malloc_block_tree(t) &*&
-        vs == node(vsl, t, v, vsr);
-        
+        t->left |-> ?l &*& Tree(l, ?vsl) &*& t->right |-> ?r &*& Tree(r, ?vsr) &*&
+        t->data |-> ?v &*& malloc_block_tree(t) &*& vs == node(t, vsl, v, vsr);
+
+predicate Tree_r(Tree t; int v, tree rvs) =
+    t->data |-> v &*& malloc_block_tree(t) &*& t->right |-> ?r &*& Tree(r, rvs);
+
 lemma_auto void Tree_inv()
     requires Tree(?t, ?vs);
-    ensures Tree(t, vs) &*& t == getptr(vs);
-{
-    open Tree(_, _);
-}
+    ensures Tree(t, vs) &*& t == p_(vs);
+{ open Tree(_, _); }
 
-fixpoint tree_ getleft(tree_ t) {
-    switch (t) {
-        case empty: return empty;
-        case node(l, p, v, r): return l;
-    }
-}
+typedef lemma void fill_hole_lemma(predicate() pred, Tree t, tree vs, Tree pp, Tree r)();
+    requires pred() &*& pp->left |-> r;
+    ensures Tree(t, delete_min(vs));
 
-fixpoint tree_ getright(tree_ t) {
-    switch (t) {
-        case empty: return empty;
-        case node(l, p, v, r): return r;
-    }
-}
-
-fixpoint int getvalue(tree_ t) {
-    switch (t) {
-        case empty: return 0;
-        case node(l, p, v, r): return v;
-    }
-}
-
-
-fixpoint Tree getptr(tree_ t) {
-    switch (t) {
-        case empty: return 0;
-        case node(l, p, v, r): return p;
-    }
-}
-
-fixpoint int min_value(tree_ vs) {
-    switch (vs) {
-        case empty: return 0;
-        case node(t1, p, v, t2): return t1 == empty ? v : min_value(t1);
-    }
-}
-
-fixpoint bool ordered_between(int min, tree_ t, int max) {
+fixpoint bool sorted_between(int min, tree t, int max) {
     switch (t) {
         case empty: return true;
-        case node(t1, p, v, t2): return min <= v && v <= max && ordered_between(min, t1, v) && ordered_between(v, t2, max);
+        case node(p, t1, v, t2): return min <= v && v <= max && sorted_between(min, t1, v) && sorted_between(v, t2, max);
     }
 }
 
-lemma void ordered_between_weaken(int min0, int min1, tree_ t, int max0, int max1)
-    requires ordered_between(min1, t, max0) == true &*& min0 <= min1 &*& max0 <= max1;
-    ensures ordered_between(min0, t, max1) == true;
+lemma void sorted_between_weaken(int min0, int min1, tree t, int max0, int max1)
+    requires sorted_between(min1, t, max0) == true &*& min0 <= min1 &*& max0 <= max1;
+    ensures sorted_between(min0, t, max1) == true;
 {
     switch (t) {
         case empty:
-        case node(t1, p, v, t2):
-            ordered_between_weaken(min0, min1, t1, v, v);
-            ordered_between_weaken(v, v, t2, max0, max1);
+        case node(p, t1, v, t2):
+            sorted_between_weaken(min0, min1, t1, v, v);
+            sorted_between_weaken(v, v, t2, max0, max1);
     }
 }
 
-lemma_auto void delete_min_ordered_between(int min, tree_ t, int max)
-    requires ordered_between(min, t, max) == true;
-    ensures ordered_between(min, delete_min(t), max) == true;
+lemma_auto void delete_min_sorted_between(int min, tree t, int max)
+    requires sorted_between(min, t, max) == true;
+    ensures sorted_between(min, delete_min(t), max) == true;
 {
     switch (t) {
         case empty:
-        case node(t1, p, v, t2):
+        case node(p, t1, v, t2):
             if (t1 != empty) {
-                delete_min_ordered_between(min, t1, v);
+                delete_min_sorted_between(min, t1, v);
             } else {
-                ordered_between_weaken(min, v, t2, max, max);
+                sorted_between_weaken(min, v, t2, max, max);
             }
     }
 }
+
+fixpoint bool sorted(tree t) { return sorted_between(INT_MIN, t, INT_MAX); }
 @*/
 
 /*@
-predicate Tree_with_hole(Tree t, Tree hole; tree_ vs) =
-    hole != 0 &*&
-    t == hole ?
-        vs == empty
-    :
-        t != 0 &*& t->left |-> ?l &*& Tree_with_hole(l, hole, ?vsl) &*&
-        t->right |-> ?r &*& Tree(r, ?vsr) &*&
-        t->data |-> ?v &*& malloc_block_tree(t) &*&
-        vs == node(vsl, t, v, vsr);
+predicate Tree_with_hole(Tree t, Tree hole; tree vs) =
+  hole != 0 &*&
+  t == hole ?
+    vs == empty
+  :
+    t != 0 &*& t->left |-> ?l &*& Tree_with_hole(l, hole, ?vsl) &*&
+    t->right |-> ?r &*& Tree(r, ?vsr) &*&
+    t->data |-> ?v &*& malloc_block_tree(t) &*&
+    vs == node(t, vsl, v, vsr);
         
-fixpoint tree_ merge(tree_ t1, tree_ t2) {
+fixpoint tree merge(tree t1, tree t2) {
   switch(t1) {
     case empty: return t2;
-    case node(l, p, v, r): return node(merge(l, t2), p, v, r); 
+    case node(p, l, v, r): return node(p, merge(l, t2), v, r); 
   }
 }
 
-lemma void move_hole(struct tree* t)
-  requires Tree_with_hole(t, ?hole, ?vs1) &*& Tree(hole, node(?left, hole, ?v, ?right)) &*& getptr(left) != 0;
-  ensures Tree_with_hole(t, getptr(left), merge(vs1, node(empty, hole, v, right))) &*& Tree(getptr(left), left) &*&
-          merge(vs1, node(left, hole, v, right)) == merge(merge(vs1, node(empty, hole, v, right)), left);
+lemma void move_hole(Tree t)
+  requires Tree_with_hole(t, ?hole, ?vs1) &*& Tree(hole, node(hole, ?left, ?v, ?right)) &*& p_(left) != 0;
+  ensures Tree_with_hole(t, p_(left), merge(vs1, node(hole, empty, v, right))) &*& Tree(p_(left), left) &*&
+          merge(vs1, node(hole, left, v, right)) == merge(merge(vs1, node(hole, empty, v, right)), left);
 {
   open Tree_with_hole(t, hole, vs1);
   open Tree(hole, _);
-  open Tree(getptr(left), _);
+  open Tree(p_(left), _);
   if(t == hole) {
-    close Tree_with_hole(t, getptr(left), merge(vs1, node(empty, hole, v, right)));
+    close Tree_with_hole(t, p_(left), merge(vs1, node(hole, empty, v, right)));
   } else {
     move_hole(t->left);
-    close Tree_with_hole(t, getptr(left), merge(vs1, node(empty, hole, v, right)));
+    close Tree_with_hole(t, p_(left), merge(vs1, node(hole, empty, v, right)));
   }
 }
 
-lemma void plug_hole(struct tree* t1, struct tree* t2)
+lemma void plug_hole(Tree t1, Tree t2)
   requires Tree_with_hole(t1, t2, ?vs1) &*& Tree(t2, ?vs2);
-  ensures Tree(t1, merge(vs1, vs2)) ;
+  ensures Tree(t1, merge(vs1, vs2));
 {
   open Tree_with_hole(t1, t2, vs1);
   open Tree(t2, vs2);
@@ -151,7 +126,7 @@ lemma void plug_hole(struct tree* t1, struct tree* t2)
   }
 }
 
-lemma void delete_min_merge(tree_ t1, tree_ t2)
+lemma void delete_min_merge(tree t1, tree t2)
   requires t2 != empty;
   ensures merge(t1, t2) != empty &*&
           delete_min(merge(t1, t2)) == merge(t1, delete_min(t2)) &*&
@@ -159,15 +134,15 @@ lemma void delete_min_merge(tree_ t1, tree_ t2)
 {
   switch(t1) {
     case empty:
-    case node(l, p, v, r):
+    case node(p, l, v, r):
       delete_min_merge(l, t2);
   }
 }
 @*/
 
 void search_tree_delete_min(struct tree* t, struct tree** r1, int* r2)
-  //@ requires Tree(t, ?vs) &*& vs != empty &*& pointer(r1, _) &*& integer(r2, _) &*& ordered_between(INT_MIN, vs, INT_MAX) == true;
-  //@ ensures pointer(r1, ?tresult) &*& Tree(tresult, delete_min(vs)) &*& integer(r2, min_value(vs)) &*& ordered_between(INT_MIN, vs, INT_MAX) == true;
+  //@ requires Tree(t, ?vs) &*& vs != empty &*& *r1 |-> _ &*& *r2 |-> _ &*& sorted(vs) == true;
+  //@ ensures *r1 |-> ?tresult &*& Tree(tresult, delete_min(vs)) &*& *r2 |-> min_value(vs) &*& sorted(delete_min(vs)) == true;
 {
   Tree tt, pp, p;
   int m;
@@ -179,8 +154,8 @@ void search_tree_delete_min(struct tree* t, struct tree** r1, int* r2)
   } else {
     pp = t; tt = p->left;
     while (tt != 0)
-      /*@ invariant Tree_with_hole(t, pp, ?vs1) &*& Tree(pp, ?vs2) &*& getptr(getleft(vs2)) == p &*& 
-                    p != 0 &*& getptr(getleft(getleft(vs2))) == tt &*& vs == merge(vs1, vs2);
+      /*@ invariant Tree_with_hole(t, pp, ?vs1) &*& Tree(pp, ?vs2) &*& vs2 == node(pp, ?lvs, _, _) &*&
+                    lvs == node(p, ?llvs, _, _) &*& p_(llvs) == tt &*& vs == merge(vs1, vs2);
       @*/
     {
       //@ open Tree(pp, vs2);
@@ -191,10 +166,10 @@ void search_tree_delete_min(struct tree* t, struct tree** r1, int* r2)
     //@ open Tree(pp, _);
     //@ open Tree(pp->left, _);
     //@ open Tree(tt, _);
-    m = p->data; tt = p->right; free(p); pp->left= tt;
+    m = p->data; tt = p->right; free(p); pp->left = tt;
     //@ plug_hole(t, pp);
     //@ delete_min_merge(vs1, vs2); 
-   }
-   *r1 = t;
-   *r2 = m;
+  }
+  *r1 = t;
+  *r2 = m;
 }
--- examples/fm2012/problem3.c
+++ examples/fm2012/problem3.c
@@ -1,187 +1,135 @@
-struct tree {
-    Tree left;
-    int data;
-    Tree right;
-};
-
+struct tree { Tree left; int data; Tree right; };
 typedef struct tree *Tree;
 
 /*@
+inductive tree = empty | node(Tree, tree, int, tree);
 
-inductive tree_ = empty | node(tree_, Tree, int, tree_);
-
-fixpoint tree_ delete_min(tree_ t) {
+fixpoint tree delete_min(tree t) {
     switch (t) {
         case empty: return empty;
-        case node(t1, p, v, t2): return t1 == empty ? t2 : node(delete_min(t1), p, v, t2);
+        case node(p, t1, v, t2): return t1 == empty ? t2 : node(p, delete_min(t1), v, t2);
+    }
+}
+fixpoint int min_value(tree vs) {
+    switch (vs) {
+        case empty: return 0;
+        case node(p, t1, v, t2): return t1 == empty ? v : min_value(t1);
+    }
+}
+fixpoint Tree p_(tree t) {
+    switch (t) {
+        case empty: return 0;
+        case node(p, l, v, r): return p;
     }
 }
 
-predicate Tree(Tree t; tree_ vs) =
+predicate Tree(Tree t; tree vs) =
     t == 0 ?
         vs == empty
     :
-        t->left |-> ?l &*& Tree(l, ?vsl) &*&
-        t->right |-> ?r &*& Tree(r, ?vsr) &*&
-        t->data |-> ?v &*& malloc_block_tree(t) &*&
-        vs == node(vsl, t, v, vsr);
-        
+        t->left |-> ?l &*& Tree(l, ?vsl) &*& t->right |-> ?r &*& Tree(r, ?vsr) &*&
+        t->data |-> ?v &*& malloc_block_tree(t) &*& vs == node(t, vsl, v, vsr);
+
+predicate Tree_r(Tree t; int v, tree rvs) =
+    t->data |-> v &*& malloc_block_tree(t) &*& t->right |-> ?r &*& Tree(r, rvs);
+
 lemma_auto void Tree_inv()
     requires Tree(?t, ?vs);
-    ensures Tree(t, vs) &*& t == getptr(vs);
-{
-    open Tree(_, _);
-}
+    ensures Tree(t, vs) &*& t == p_(vs);
+{ open Tree(_, _); }
 
-fixpoint tree_ getleft(tree_ t) {
-    switch (t) {
-        case empty: return empty;
-        case node(l, p, v, r): return l;
-    }
-}
-
-fixpoint tree_ getright(tree_ t) {
-    switch (t) {
-        case empty: return empty;
-        case node(l, p, v, r): return r;
-    }
-}
-
-fixpoint int getvalue(tree_ t) {
-    switch (t) {
-        case empty: return 0;
-        case node(l, p, v, r): return v;
-    }
-}
-
-
-fixpoint Tree getptr(tree_ t) {
-    switch (t) {
-        case empty: return 0;
-        case node(l, p, v, r): return p;
-    }
-}
-
-typedef lemma void fill_hole_lemma(predicate() pred, Tree t, tree_ vs, Tree pp, Tree r)();
+typedef lemma void fill_hole_lemma(predicate() pred, Tree t, tree vs, Tree pp, Tree r)();
     requires pred() &*& pp->left |-> r;
     ensures Tree(t, delete_min(vs));
 
-fixpoint int min_value(tree_ vs) {
-    switch (vs) {
-        case empty: return 0;
-        case node(t1, p, v, t2): return t1 == empty ? v : min_value(t1);
-    }
-}
-
-fixpoint bool ordered_between(int min, tree_ t, int max) {
+fixpoint bool sorted_between(int min, tree t, int max) {
     switch (t) {
         case empty: return true;
-        case node(t1, p, v, t2): return min <= v && v <= max && ordered_between(min, t1, v) && ordered_between(v, t2, max);
+        case node(p, t1, v, t2): return min <= v && v <= max && sorted_between(min, t1, v) && sorted_between(v, t2, max);
     }
 }
 
-lemma void ordered_between_weaken(int min0, int min1, tree_ t, int max0, int max1)
-    requires ordered_between(min1, t, max0) == true &*& min0 <= min1 &*& max0 <= max1;
-    ensures ordered_between(min0, t, max1) == true;
+lemma void sorted_between_weaken(int min0, int min1, tree t, int max0, int max1)
+    requires sorted_between(min1, t, max0) == true &*& min0 <= min1 &*& max0 <= max1;
+    ensures sorted_between(min0, t, max1) == true;
 {
     switch (t) {
         case empty:
-        case node(t1, p, v, t2):
-            ordered_between_weaken(min0, min1, t1, v, v);
-            ordered_between_weaken(v, v, t2, max0, max1);
+        case node(p, t1, v, t2):
+            sorted_between_weaken(min0, min1, t1, v, v);
+            sorted_between_weaken(v, v, t2, max0, max1);
     }
 }
 
-lemma_auto void delete_min_ordered_between(int min, tree_ t, int max)
-    requires ordered_between(min, t, max) == true;
-    ensures ordered_between(min, delete_min(t), max) == true;
+lemma_auto void delete_min_sorted_between(int min, tree t, int max)
+    requires sorted_between(min, t, max) == true;
+    ensures sorted_between(min, delete_min(t), max) == true;
 {
     switch (t) {
         case empty:
-        case node(t1, p, v, t2):
+        case node(p, t1, v, t2):
             if (t1 != empty) {
-                delete_min_ordered_between(min, t1, v);
+                delete_min_sorted_between(min, t1, v);
             } else {
-                ordered_between_weaken(min, v, t2, max, max);
+                sorted_between_weaken(min, v, t2, max, max);
             }
     }
 }
 
+fixpoint bool sorted(tree t) { return sorted_between(INT_MIN, t, INT_MAX); }
 @*/
 
 void search_tree_delete_min(Tree t, Tree *r1, int *r2)
-   //@ requires Tree(t, ?vs) &*& vs != empty &*& pointer(r1, _) &*& integer(r2, _) &*& ordered_between(INT_MIN, vs, INT_MAX) == true;
-   //@ ensures pointer(r1, ?tresult) &*& Tree(tresult, delete_min(vs)) &*& integer(r2, min_value(vs)) &*& ordered_between(INT_MIN, delete_min(vs), INT_MAX) == true;
+    //@ requires Tree(t, ?vs) &*& vs != empty &*& *r1 |-> _ &*& *r2 |-> _ &*& sorted(vs) == true;
+    //@ ensures *r1 |-> ?tresult &*& Tree(tresult, delete_min(vs)) &*& *r2 |-> min_value(vs) &*& sorted(delete_min(vs)) == true;
 {
-   Tree tt, pp, p;
-   int m;
-   //@ open Tree(_, _);
-   p = t->left;
-   if (p == 0) {
-       //@ open Tree(0, _);
-       m = t->data; tt = t->right; free (t); t = tt;
-   } else {
-       pp = t; tt = p->left;
-       //while (tt != 0)
-       for (;;)
-           //@ requires Tree(pp, ?vs_) &*& pp != 0 &*& getptr(getleft(vs_)) == p &*& p != 0 &*& getptr(getleft(getleft(vs_))) == tt;
-           //@ ensures is_fill_hole_lemma(_, ?pred, old_pp, vs_, pp, ?r) &*& pred() &*& pp->left |-> p &*& p->left |-> _ &*& p->data |-> min_value(vs_) &*& p->right |-> r &*& malloc_block_tree(p);
-       {
-           //@ open Tree(pp, _);
-           //@ open Tree(p, _);
-           //@ open Tree(tt, _);
-           if (tt == 0) {
-               {
-                   /*@
-                   predicate P() =
-                       pp->data |-> getvalue(vs_) &*&
-                       pp->right |-> getptr(getright(vs_)) &*& Tree(getptr(getright(vs_)), getright(vs_)) &*& 
-                       malloc_block_tree(pp) &*&
-                       Tree(getptr(getright(getleft(vs_))), getright(getleft(vs_)));
-                   lemma void lem()
-                       requires P() &*& pp->left |-> getptr(getright(getleft(vs_)));
-                       ensures Tree(pp, delete_min(vs_));
-                   {
-                       open P();
-                   }
-                   @*/
-                   //@ produce_lemma_function_pointer_chunk(lem) : fill_hole_lemma(P, pp, vs_, pp, getptr(getright(getleft(vs_))))() { call(); };
-                   //@ close P();
-               }
-               break;
-           }
-           //@ Tree oldpp = pp;
-           pp = p; p = tt; tt = p->left;
-           //@ recursive_call();
-           //@ assert is_fill_hole_lemma(?lem0, ?P0, _, _, ?pp_, ?r);
-           /*@
-           {
-               predicate P() =
-                   oldpp->data |-> getvalue(vs_) &*&
-                   oldpp->left |-> getptr(getleft(vs_)) &*&
-                   oldpp->right |-> getptr(getright(vs_)) &*& Tree(getptr(getright(vs_)), getright(vs_)) &*&
-                   malloc_block_tree(oldpp) &*&
-                   is_fill_hole_lemma(lem0, P0, getptr(getleft(vs_)), getleft(vs_), pp_, r) &*&
-                   P0();
-               lemma void lem()
-                   requires P() &*& pp->left |-> r;
-                   ensures Tree(oldpp, delete_min(vs_));
-               {
-                   open P();
-                   lem0();
-                   leak is_fill_hole_lemma(_, _, _, _, _, _);
-               }
-               produce_lemma_function_pointer_chunk(lem) : fill_hole_lemma(P, oldpp, vs_, pp_, r)() { call(); };
-               close P();
-           }
-           @*/
-       }
-       m = p->data; tt = p->right; free(p); pp->left= tt;
-       //@ assert is_fill_hole_lemma(?theLemma, _, _, _, _, _);
-       //@ theLemma();
-       //@ leak is_fill_hole_lemma(_, _, _, _, _, _);
-   }
-   //return (t,m);
-   *r1 = t;
-   *r2 = m;
+    //@ open Tree(_, _);
+    if (t->left == 0) { /*@ open Tree(0, _); @*/ *r2 = t->data; *r1 = t->right; free(t); } else {
+        Tree pp = t; Tree p = pp->left; Tree tt = p->left;
+        for (;;)
+            /*@ requires Tree(pp, ?vs_) &*& vs_ == node(pp, ?lvs, ?v, ?rvs) &*&
+                    lvs == node(p, ?llvs, _, ?rlvs) &*& tt == p_(llvs); @*/
+            /*@ ensures p->left |-> _ &*& p->data |-> min_value(vs_) &*& p->right |-> ?r &*&
+                    malloc_block_tree(p) &*&
+                    is_fill_hole_lemma(_, ?pred, old_pp, vs_, pp, r) &*& pred() &*& pp->left |-> p; @*/
+        {
+            //@ { open Tree(pp, _); open Tree(p, _); open Tree(tt, _); }
+            if (tt == 0) {
+                /*@ {
+                    predicate P() = Tree_r(pp, v, rvs) &*& Tree(p_(rlvs), rlvs);
+                    lemma void lem()
+                        requires P() &*& pp->left |-> p_(rlvs);
+                        ensures Tree(pp, delete_min(vs_));
+                    { open P(); }
+                    produce_lemma_function_pointer_chunk(lem) : fill_hole_lemma(P, pp, vs_, pp, p_(rlvs))()
+                    { call(); };
+                    close P();
+                } @*/
+                break;
+            }
+            pp = p; p = tt; tt = p->left;
+            //@ recursive_call();
+            //@ assert is_fill_hole_lemma(?lem0, ?P0, _, _, ?pp_, ?r);
+            /*@ {
+                predicate P() =
+                    Tree_r(old_pp, v, rvs) &*& old_pp->left |-> old_p &*&
+                    is_fill_hole_lemma(lem0, P0, p_(lvs), lvs, pp_, r) &*& P0();
+                lemma void lem()
+                    requires P() &*& pp->left |-> r;
+                    ensures Tree(old_pp, delete_min(vs_));
+                {
+                    open P();
+                    lem0();
+                    leak is_fill_hole_lemma(_, _, _, _, _, _);
+                }
+                produce_lemma_function_pointer_chunk(lem) : fill_hole_lemma(P, old_pp, vs_, pp_, r)()
+                { call(); };
+                close P();
+            } @*/
+        }
+        *r1 = t; *r2 = p->data; tt = p->right; free(p); pp->left = tt;
+        //@ assert is_fill_hole_lemma(?lem, _, _, _, _, _);
+        //@ lem();
+        //@ leak is_fill_hole_lemma(_, _, _, _, _, _);
+    }
 }
