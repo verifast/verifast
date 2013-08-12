4cd7e104a949b70b23eadf7778eae2bb8710d6cf
Willem Penninckx <willem.penninckx@cs.kuleuven.be>
2013-08-12 07:25:30 +0000

    Forgot to commit usbkbd/src/compiler.h. Added more to .gitignore.

---
+++ examples/usbkbd/src/compiler.h
@@ -0,0 +1,17 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+
+/**
+ * Compiler.h
+ * 
+ * In Linux this file also includes e.g. compile-gcc.h for
+ * macros that need to be defined depending on the compiler.
+ *
+ * In this file, you find the VeriFast equivalent of these
+ * macro definitions.
+ */
+
+/* Does not make a difference for VeriFast, so just empty */
+#define inline
+
+#endif
