aa867d6c07d776a0d9e55c10003b106e07d053f6
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2013-09-17 09:07:48 +0000

    - The Makefile for nmake on Windows/MSVC now builds (the OCaml part of) the Java frontend. (Quick hack, e.g. incomplete dependency info; TODO: clean up, or even better: switch to new build system) - Minor improvements in OriginalGoal.java (uses more frontend goodness)

--- examples/java/frontend/OriginalGoal.java
+++ examples/java/frontend/OriginalGoal.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 /*@
 
-fixpoint int sumize(list<int> l){
+fixpoint int sum(list<int> l){
    switch(l) {
      case nil : return 0;
-     case cons(i, r) : return i + sumize(r);
+     case cons(i, r) : return i + sum(r);
    }
 }
 
@@ -25,7 +25,7 @@ predicate_family FoldFunc(Class c)(FoldFunc f, list<Object> in, Object acc, any 
 interface FoldFunc<A, T> 
 {    
   T fold(T x0, A x1);  
-    //@ requires FoldFunc(this.getClass())(this, cons(?o, ?r), x0, ?info) &*& o == x1;
+    //@ requires FoldFunc(this.getClass())(this, cons(x1, ?r), x0, ?info);
     //@ ensures FoldFunc(this.getClass())(this, r, result, info);
 }
 
@@ -37,7 +37,7 @@ class EmptyException extends Exception
   
 predicate_family_instance FoldFunc(OriginalGoal$1.class)(FoldFunc f, list<Integer> in, Integer acc, list<Integer> info) =
          not_null(in) == true &*& acc != null &*&
-         sumize(map(Integer_intValue, info)) == sumize(map(Integer_intValue, in)) + Integer_intValue(acc); 
+         sum(map(Integer_intValue, info)) == sum(map(Integer_intValue, in)) + Integer_intValue(acc); 
   
 @*/
 
@@ -100,7 +100,7 @@ public class OriginalGoal
     FoldFunc func = new FoldFunc<Integer, Integer>() 
     {
       public Integer fold(Integer x0, Integer x1) 
-        //@ requires FoldFunc(OriginalGoal$1.class)(this, cons(?o, ?r), x0, ?info) &*& o == x1;
+        //@ requires FoldFunc(OriginalGoal$1.class)(this, cons(x1, ?r), x0, ?info);
         //@ ensures FoldFunc(OriginalGoal$1.class)(this, r, result, info);
       { 
         //@ open FoldFunc(OriginalGoal$1.class)(this, cons(?_o, ?_r), x0, ?_info);
@@ -113,14 +113,14 @@ public class OriginalGoal
     //@ close FoldFunc(OriginalGoal$1.class)(func, exs, acc, cons(acc, exs));
     Integer vi = fold(func, xs, acc);
     //@ open FoldFunc(OriginalGoal$1.class)(_, _, _, _);
-    int v = vi.intValue();
+    int v = vi;
     
     //@ assert v == 17;
     
     //@ boolean is_thrown = false;
     try
     {
-      addAll(xs, new Integer[]{});
+      addAll(xs);
     }
     catch (EmptyException e)
     {
