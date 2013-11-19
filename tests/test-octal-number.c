b165326936a6c1a5d5e69ba5d965a5898cbde7a8
Willem Penninckx <willem.penninckx@cs.kuleuven.be>
2013-11-19 16:03:13 +0000

    Forgot to add the test file.

---
+++ tests/test-octal-number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+//@ requires true;
+//@ ensures true;
+{
+	if (0666 == 666){
+	  //@ assert(false); // Cannot happen, 0666 is octal.
+	}
+	
+	//@ assert 0666 != 666;
+	//@ assert 0666 == 438;
+	//@ assert 01 == 1;
+	//@ assert 001 == 1;
+	//@ assert 010 == 8;
+	//@ assert 012345670 == 2739128;
+}
