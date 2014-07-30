106bfcd62df226fde97f60883118b2c416537586
Willem Penninckx <willem.penninckx@cs.kuleuven.be>
2014-07-30 15:14:25 +0000

    Small IO example: specify main's contract by the launcher of VeriFast.
    This is just so that I can remove something from my TODO list.

---
+++ examples/io/user-sets-contract/untrusted_implementation.c
@@ -0,0 +1,25 @@
+/**
+ * If the contract you want to verify against
+ * is in the untrusted .c file, you might
+ * actually verify against the wrong contract.
+ * So there must be a way to say what the contract
+ * is, without trusting the .c file refers to the
+ * correct contract.
+ *
+ * This example illustrates how you can set
+ * the contract separately. See verify.mysh
+ * to see how to invoke VeriFast.
+ */
+
+#include <stdio_simple.h>
+#include "specification.h"
+//#include <stdio.h>
+
+void main()
+//@ requires time(?t1) &*& example_io(t1, ?t2);
+//@ ensures time(t2);
+{
+  //@ open example_io(_, _);
+  putchar('h');
+  putchar('i');
+}
