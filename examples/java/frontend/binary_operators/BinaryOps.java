982a2bbbf6e66d6205724ab950753169f17c053e
Gijs Vanspauwen <gijs.vanspauwen@cs.kuleuven.be>
2014-08-27 10:57:56 +0000

    Added missing file

---
+++ examples/java/frontend/binary_operators/BinaryOps.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class BinaryOps
+{
+  public static void main(String[] args)
+    //@ requires true;
+    //@ ensures true;
+  {
+     int a = 60; /* 60 = 0011 1100 */  
+     int b = 13;  /* 13 = 0000 1101 */
+     int c = 0;
+
+     c = a & b;
+     c = a | b;
+     c = a ^ b;
+     c = ~a;  
+     //@ assert c == 0; 
+     c = a << 2;
+     c = a >> 2;
+  }
+}
+
