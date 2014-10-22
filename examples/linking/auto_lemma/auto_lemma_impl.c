7de1d94cc34af481f0dbc8e089283f5274285335
Gijs Vanspauwen <gijs.vanspauwen@cs.kuleuven.be>
2014-10-22 13:14:23 +0000

    Improved linking of auto lemmas

---
+++ examples/linking/auto_lemma/auto_lemma_impl.c
@@ -0,0 +1,11 @@
+#include "auto_lemma_header.h"
+
+/*@
+
+lemma_auto void auto_lemma()
+  requires true;
+  ensures  true;
+{
+}
+
+@*/
---
+++ examples/linking/auto_lemma/main.c
@@ -0,0 +1,9 @@
+#include "auto_lemma_header.h"
+
+int main() //@ : main
+  //@ requires true;
+  //@ ensures  true;
+{
+  //@ auto_lemma();
+  return 0;
+}
