afb49cba7dcdc90e4ec856fe39621a0e37adce91
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2014-11-07 14:31:06 +0000

    Fixed a bug in the type checking of switch statements

--- src/verifast.ml
+++ src/verifast.ml
@@ -556,8 +556,8 @@ module VerifyProgram(VerifyProgramArgs: VERIFY_PROGRAM_ARGS) = struct
       let tcont _ _ _ h env = tcont sizemap tenv ghostenv h (List.filter (fun (x, _) -> List.mem_assoc x tenv) env) in
       begin match tp with
         InductiveType (i, targs) ->
-        let (tn, targs, Some (_, tparams, ctormap, _)) = (i, targs, try_assoc' Ghost (pn,ilist) i inductivemap) in
-        let (Some tpenv) = zip tparams targs in
+        let (tn, targs, Some (_, itparams, ctormap, _)) = (i, targs, try_assoc' Ghost (pn,ilist) i inductivemap) in
+        let (Some tpenv) = zip itparams targs in
         let rec iter ctors cs =
           match cs with
             [] ->
