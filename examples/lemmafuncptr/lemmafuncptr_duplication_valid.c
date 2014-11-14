938a4a0c44d8071f2fc4103feb440cc48d6043e8
Gijs Vanspauwen <gijs.vanspauwen@cs.kuleuven.be>
2014-11-14 15:22:02 +0000

    Duplication of lemma_function_pointer_chuncks now also works for lemma typedefs with type parameters

--- examples/lemmafuncptr/lemmafuncptr_duplication_valid.c
+++ examples/lemmafuncptr/lemmafuncptr_duplication_valid.c
@@ -12,25 +12,67 @@ typedef lemma void lemma_type4(fixpoint(bool, int) targ1, fixpoint(bool, int) ta
                               (bool arg1, int arg2);
   requires true;
   ensures  true;
-  
+typedef lemma void lemma_type5<T>(fixpoint(T, int) targ)(T arg);
+  requires true;
+  ensures  true;
+
 lemma void test1() nonghost_callers_only
   requires [?f]is_lemma_type1(?lem);
-  ensures [f]is_lemma_type1(lem) &*& is_lemma_type1(lem);
+  ensures  [f]is_lemma_type1(lem) &*& is_lemma_type1(lem);
 {
   duplicate_lemma_function_pointer_chunk(lemma_type1);
+  assert [f]is_lemma_type1(lem) &*& is_lemma_type1(lem);
+}
+
+lemma void test2() nonghost_callers_only
+  requires [?f]is_lemma_type2(?lem);
+  ensures  [f]is_lemma_type2(lem) &*& is_lemma_type2(lem);
+{
+  duplicate_lemma_function_pointer_chunk(lemma_type2);
+  assert [f]is_lemma_type2(lem) &*& is_lemma_type2(lem);
+}
+
+lemma void test3() nonghost_callers_only
+  requires [?f]is_lemma_type3(?lem);
+  ensures  [f]is_lemma_type3(lem) &*& is_lemma_type3(lem);
+{
+  duplicate_lemma_function_pointer_chunk(lemma_type3);
+  assert [f]is_lemma_type3(lem) &*& is_lemma_type3(lem);
+}
+
+lemma void test4() nonghost_callers_only
+  requires [?f]is_lemma_type4(?targ1, ?targ2, ?lem);
+  ensures  [f]is_lemma_type4(targ1, targ2, lem) &*& 
+           is_lemma_type4(targ1, targ2, lem);
+{
+  duplicate_lemma_function_pointer_chunk(lemma_type4);
+  assert [f]is_lemma_type4(targ1, targ2, lem) &*& 
+         is_lemma_type4(targ1, targ2, lem);
+}
+
+lemma void test5<T>() nonghost_callers_only
+  requires [?f]is_lemma_type5<T>(?targ, ?lem);
+  ensures  [f]is_lemma_type5<T>(targ, lem) &*& 
+           is_lemma_type5<T>(targ, lem);
+{
+  duplicate_lemma_function_pointer_chunk(lemma_type5);
+  assert [f]is_lemma_type5<T>(targ, lem) &*& 
+         is_lemma_type5<T>(targ, lem);
 }
 
 @*/
 
-void test2()
+void test_all()
   /*@ requires [?f1]is_lemma_type1(?lem1) &*& 
                [?f2]is_lemma_type2(?lem2) &*& 
                [?f3]is_lemma_type3(?lem3) &*& 
-               [?f4]is_lemma_type4(?targ1, ?targ2, ?lem4); @*/
+               [?f4]is_lemma_type4(?targ1, ?targ2, ?lem4) &*&
+               [?f5]is_lemma_type5(?targ3, ?lem5); @*/
   /*@ ensures  [f1]is_lemma_type1(lem1) &*& is_lemma_type1(lem1) &*& 
                [f2]is_lemma_type2(lem2) &*& is_lemma_type2(lem2) &*& 
                [f3]is_lemma_type3(lem3) &*& is_lemma_type3(lem3) &*&
-               [f4]is_lemma_type4(targ1, targ2, lem4) &*& is_lemma_type4(targ1, targ2, lem4); @*/
+               [f4]is_lemma_type4(targ1, targ2, lem4) &*& is_lemma_type4(targ1, targ2, lem4) &*&
+               [f5]is_lemma_type5(targ3, lem5) &*& is_lemma_type5(targ3, lem5); @*/
 {
   //@ duplicate_lemma_function_pointer_chunk(lemma_type1);
   //@ assert [f1]is_lemma_type1(lem1) &*& is_lemma_type1(lem1);
@@ -40,5 +82,7 @@ void test2()
   //@ assert [f3]is_lemma_type3(lem3) &*& is_lemma_type3(lem3);
   //@ duplicate_lemma_function_pointer_chunk(lemma_type4);
   //@ assert [f4]is_lemma_type4(targ1, targ2, lem4) &*& is_lemma_type4(targ1, targ2, lem4);
+  //@ duplicate_lemma_function_pointer_chunk(lemma_type5);
+  //@ assert [f5]is_lemma_type5(targ3, lem5) &*& is_lemma_type5(targ3, lem5);
 }
 
