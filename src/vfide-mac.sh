d312a855c0f1eae6f2466aab9481e6cd071a569a
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2015-05-04 20:11:53 +0000

    Fixed a bug in the OS X build setup that caused vfide on OS X to be unusable in the 14.12 and 15.04 releases.

--- src/vfide-mac.sh
+++ src/vfide-mac.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-MYRELDIR=`dirname $0`
+MYRELDIR=`dirname "$0"`
 MYABSDIR=`cd $MYRELDIR; pwd`
-export DYLD_LIBRARY_PATH=$MYABSDIR/../lib:$DYLD_LIBRARY_PATH
-export GDK_PIXBUF_MODULE_FILE=$MYABSDIR/../etc/gtk-2.0/gdk-pixbuf.loaders
+export DYLD_LIBRARY_PATH="$MYABSDIR/../lib:$DYLD_LIBRARY_PATH"
+export GDK_PIXBUF_MODULE_FILE="$MYABSDIR/../lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
+export PANGO_LIBDIR="$MYABSDIR/../lib"
+export PANGO_SYSCONFDIR="$MYABSDIR/../etc"
 "$MYABSDIR/vfide-core" "$@"
