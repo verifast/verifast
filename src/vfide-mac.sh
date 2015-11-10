3b372384edd3e73de414ab5f9c6f0c7ef755e2a9
Willem Penninckx <willem.penninckx@cs.kuleuven.be>
2015-11-10 10:00:48 +0000

    Fixed: vfide did not start on OsX if directory name had a space. Bug found by Mahmoud. Thanks to Mahmoud for testing.

--- src/vfide-mac.sh
+++ src/vfide-mac.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MYRELDIR=`dirname "$0"`
-MYABSDIR=`cd $MYRELDIR; pwd`
+MYABSDIR=`cd "$MYRELDIR"; pwd`
 export DYLD_LIBRARY_PATH="$MYABSDIR/../lib:$DYLD_LIBRARY_PATH"
 export GDK_PIXBUF_MODULE_FILE="$MYABSDIR/../lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
 export PANGO_LIBDIR="$MYABSDIR/../lib"
