a82318c7a27a9e3e221f9f6513249463f4a2a994
Willem Penninckx <willem.penninckx@cs.kuleuven.be>
2015-12-15 09:56:11 +0000

    Merge branch 'split-invar'
    1. Ghost critical sections: split/join must (and now can) go through
       an invariant update, disallowing split/joins the invariant does
       not approve
    2. Introduced "alive-tracking" of tokens (to support "1." above)
    3. Tokens now also embed/track their parents
    4. Places now also embed/track their parents
    5. Places now also track their io-actions (instead of only user-defined,
       and therefore optional, progress).
    6. "ported" the core idea of framepresupd: now there is also
       an opaque token and invariant. The approach can now be considered
       as more or less a merge of two previous approaches (with some extra ideas).
    7. Removed the (actually broken) constraint about which places are joinable, because
       it turned out to be not necessary
    8. Removed 1,2,3 from above because 6 solves the problems
       they solve. In case one wants to write an invariant that uses
       information about which splits/joins have happened, this is
       not as straightforward anymore, but I don't know if you often
       need this since you can use information about progress of
       the ghost threads (i.e. use "if both branches have enough progress"
       instead of "if both bracnhes are joined"). Alternatively you can
       write a custom join on top of the provided join that tracks
       joins in the progress (untested).

--- examples/io/work-in-progress/in-memory-io-style-v3/04-split-invar/buffer.c
+++ examples/io/work-in-progress/in-memory-io-style-v3/04-split-invar/buffer.c
@@ -1,5 +1,7 @@
 /**
  * "split-invar" approach.
+ *
+ * TODO THIS IS A DRAFT PROTOTYPE!
  * 
  * Nice properties:
  * + Supports split and join (contrary to the "weak" approach)
@@ -8,17 +10,21 @@
  * + threads can (but do not have to) have knowledge what the other threads
  *   do, e.g. a reader can know it will never read a negative number because
  *   the writer never writes one (like weak, contrary to framepresupd)
- * + nonterminating programs supported
  * + reading works (using prophecies)
  * + underspecification should just work (but untested)
+ * + invariant, which links together the ghost-threads, does not have to be complex
+ *   (contrary to e.g. IGR)
  *
  * TODO
  * - compositionaly: just works if they all share the same invariant, otherwise TODO
+ * - support nonterminating programs (closing copredicates)
+ * - make parts expected to be generic, generic
+ * - more examples to get a better feeling with this approach and showcase more
+ * - cleanup
  *
  * Issues:
  * - as with all in-memory split/join: high annotation overhead
  * - lemma function pointer chunks are awkward to use
- * - too many "leak"-annotations necessary.
  * 
  * This is an example on a circular buffer.
  */
@@ -26,7 +32,6 @@
 
 #include <malloc.h>
 #include <stdlib.h> // abort()
-//@ #include <ghost_cells.gh>
 //@ #include "prophecy.gh"
 
 #include "../../../../vstte2012/problem3/problem3.h" // Ring buffer
@@ -65,15 +70,17 @@ struct buffer *create_buffer(int size)
 /*@ requires
   size > 0 &*& size * sizeof(int) < INT_MAX &*& create_buffer_ghost_arg(?family, ?the_invariant)
   &*& [1/2]progress(id_init(family), nil)
-  &*& the_invariant(family, nil);
+  &*& the_invariant(family, nil)
+  &*& token_opaque(place_init(family));
 @*/
 /*@ ensures
   result == 0 ?
     [1/2]progress(id_init(family), nil)
     &*& the_invariant(family, nil)
+    &*& token_opaque(place_init(family))
   :
     buffer(family, the_invariant, result)
-    &*& token(place(id_init(family), nil));
+    &*& token(place(id_init(family), no_place, no_place, nil, nil));
 @*/
 {
   //@ open create_buffer_ghost_arg(_, _);
@@ -97,18 +104,16 @@ struct buffer *create_buffer(int size)
 
   return buffer;
   //@ close buffer(family, the_invariant, buffer);
-  //@ close token(place(id_init(family), nil));
+  //@ close token(place(id_init(family), no_place, no_place, nil, nil));
 }
 
-
 /*@
-
 fixpoint bool push_io_fp(int c, list<int> input, list<int> output) {
   return output == append(input, {c});
 }
 
 predicate push_io(place t1, int c, int family, predicate(int, list<int>) the_invariant, place t2) =
-  t2 == place(place_id(t1), append(place_progress(t1), {c}))
+  t2 == place_io(t1, (push_io_fp)(c), append(place_progress(t1), {c}))
   // There is a proof that the invariant is updatadble
   &*& is_invar_updatable(?f, (push_io_fp)(c), t1, the_invariant, t2)
   &*& family == id_family(place_id(t1))
@@ -165,7 +170,6 @@ void push(struct buffer *buffer, int x)
   //@ assert ring_buffer(_, _, ?new_contents);
   //@ close exists(new_contents);
   //@ invar_updater();
-  
   //@ close token(t2);
   //@ close buffer_protected(family, the_invariant, buffer)();
   mutex_release(buffer->mutex);
@@ -173,14 +177,13 @@ void push(struct buffer *buffer, int x)
   //@ leak is_invar_updatable(invar_updater, _, _, _, _);
 }
 
-
 /*@
 fixpoint bool pop_io_fp(int c, list<int> input, list<int> output) {
   return output == tail(input) && c == head(input);
 }
 
 predicate pop_io(place t1, int c, int family, predicate(int, list<int>) the_invariant, place t2) =
-  t2 == place(place_id(t1), tail(place_progress(t1)))
+  t2 == place_io(t1, (pop_io_fp)(c), tail(place_progress(t1)))
   // There is a proof that the invariant is updatadble
   &*& is_invar_updatable(?f, (pop_io_fp)(c), t1, the_invariant, t2)
   &*& family == id_family(place_id(t1))
@@ -191,7 +194,7 @@ predicate pop_io(place t1, int c, int family, predicate(int, list<int>) the_inva
 typedef lemma void read_proof_pop(int family, predicate(int, list<int>) the_invariant, int c, fixpoint(int, list<int>, list<int>, bool) fp, predicate() p, place t1)();
   requires the_invariant(family, ?contents) &*& contents != nil
     &*& pop_io(t1, c, family, the_invariant, ?t2);
-  ensures the_invariant(family, contents)
+  ensures the_invariant(family, contents) // TODO: allow content change to allow more invariants?
     &*& pop_io(t1, c, family, the_invariant, t2)
     &*& p();
 @*/
@@ -263,7 +266,8 @@ int pop(struct buffer *buffer)
 
 /*@
 predicate example_invariant(int family, list<int> contents) =
-  [1/2]progress(id_init(family), ?progress_before_split)
+  invar_opaque(family, contents)
+  &*& [1/2]progress(id_init(family), ?progress)
 ;
 @*/
 
@@ -279,68 +283,50 @@ void write_12(struct buffer *buffer)
   push(buffer, 2);
 }
 
-/*@
-
-typedef lemma void invar_updatable(fixpoint (list<int>, list<int>, bool) fp, place t1, predicate(int, list<int>) the_invariant, place t2)();
- requires
-    the_invariant(id_family(place_id(t1)), ?contents_in)
-    &*& exists<list<int> >(?contents_out)
-    &*& true==fp(contents_in, contents_out)
-    &*& [1/2]progress(place_id(t1), place_progress(t1));
-  ensures the_invariant(id_family(place_id(t1)), contents_out) &*& [1/2]progress(place_id(t2), place_progress(t2));
-
-// To create semi-anonymous lemmas using "produce_lemma_function_pointer_chunk(empty_lemma): invar_updatable(....)(){...}"
-lemma void empty_lemma()
-  requires true;
-  ensures true;
-{
-}
-@*/
-
 
 
 void write_12_and_build_io_contract()
 //@ requires true;
 //@ ensures true;
 {
-  //@ int family = create_initial_progress();
-  //@ create_progress(id_init(family));
+  //@ int family = create_initial_progress(nil);
   //@ close example_invariant(family, nil);
   //@ close create_buffer_ghost_arg(family, example_invariant);
   struct buffer *buffer = create_buffer(10);
   if (buffer == 0){ abort (); }
   //@ assert token(?t1);
-  /*@ produce_lemma_function_pointer_chunk(empty_lemma) : invar_updatable((push_io_fp)(1), place(id_init(family), {}), example_invariant, place(id_init(family), {1}))() {
-    open example_invariant(_, _);
+  //@ place t2 = place_io(t1, (push_io_fp)(1), {1});
+  /*@ produce_lemma_function_pointer_chunk(empty_lemma) : invar_updatable((push_io_fp)(1), t1, example_invariant, t2)() {
+    open example_invariant(_, ?state);
     assert exists<list<int> >(?out);
     update_progress(id_init(family), {1});
+    io_opaque(family, t1, (push_io_fp)(1), append(state, {1}), {1});
     close example_invariant(family, out);
     call ();
   };
   @*/
-  //@ close push_io(t1, 1, family, example_invariant, ?t2);
-  /*@ produce_lemma_function_pointer_chunk(empty_lemma) : invar_updatable((push_io_fp)(2), place(id_init(family), {1}), example_invariant, place(id_init(family), {1,2}))() {
-    open example_invariant(_, _);
+  //@ close push_io(t1, 1, family, example_invariant, t2);
+  //@ place t3 = place_io(t2, (push_io_fp)(2), {1, 2});
+  /*@ produce_lemma_function_pointer_chunk(empty_lemma) : invar_updatable((push_io_fp)(2), t2, example_invariant, t3)() {
+    open example_invariant(_, ?state);
     assert exists<list<int> >(?out);
     update_progress(id_init(family), {1,2});
+    io_opaque(family, t2, (push_io_fp)(2), append(state, {2}), {1,2});
     close example_invariant(family, out);
     call();
   };
   @*/
-  //@ place t3 = place(place_id(t2), {1, 2});
   //@ close push_io(t2, 2, family, example_invariant, t3);
   write_12(buffer);
-  //@ leak buffer(_, _, _); // TODO hmm all this leaking seems to be a drawback of this approach
-  //@ leak can_create_progress(_);
-  //@ leak can_create_progress(_);
-  //@ leak can_create_progress(_);
-  //@ leak token(_);
+  //@ leak buffer(_, _, _);
+  //@ leak can_create_progress(_, _);
+  //@ leak token(t3);
 }
 
 
 //@ predicate_ctor positive(int c)() = c >= 0;
 /**
- * pop but crashes is negative number popped. Verified to never crash. Uses knowledge of the invariant.
+ * pop but crashes if negative number popped. Verified to never crash. Uses knowledge of the invariant.
  */
 int pop_positive(struct buffer *buffer)
 /*@
@@ -380,14 +366,15 @@ ensures [f]buffer(family, example_split_invariant, buffer) &*& token(t2);
 
 void split_write_1212_read(struct buffer *buffer)
 /*@ requires [?f]buffer(?family, example_split_invariant, buffer)
-  &*& token(?t1) &*& split(t1, ?ta_1, ?tb_1)
+  &*& token(?t1) &*& split(family, example_split_invariant, buffer, t1, ?ta_1, ?tb_1)
   &*& push_io(ta_1, 1, family, example_split_invariant, ?ta_2) &*& push_io(ta_2, 2, family, example_split_invariant, ?ta_3) // can be put in one predicate
   &*& push_io(tb_1, 1, family, example_split_invariant, ?tb_2) &*& push_io(tb_2, 2, family, example_split_invariant, ?tb_3)
-  &*& join(ta_3, tb_3, ?t2)
+  &*& join(family, example_split_invariant, buffer, ta_3, tb_3, ?t2)
   &*& pop_io(t2, ?c, family, example_split_invariant, ?t3) // note: only here knowledge that it is this invariant is necessary
   ;
 @*/
-/*@ ensures [f]buffer(family, example_split_invariant, buffer)
+/*@ ensures
+  [f]buffer(family, example_split_invariant, buffer)
   &*& token(t3);
 @*/
 {
@@ -404,14 +391,14 @@ fixpoint bool lte(int v1, int v2){
 }
 
 predicate example_split_invariant(int family, list<int> contents) =
-  [1/2]progress(id_init(family), ?progress_before_split)
+  invar_opaque(family, contents)
+  &*& [1/2]progress(id_init(family), ?progress_before_split)
   &*& [1/2]progress(id_split_left(id_init(family)), ?progress_left)
   &*& [1/2]progress(id_split_right(id_init(family)), ?progress_right)
-  &*& [1/2]progress(id_join(id_init(family)), ?progress_join)
-  // If a reader thread expects something, you can guarantee it here.
-  // It is actually not necessary to link contents with progress,
-  // but you can do it when it's useful, e.g. an invariant that only holds
-  // after certain progress.
+  &*& [1/2]progress(id_join(id_split_left(id_init(family)), id_split_right(id_init(family))), ?progress_join)
+  
+  &*& progress_before_split == {}
+  // ... (optional)
   
   // here, reader simply knows all values are greater than or equal to 0.
   &*& true==forall(contents, (lte)(0))
@@ -423,69 +410,72 @@ void split_write_1212_and_build_io_contract()
 //@ requires true;
 //@ ensures true;
 {
-  //@ int family = create_initial_progress();
-  //@ create_progress(id_init(family));
+  //@ int family = create_initial_progress({});
   //@ create_progress(id_split_left(id_init(family)));
   //@ create_progress(id_split_right(id_init(family)));
-  //@ create_progress(id_join(id_init(family)));
+  //@ create_progress(id_join( id_split_left(id_init(family)),  id_split_right(id_init(family))  ));
   //@ close example_split_invariant(family, {});
   //@ close create_buffer_ghost_arg(family, example_split_invariant);
   struct buffer *buffer = create_buffer(10);
   if (buffer == 0){ abort (); }
   //@ assert token(?t1);
-  
-  //@ close split(t1, ?ta_1, ?tb_1);
-  
+  //@ close split(family, example_split_invariant, buffer, t1, ?ta_1, ?tb_1);
+  //@ place ta_2 = place_io(ta_1, (push_io_fp)(1), {1});
   /*@ 
-  produce_lemma_function_pointer_chunk(empty_lemma) : invar_updatable((push_io_fp)(1), place(id_split_left(id_init(family)), {}), example_split_invariant, place(id_split_left(id_init(family)), {1}))() {
+  produce_lemma_function_pointer_chunk(empty_lemma) : invar_updatable((push_io_fp)(1), ta_1, example_split_invariant, ta_2)() {
     open example_split_invariant(_, ?contents);
     assert exists<list<int> >(?out);
     update_progress(id_split_left(id_init(family)), {1});
     forall_append(contents, {1}, (lte)(0));
+    io_opaque(family, ta_1, (push_io_fp)(1), append(contents, {1}), {1});
     close example_split_invariant(family, out);
     call ();
   };
   @*/
-  //@ close push_io(ta_1, 1, family, example_split_invariant, ?ta_2);
+  //@ close push_io(ta_1, 1, family, example_split_invariant, ta_2);
+  //@ place ta_3 = place_io(ta_2, (push_io_fp)(2), {1,2});
   /*@ 
-  produce_lemma_function_pointer_chunk(empty_lemma) : invar_updatable((push_io_fp)(2), place(id_split_left(id_init(family)), {1}), example_split_invariant, place(id_split_left(id_init(family)), {1,2}))() {
+  produce_lemma_function_pointer_chunk(empty_lemma) : invar_updatable((push_io_fp)(2), ta_2, example_split_invariant, ta_3)() {
     open example_split_invariant(_, ?contents);
     assert exists<list<int> >(?out);
     update_progress(id_split_left(id_init(family)), {1,2});
     forall_append(contents, {2}, (lte)(0));
+    io_opaque(family, ta_2, (push_io_fp)(2), append(contents, {2}), {1,2});
     close example_split_invariant(family, out);
     call ();
   };
   @*/
-  //@ close push_io(ta_2, 2, family, example_split_invariant, ?ta_3);
-  
+  //@ close push_io(ta_2, 2, family, example_split_invariant, ta_3);
+  //@ place tb_2 = place_io(tb_1, (push_io_fp)(1), {1});
   /*@ 
-  produce_lemma_function_pointer_chunk(empty_lemma) : invar_updatable((push_io_fp)(1), place(id_split_right(id_init(family)), {}), example_split_invariant, place(id_split_right(id_init(family)), {1}))() {
+  produce_lemma_function_pointer_chunk(empty_lemma) : invar_updatable((push_io_fp)(1), tb_1, example_split_invariant, tb_2)() {
     open example_split_invariant(_, ?contents);
     assert exists<list<int> >(?out);
     update_progress(id_split_right(id_init(family)), {1});
     forall_append(contents, {1}, (lte)(0));
+    io_opaque(family, tb_1, (push_io_fp)(1), append(contents, {1}), {1});
     close example_split_invariant(family, out);
     call ();
   };
   @*/
-  //@ close push_io(tb_1, 1, family, example_split_invariant, ?tb_2);
+  //@ close push_io(tb_1, 1, family, example_split_invariant, tb_2);
+  //@ place tb_3 = place_io(tb_2, (push_io_fp)(2), {1,2});
   /*@ 
-  produce_lemma_function_pointer_chunk(empty_lemma) : invar_updatable((push_io_fp)(2), place(id_split_right(id_init(family)), {1}), example_split_invariant, place(id_split_right(id_init(family)), {1,2}))() {
+  produce_lemma_function_pointer_chunk(empty_lemma) : invar_updatable((push_io_fp)(2), tb_2, example_split_invariant, tb_3)() {
     open example_split_invariant(_, ?contents);
     assert exists<list<int> >(?out);
     update_progress(id_split_right(id_init(family)), {1,2});
     forall_append(contents, {2}, (lte)(0));
+    io_opaque(family, tb_2, (push_io_fp)(2), append(contents, {2}), {1,2});
     close example_split_invariant(family, out);
     call ();
   };
   @*/
-  //@ close push_io(tb_2, 2, family, example_split_invariant, ?tb_3);
-  
-  //@ close join(ta_3, tb_3, ?t2);
+  //@ close push_io(tb_2, 2, family, example_split_invariant, tb_3);
+  //@ close join(family, example_split_invariant, buffer, ta_3, tb_3, ?t2);
   
   //@ int c = prophecy_create(family, example_split_invariant, pop_io_fp);
-  //@ place t3 = t2; // TODO: reading should change progress.
+  //@ place t3 = place_io(t2, (pop_io_fp)(c), {});
   /*@
   produce_lemma_function_pointer_chunk(empty_lemma) : invar_updatable((pop_io_fp)(c), t2, example_split_invariant, t3)() {
     open example_split_invariant(_, ?contents);
@@ -495,6 +485,7 @@ void split_write_1212_and_build_io_contract()
       case nil:
       case cons(ahead, atail):
     }
+    io_opaque(family, t2, (pop_io_fp)(c), tail(contents), {});
     close example_split_invariant(family, out);
     call ();
   };
@@ -503,16 +494,7 @@ void split_write_1212_and_build_io_contract()
   
   split_write_1212_read(buffer);
   
-  //@ leak can_create_progress(_);
-  //@ leak can_create_progress(_);
-  //@ leak can_create_progress(_);
-  //@ leak can_create_progress(_);
-  //@ leak can_create_progress(_);
-  //@ leak can_create_progress(_);
-  //@ leak can_create_progress(_);
-  //@ leak can_create_progress(_);
-  //@ leak can_create_progress(_);
+  //@ leak can_create_progress(_, _);
   //@ leak token(_);
   //@ leak buffer(_, _, _);
 }
-
--- examples/io/work-in-progress/in-memory-io-style-v3/04-split-invar/io.h
+++ examples/io/work-in-progress/in-memory-io-style-v3/04-split-invar/io.h
@@ -1,103 +1,235 @@
 #ifndef IO_H
 #define IO_H
 
-// TODO: I want to put here things that does not have a body (i.e. needs a soundness proof),
-//       or that remains the same always anyways (if any).
-//       Find out what fits here and what not.
+// TODO THIS IS A DRAFT PROTOTYPE
 
 
+struct buffer;
 /*@
+#define trace_t list<fixpoint(list<int>, list<int>, bool)>
+
+// id
 inductive id =
   id_init(int family)
   | id_split_left(id)
   | id_split_right(id)
-  | id_join(id node_before_split)
+  | id_join(id left, id right)
 ;
-
 fixpoint int id_family(id id) {
   switch(id){
     case id_init(family): return family;
     case id_split_left(sub): return id_family(sub);
     case id_split_right(sub): return id_family(sub);
-    case id_join(parent): return id_family(parent);
+    case id_join(parent_left, parent_right): return id_family(parent_left);
   }
 }
-
-fixpoint id id_before_split(id id) {
+fixpoint id id_parent(id id){
   switch(id){
     case id_init(family): return default_value<id>;
     case id_split_left(sub): return sub;
     case id_split_right(sub): return sub;
-    case id_join(parent): return parent;
+    case id_join(parent_left, parent_right): return parent_left;
+  }
+}
+fixpoint id id_parent_right(id id){
+  switch(id){
+    case id_init(family): return default_value<id>;
+    case id_split_left(sub): return sub;
+    case id_split_right(sub): return sub;
+    case id_join(parent_left, parent_right): return parent_right;
   }
 }
 
-inductive place = place(id id, list<int> progress);
+// Place
+inductive place = no_place | place(id id, place parent1, place parent2, trace_t trace, list<int> progress);
+
 fixpoint id place_id(place t1) {
-  switch(t1) { case place(id, progress): return id; }
+  switch(t1) {
+    case no_place(): return default_value<id>;
+    case place(id, parent1, parent2, trace, progress): return id;
+  }
+}
+fixpoint place place_parent1(place t1) {
+  switch(t1) {
+    case no_place(): return default_value<place>;
+    case place(id, parent1, parent2, trace, progress): return parent1;
+  }
+}
+fixpoint place place_parent2(place t1) {
+  switch(t1) {
+    case no_place(): return default_value<place>;
+    case place(id, parent1, parent2, trace, progress): return parent2;
+  }
+}
+fixpoint trace_t place_trace(place t1) {
+  switch(t1) {
+    case no_place(): return default_value<trace_t>;
+    case place(id, parent1, parent2, trace, progress): return trace;
+  }
 }
 fixpoint list<int> place_progress(place t1) {
-  switch(t1) { case place(id, progress): return progress; }
+  switch(t1) {
+    case no_place(): return default_value<list<int> >;
+    case place(id, parent1, parent2, trace, progress): return progress;
+  }
 }
 
-// This has two arguments (instead of one "place" argument),
-// so that [1/2]progress(key, ?value1) &*& [1/2]progress(key, ?value2) implies value1==value2.
+
+// Progress
 predicate progress(id key; list<int> progress);
 
-predicate can_create_progress(id id);
+predicate can_create_progress(int family, list<id> used);
 
-lemma int create_initial_progress();
+lemma int create_initial_progress(list<int> init_state);
   requires true;
-  ensures can_create_progress(id_init(result));
+  ensures
+    progress(id_init(result), nil)
+    &*& can_create_progress(result, {id_init(result)})
+    &*& token_opaque(place_init(result))
+    &*& invar_opaque(result, init_state);
 
 lemma void create_progress(id id);
-  requires can_create_progress(id);
+  requires
+    can_create_progress(?family, ?used)
+    &*& !mem(id, used)
+    &*& id_family(id) == family;
   ensures
     progress(id, nil)
-    &*& can_create_progress(id_split_left(id))
-    &*& can_create_progress(id_split_right(id))
-    &*& can_create_progress(id_join(id));
+    &*& can_create_progress(family, cons(id, used));
 
 lemma void update_progress(id key, list<int> value);
   requires progress(key, _);
   ensures progress(key, value);
 
+// Token
 predicate token(place t1) =
-  [1/2]progress(place_id(t1), place_progress(t1));
+  token_opaque(t1)
+  &*& [1/2]progress(place_id(t1), place_progress(t1));
 
-predicate split(place t1, place t2, place t3) =
-  t2 == place(id_split_left(place_id(t1)), nil) &*&
-  t3 == place(id_split_right(place_id(t1)), nil)
-  &*& [1/2]progress(place_id(t2), nil)
-  &*& [1/2]progress(place_id(t3), nil);
-  
-// TODO: maybe make opaque to avoid construction of "wild joins"?
-predicate join(place t1, place t2, place t3) =
-  id_before_split(place_id(t1)) == id_before_split(place_id(t2))
-  &*& t1 != t2
-  &*& t3 == place(id_join(id_before_split(place_id(t1))), nil)
-  &*& [1/2]progress(place_id(t3), nil);
+// Opaque
+predicate token_opaque(place t1);
+
+predicate invar_opaque(int family, list<int> state);
+
+predicate split_opaque(place t1, place t2, place t3);
+
+lemma void split_opaque(place t1);
+  requires token_opaque(t1);
+  ensures token_opaque(split_place_left(t1)) &*& token_opaque(split_place_right(t1));
+
+predicate join_opaque(place t1, place t2, place t3);
+
+lemma void join_opaque(place t1, place t2);
+  requires token_opaque(t1) &*& token_opaque(t2);
+  ensures
+    token_opaque(join_place(t1, t2));
+
+lemma void io_opaque(int family, place t1, fixpoint(list<int> state1, list<int> state2, bool allowed) io_fp, list<int> state2, list<int> progress2);
+  requires token_opaque(t1) &*& invar_opaque(family, ?state1) &*& true==io_fp(state1, state2);
+  ensures token_opaque(place_io(t1, io_fp, progress2)) &*& invar_opaque(family, state2);
+
+// Split and join
+predicate split(int family, predicate(int, list<int>) the_invariant, struct buffer *buffer, place t1, place t2, place t3) =
+  t2 == split_place_left(t1)
+  &*& t3 == split_place_right(t1)
+  &*& [1/2]progress(place_id(split_place_left(t1)), nil)
+  &*& [1/2]progress(place_id(split_place_right(t1)), nil);
 
 lemma void split(place t1)
-  requires split(t1, ?t2, ?t3) &*& token(t1);
+  requires split(?family, ?buffer, ?the_invariant, t1, ?t2, ?t3) &*& token(t1);
   ensures token(t2) &*& token(t3);
 {
-  leak token(t1);
-  open split(t1, t2, t3);
-  close token(t2);
-  close token(t3);
+  open split(_, _, _, _, _, _);
+  open token(t1);
+  split_opaque(t1);
+  close token(split_place_left(t1));
+  close token(split_place_right(t1));
+  leak [1/2]progress(_, _);
 }
+
+predicate join(int family, predicate(int, list<int>) the_invariant, struct buffer *buffer, place t1, place t2, place t3) =
+  t3 == join_place(t1, t2)
+  &*& [1/2]progress(place_id(join_place(t1, t2)), nil);
 
 lemma void join(place t1)
-  requires join(t1, ?t2, ?t3) &*& token(t1) &*& token(t2);
+  requires join(?family, ?the_invariant, ?buffer, t1, ?t2, ?t3) &*& token(t1) &*& token(t2);
   ensures token(t3);
 {
-  leak token(t1);
-  leak token(t2);
-  open join(t1, t2, t3);
+  open join(_, _, _, _, _, _);
+  open token(t1);
+  open token(t2);
+  join_opaque(t1, t2);
   close token(t3);
+  leak [1/2]progress(_, _);
+  leak [1/2]progress(_, _);
 }
 
+
+// Most I/O actions will require a proof (of this typedef) that the invariant allows the I/O action.
+typedef lemma void invar_updatable(fixpoint (list<int>, list<int>, bool) fp, place t1, predicate(int, list<int>) the_invariant, place t2)();
+ requires
+    the_invariant(id_family(place_id(t1)), ?state1)
+    // Alternative to this exist is to have state2 in the second set of parameters and have that parameter also in empty_lemma.
+    &*& exists<list<int> >(?state2)
+    &*& true==fp(state1, state2)
+    &*& [1/2]progress(place_id(t1), place_progress(t1))
+    &*& token_opaque(t1);
+  ensures
+    the_invariant(id_family(place_id(t1)), state2)
+    &*& [1/2]progress(place_id(t2), place_progress(t2))
+    &*& token_opaque(t2);
+
+
+// To create semi-anonymous lemmas using "produce_lemma_function_pointer_chunk(empty_lemma): lemma_type_t(....)(){...}"
+lemma void empty_lemma()
+  requires true;
+  ensures true;
+{
+}
+
+
+// Construction of new places
+fixpoint place split_place_left(place t1){
+  return place(
+    id_split_left(place_id(t1)),
+    place(place_id(t1), place_parent1(t1), place_parent2(t1), place_trace(t1), place_progress(t1)),
+    no_place,
+    nil,
+    nil);
+}
+fixpoint place split_place_right(place t1){
+  return place(
+    id_split_right(place_id(t1)),
+    place(place_id(t1), place_parent1(t1), place_parent2(t1), place_trace(t1), place_progress(t1)),
+    no_place,
+    nil,
+    nil);
+}
+fixpoint place join_place(place t1, place t2){
+  return place(
+    id_join(place_id(t1), place_id(t2)),
+    place(place_id(t1), place_parent1(t1), place_parent2(t1), place_trace(t1), place_progress(t1)),
+    place(place_id(t2), place_parent1(t2), place_parent2(t2), place_trace(t1), place_progress(t2)),
+    nil,
+    nil);
+}
+fixpoint place place_io(place t1, fixpoint(list<int>, list<int>, bool) io_fp, list<int> progress){
+  return place(
+    place_id(t1),
+    place_parent1(t1),
+    place_parent2(t1),
+    cons(io_fp, place_trace(t1)),
+    progress);
+}
+fixpoint place place_init(int family){
+  return place(
+    id_init(family),
+    no_place,
+    no_place,
+    nil,
+    nil);
+}
 @*/
+
 
 #endif
