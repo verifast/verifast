a8cb4fdec60ea820eafe37ef07d6c6d2c244f3a4
Willem Penninckx <willem.penninckx@cs.kuleuven.be>
2016-02-08 09:03:12 +0000

    Found an unsoundness: there is a way to call a nonghost_callers_only lemma in a normal lemma.
    lemma ...
    nonghost_callers_only // (2)
    ...
    {
      ... // <-- context is nonghost_callers_only
      produce_lemma_function_pointer_chunk(...):... {
         ... // <-- context should not be nonghost_callers_only (1)
         call();
         ...
      }
      {
         ... // <-- context is nonghost_callers_only
      }
    }
    
    A call to a nonghost_callers_only-lemma at (1) is currently allowed if (2) is nonghost_callers_only.
    This allows to wrap a nonghost_callers_only lemma call inside a lemma function pointer chunk,
    which can then later be called by a non-nonghost_callers_only lemma.
    I think the fix is to always disallow nonghost_callers_only lemma calls at (1).
    
    I wanted to implement this fix but it seems that it'll take longer than expected
    and I'm behind schedule so I canceled trying to fix this for now.
    
    The .c file in this commit shows an exploit that proves false.

---
+++ tests/bugs/lemmafuncptr_call_nonghost_in_producelemfunptr.c
@@ -0,0 +1,58 @@
+/**
+ * Call a nonghost_callers_only lemma in a produce_lemma_function_pointer_chunk's body.
+ *
+ * Since lemma function pointers can be called in non-nonghost_callers_only context,
+ * we can indirectly still call a nonghost_callers_only lemma in a
+ * no-nonghost_callers_only context. This allows to prove false.
+ *
+ * This example is based on lemmafuncptr_produce_invalid.c.
+ */
+
+/*@
+typedef lemma void evil_lemma()();
+  requires is_evil_lemma(?proof);
+  ensures  false;
+
+lemma void lemma1_orig()
+nonghost_callers_only
+  requires is_evil_lemma(?proof);
+  ensures  false;
+{  
+  assert is_evil_lemma(proof);
+  duplicate_lemma_function_pointer_chunk(evil_lemma);
+  proof();
+}
+
+lemma void lemma1()
+  requires is_evil_lemma(?proof) &*& is_evil_lemma(?proof2);
+  ensures  false;
+{  
+  assert is_evil_lemma(proof);
+  proof();
+}
+
+lemma void lemma2()
+  nonghost_callers_only
+  requires true;
+  ensures false;
+{
+  produce_lemma_function_pointer_chunk(lemma1) : evil_lemma()(){
+    duplicate_lemma_function_pointer_chunk(evil_lemma);
+    call();
+  }{
+    assert is_evil_lemma(?proof);
+    duplicate_lemma_function_pointer_chunk(evil_lemma);
+    proof();
+  };
+}
+
+
+@*/
+
+void main()
+  //@ requires true;
+  //@ ensures false;
+{
+  //@ lemma2();
+}
+
