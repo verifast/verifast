4701334bde1074030c092d0581cae7e79fce1db6
James Fisher <jameshfisher@gmail.com>
2016-04-24 22:53:34 +0100

    add missing types int64_t and uint64_t

--- bin/stdint.h
+++ bin/stdint.h
@@ -6,12 +6,14 @@
 // you cannot change this soundly without changing
 // VeriFast.
 
-typedef char  int8_t;
-typedef short int16_t;
-typedef int   int32_t;
+typedef char      int8_t;
+typedef short     int16_t;
+typedef int       int32_t;
+typedef long long int64_t;
 
-typedef unsigned char   uint8_t;
-typedef unsigned short  uint16_t;
-typedef unsigned int    uint32_t;
+typedef unsigned char      uint8_t;
+typedef unsigned short     uint16_t;
+typedef unsigned int       uint32_t;
+typedef unsigned long long uint64_t;
 
 #endif // STDINT_H
