f7a62eacab18d50a96a89fb8b975e54a0214e885
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2016-05-10 13:12:17 +0200

    Narrow the scope of an overbroad `try` expression

--- src/assertions.ml
+++ src/assertions.ml
@@ -299,9 +299,8 @@ module Assertions(VerifyProgramArgs: VERIFY_PROGRAM_ARGS) = struct
             produce_post [(f, coef)]
       )
     | WInstPredAsn (l, e_opt, st, cfin, tn, g, index, pats) ->
-      begin
-      try
         let (pmap, pred_symb) =
+          try
           match try_assoc tn classmap1 with
             Some (lcn, abstract, fin, methods, fds_opt, ctors, super, interfs, preds, pn, ilist) ->
             let (_, pmap, _, symb, _) = List.assoc g preds in (pmap, symb)
@@ -316,6 +315,7 @@ module Assertions(VerifyProgramArgs: VERIFY_PROGRAM_ARGS) = struct
                 let InterfaceInfo (li, fields, methods, preds, interfs) = List.assoc tn interfmap0 in
                 let (_, pmap, family, symb) = List.assoc g preds in
                 (pmap, symb)
+          with Not_found -> assert_false h env l ("Definition of predicate " ^ g ^ " is missing from implementing class") None
         in
         let target = match e_opt with None -> List.assoc "this" env | Some e -> ev e in
         let index = ev index in
@@ -325,8 +325,6 @@ module Assertions(VerifyProgramArgs: VERIFY_PROGRAM_ARGS) = struct
         evalpats ghostenv env pats types types $. fun ghostenv env args ->
         produce_chunk h (pred_symb, true) [] coef (Some 2) (target::index::args) size_first $. fun h ->
         cont h ghostenv env
-      with Not_found -> assert_false h env l ("Definition of predicate " ^ g ^  " is missing from implementing class") None
-      end
     | ExprAsn (l, e) -> assume (ev e) (fun _ -> cont h ghostenv env)
     | WMatchAsn (l, e, pat, tp) ->
       let v = ev e in
