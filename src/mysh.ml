b9f274da6d2046b5e613ecb112c5ecd9d8345824
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2016-11-13 23:22:06 +0100

    Improve readability of test suite report
    By default, mysh now produces more concise and more readable output.
    You can get the old output by supplying the -verbose option.
    You can get even more concise output by supplying the -dots option.
    
    TODO:
    - Extend the mysh input language with explicit sequential blocks and parallel
      blocks. Support sequential blocks within parallel blocks.
    - Replace the (long-running) nested mysh calls in the test suite by
      includes to get more fine-grained reporting.
    - Investigate the longest-running test cases to see if they can be made to
      run faster.

--- src/mysh.ml
+++ src/mysh.ml
@@ -2,6 +2,13 @@ let push xs x = xs := x::!xs
 
 let (|>) x f = f x
 
+let string_of_status status =
+  match status with
+    Unix.WEXITED 0 -> "terminated successfully"
+  | Unix.WEXITED n -> Printf.sprintf "terminated with exit code %d" n
+  | Unix.WSIGNALED n -> Printf.sprintf "terminated because of signal %d" n
+  | Unix.WSTOPPED n -> Printf.sprintf "stopped because of signal %d" n
+
 let remove_leading_whitespace s =
   let n = String.length s in
   let first_nonwhite =
@@ -15,10 +22,50 @@ let remove_leading_whitespace s =
   in
   if first_nonwhite = 0 then s else String.sub s first_nonwhite (n - first_nonwhite)
 
-let max_processes =
-  match Sys.argv with
-    [| _; "-cpus"; n |] -> int_of_string n
-  | _ -> 1
+let decompose_path s =
+  let rec iter k =
+    let rec iter' k' =
+      if k' = String.length s then
+        [String.sub s k (k' - k)]
+      else
+        match s.[k'] with
+          '/'|'\\' -> String.sub s k (k' - k)::iter (k' + 1)
+        | _ -> iter' (k' + 1)
+    in
+    iter' k
+  in
+  iter 0
+
+let max_processes = ref 1
+let dots = ref false  (* Print only a dot for successfully terminated processes. *)
+let verbose = ref false
+let main_filename = ref "standard input"
+let main_file = ref stdin
+
+let () =
+  let rec iter args =
+    match args with
+      [] -> ()
+    | "-cpus"::n::args ->
+      max_processes := int_of_string n;
+      iter args
+    | "-dots"::args ->
+      dots := true;
+      iter args
+    | "-verbose"::args ->
+      verbose := true;
+      iter args
+    | filename::args when String.length filename > 0 && filename.[0] <> '-' ->
+      main_filename := filename;
+      let file = try open_in filename with Sys_error s -> failwith (Printf.sprintf "Could not open file '%s': %s" filename s) in
+      main_file := file;
+      iter args
+    | arg::args ->
+      Printf.printf "Invalid argument: %s\n" arg;
+      print_endline "Usage: mysh [-cpus n] [-dots] [-verbose] [filename]";
+      exit 1
+  in
+  iter (List.tl (Array.to_list Sys.argv))
 
 let parse_cmdline line =
   if line <> "" && line.[0] = '#' then [""] else
@@ -47,6 +94,18 @@ let enqueue x =
   Condition.signal queueNonempty;
   Mutex.unlock queueMutex
 
+let dots_printed = ref false
+
+let print_dot () =
+  print_char '.';
+  flush stdout;
+  dots_printed := true
+
+let print_endline s =
+  if !dots_printed then print_newline ();
+  print_endline s;
+  dots_printed := false
+
 let do_print_line s =
   enqueue (fun () -> print_endline s)
 
@@ -62,28 +121,112 @@ let pump_events () =
       iter ()
     end else
       let event = Queue.take queue in
-      begin
-        try
-          event ()
-        with e -> Mutex.unlock queueMutex; raise e
-      end;
+      Mutex.unlock queueMutex;
+      event ();
+      Mutex.lock queueMutex;
       iter ()
   in
   iter ();
   Mutex.unlock queueMutex
+
+type alarm = {mutable alarm_prev: alarm; alarm_time: float; mutable alarm_handler: unit -> unit; mutable alarm_next: alarm}
+
+let remove_alarm alarm =
+  alarm.alarm_prev.alarm_next <- alarm.alarm_next;
+  alarm.alarm_next.alarm_prev <- alarm.alarm_prev;
+  (* Ensure that calling remove_alarm on an alarm that was already removed has no effect *)
+  alarm.alarm_prev <- alarm;
+  alarm.alarm_next <- alarm
+
+let rec alarms = {alarm_prev=alarms; alarm_time=0.0; alarm_handler=(fun () -> ()); alarm_next=alarms}
+
+let alarm_mutex = Mutex.create ()
+let alarm_condition = Condition.create ()
+
+let alarm_thread = Thread.create begin fun () ->
+    Mutex.lock alarm_mutex;
+    let rec iter () =
+      let next = alarms.alarm_next in
+      if next == alarms then begin
+        Condition.wait alarm_condition alarm_mutex
+      end else begin
+        let tnow = Unix.gettimeofday () in
+        let delta = next.alarm_time -. tnow in
+        if delta <= 0.2 then begin
+          remove_alarm next;
+          enqueue begin fun () -> next.alarm_handler () end
+        end else begin
+          Mutex.unlock alarm_mutex;
+          Thread.delay delta;
+          Mutex.lock alarm_mutex
+        end
+      end;
+      iter ()
+    in
+    iter ()
+  end ()
+
+let create_alarm t h =
+  Mutex.lock alarm_mutex;
+  let prev =
+    let rec iter n =
+      if n == alarms || n.alarm_time <= t then n else iter n.alarm_prev
+    in
+    iter alarms.alarm_prev
+  in
+  let alarm = {alarm_prev=prev; alarm_time=t; alarm_handler=h; alarm_next=prev.alarm_next} in
+  prev.alarm_next <- alarm;
+  alarm.alarm_next.alarm_prev <- alarm;
+  Condition.signal alarm_condition;
+  Mutex.unlock alarm_mutex;
+  alarm
+
+let cancel_alarm alarm =
+  Mutex.lock alarm_mutex;
+  remove_alarm alarm;
+  alarm.alarm_handler <- (fun () -> ());
+  Mutex.unlock alarm_mutex
+
+let cwd = ref []
+
+let has_char s c =
+  try
+    ignore (String.index s c); true
+  with Not_found -> false
+
+let getcwd () =
+  match !cwd with
+    [] -> "."
+  | cs -> String.concat "/" (List.rev cs)
 
 let rec exec_lines filepath file lineno =
   if !failed_processes_log = [] then
   let error msg =
     failwith (Printf.sprintf "mysh: %s: line %d: %s" filepath lineno msg)
   in
+  let cd s =
+    decompose_path s |> List.iter begin fun s ->
+      if s = "." then
+        ()
+      else if s = ".." then
+        match !cwd with
+          _::cs -> cwd := cs
+        | [] -> error "cd ..: already at script base directory"
+      else begin
+        if has_char s '/' || has_char s '\\' then error "cd: composite paths are not supported; split into multiple cd commands";
+        push cwd s
+      end;
+      Sys.chdir s
+    end
+  in
   try
     let line = read_line_canon file in
     let rec exec_line line =
+      let line = remove_leading_whitespace line in
       if !failed_processes_log = [] then begin
-      print_endline line;
-      match parse_cmdline (remove_leading_whitespace line) with
-        ["cd"; dir] -> Sys.chdir dir
+      if !verbose then print_endline line;
+      match parse_cmdline line with
+        ["cd"; dir] -> cd dir
       | ["del"; file] -> while !active_processes_count > 0 do pump_events() done; Sys.remove file
       | ["ifnotmac"; line] -> if not Fonts.is_macos then exec_line line
       | ["ifz3"; line] -> if Vfconfig.z3_present then exec_line line
@@ -112,41 +255,64 @@ let rec exec_lines filepath file lineno =
       | _ ->
         incr processes_started_count;
         let pid = !processes_started_count in
-        Printf.printf "Starting process %d\n" pid;
+        if !verbose then Printf.printf "Starting process %d\n" pid;
         incr active_processes_count;
         let time0 = Unix.gettimeofday () in
-        let cwd = Sys.getcwd () in
-        let cin = Unix.open_process_in line in
+        let cwd = getcwd () in
+        let line' = if cwd = "." then line else cwd ^ "$ " ^ line in
+        let cin = Unix.open_process_in (line ^ " 2>&1") in
+        let current_alarm = ref None in
+        let rec produce_alarm i =
+          let runtime = i * 5 in
+          let alarm = create_alarm (time0 +. float_of_int runtime) begin fun () ->
+              print_endline (Printf.sprintf "SLOW: %s has been running for %ds" line' runtime);
+              produce_alarm (i + 1)
+            end
+          in
+          current_alarm := Some alarm
+        in
+        produce_alarm 1;
         let t = Thread.create
           begin fun () ->
             let output = ref [] in
-            push output (cwd ^ "$ " ^ line);
+            if !verbose then push output line';
             try
               while true do
                 let line = input_line cin in
                 push output line;
-                do_print_line (Printf.sprintf "[%d]%s" pid line)
+                if !verbose then do_print_line (Printf.sprintf "[%d]%s" pid line)
               done
             with End_of_file -> ();
             let status = Unix.close_process_in cin in
             let time1 = Unix.gettimeofday() in
-            do_print_line (Printf.sprintf "[%d]%f seconds\n" pid (time1 -. time0));
+            if !verbose then do_print_line (Printf.sprintf "[%d]%f seconds\n" pid (time1 -. time0));
+            enqueue begin fun () ->
+                let Some alarm = !current_alarm in
+                cancel_alarm alarm
+              end;
             if status <> Unix.WEXITED 0 then begin
               let msg =
-                match status with
-                  Unix.WEXITED n -> Printf.sprintf "=== Process %d terminated with exit code %d ===" pid n
-                | Unix.WSIGNALED n -> Printf.sprintf "=== Process %d terminated because of signal %d ===" pid n
-                | Unix.WSTOPPED n -> Printf.sprintf "=== Process %d stopped because of signal %d ===" pid n
+                if !verbose then
+                  Printf.sprintf "=== Process %d %s ===" pid (string_of_status status)
+                else
+                  Printf.sprintf "FAIL: %s %s" line' (string_of_status status)
               in
+              let lines = msg::List.map (fun s -> "> " ^ s) (List.rev !output) in
+              let msg = if !verbose then msg else String.concat "\n" lines in
               do_print_line msg;
-              enqueue (fun () -> push failed_processes_log (msg::List.rev !output))
+              enqueue (fun () -> push failed_processes_log lines)
+            end else begin
+              if !dots then
+                enqueue (fun () -> print_dot ())
+              else
+                do_print_line (Printf.sprintf "PASS: %s (%.2fs)" line' (time1 -. time0))
             end;
             enqueue (fun () -> decr active_processes_count)
           end
           ()
         in
         ignore t;
-        if !active_processes_count = max_processes then pump_events ()
+        if !active_processes_count = !max_processes then pump_events ()
       end
     in
     exec_line line;
@@ -155,7 +321,7 @@ let rec exec_lines filepath file lineno =
 
 let () =
   let time0 = Unix.gettimeofday() in
-  exec_lines "standard input" stdin 1;
+  exec_lines !main_filename !main_file 1;
   while !active_processes_count > 0 do pump_events () done;
   let time1 = Unix.gettimeofday() in
   Printf.printf "Total execution time: %f seconds\n" (time1 -. time0);
