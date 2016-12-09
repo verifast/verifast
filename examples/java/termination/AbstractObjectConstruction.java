a388851971f2215e598725de0b7e4db1f0b671d2
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2016-12-09 20:59:19 +0100

    Add AbstractObjectConstruction termination example

--- examples/java/termination/AbstractObjectConstruction.java
+++ examples/java/termination/AbstractObjectConstruction.java
@@ -11,17 +11,29 @@ interface Func {
         //@ terminates;
 }
 
+//@ predicate Func(Func f, list<Class> level;) = f.valid(?level0) &*& level_le(level0, level) == true;
+
 class Util {
+    static int apply(Func f, int x)
+        //@ requires [_]Func(f, ?level) &*& [2]call_perm(level);
+        //@ ensures true;
+        //@ terminates;
+    {
+        //@ open Func(_, _);
+        //@ assert [_]f.valid(?level0);
+        //@ call_perm_weaken(2, level0);
+        //@ f.getClass_le_level();
+        //@ consume_call_perm_for(f.getClass());
+        return f.apply(x);
+    }
+    
     static int deriv(Func f, int x)
-        //@ requires [_]f.valid(?level) &*& call_perm(cons(Util.class, level));
+        //@ requires [_]Func(f, ?level) &*& call_perm(cons(Util.class, level));
         //@ ensures true;
         //@ terminates;
     {
         //@ call_perm_weaken_and_dup(4);
-        //@ f.getClass_le_level();
-        //@ consume_call_perm_for(f.getClass());
-        //@ consume_call_perm_for(f.getClass());
-        return f.apply(x + 1) - f.apply(x);
+        return apply(f, x + 1) - apply(f, x);
     }
 }
 
@@ -49,11 +61,21 @@ final class ZeroFunc implements Func {
     {
         return 0;
     }
+    public static Func create()
+        //@ requires true;
+        //@ ensures Func(result, {ZeroFunc.class});
+        //@ terminates;
+    {
+        Func f = new ZeroFunc();
+        //@ close Func(f, {ZeroFunc.class});
+        return f;
+    }
 }
 
 final class PlusOneFunc implements Func {
     Func f;
-    //@ predicate valid(list<Class> level) = f |-> ?f &*& [_]f.valid(?fLevel) &*& level == cons(PlusOneFunc.class, fLevel);
+    //@ list<Class> fLevel;
+    //@ predicate valid(list<Class> level) = f |-> ?f &*& fLevel |-> ?fLevel &*& [_]Func(f, fLevel) &*& level == cons(PlusOneFunc.class, fLevel);
     /*@
     lemma void getClass_le_level()
         requires [_]valid(?level);
@@ -63,11 +85,12 @@ final class PlusOneFunc implements Func {
     }
     @*/
     PlusOneFunc(Func f)
-        //@ requires [_]f.valid(?fLevel);
+        //@ requires [_]Func(f, ?fLevel);
         //@ ensures valid(cons(PlusOneFunc.class, fLevel));
         //@ terminates;
     {
         this.f = f;
+        //@ this.fLevel = fLevel;
     }
     public int apply(int x)
         //@ requires [_]valid(?level) &*& call_perm(level);
@@ -76,9 +99,14 @@ final class PlusOneFunc implements Func {
     {
         //@ open valid(_);
         //@ call_perm_weaken_and_dup(2);
-        //@ f.getClass_le_level();
-        //@ consume_call_perm_for(f.getClass());
-        return f.apply(x) + 1;
+        return Util.apply(f, x) + 1;
+    }
+    public static Func create(Func f)
+        //@ requires [_]Func(f, ?fLevel) &*& call_perm(cons(PlusOneFunc.class, fLevel));
+        //@ ensures Func(result, cons(PlusOneFunc.class, fLevel));
+        //@ terminates;
+    {
+        return new PlusOneFunc(f);
     }
 }
 
@@ -88,11 +116,14 @@ class Main {
         //@ ensures true;
         //@ terminates;
     {
-        Func f1 = new ZeroFunc();
-        Func f2 = new PlusOneFunc(f1);
-        Func f3 = new PlusOneFunc(f2);
         //@ produce_call_below_perm_();
-        //@ call_below_perm__elim(1, {Util.class, PlusOneFunc.class, PlusOneFunc.class, ZeroFunc.class});
+        //@ list<Class> level = {Util.class, PlusOneFunc.class, PlusOneFunc.class, ZeroFunc.class};
+        //@ call_below_perm__elim(3, level);
+        Func f1 = ZeroFunc.create();
+        //@ call_perm_weaken(1, {PlusOneFunc.class, ZeroFunc.class});
+        Func f2 = PlusOneFunc.create(f1);
+        //@ call_perm_weaken(1, {PlusOneFunc.class, PlusOneFunc.class, ZeroFunc.class});
+        Func f3 = PlusOneFunc.create(f2);
         Util.deriv(f3, 0);
     }
 }
