c4eb845d1a0b4d32456483d043a25bba5e0de188
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2016-12-21 16:53:24 +0100

    Add Beep termination example
    Shows how the termination verification approach can be used to
    verify liveness of non-terminating programs.

---
+++ examples/java/termination/Beep.java
@@ -0,0 +1,44 @@
+//@ predicate IO(int n);
+
+class OS {
+
+    static void beep()
+        //@ requires IO(?n);
+        //@ ensures 0 < n &*& IO(n - 1);
+        //@ terminates;
+    {
+        throw new RuntimeException();
+    }
+
+}
+
+class Beeper {
+
+    static void beepForever()
+        //@ requires IO(?n) &*& call_perm_rec({Beeper.class}, int_lt, n);
+        //@ ensures false;
+        //@ terminates;
+    {
+        OS.beep();
+        //@ call_perm_rec_weaken(2, n - 1);
+        //@ call_perm_rec_elim(1);
+        //@ consume_call_perm_for(Beeper.class);
+        beepForever();
+    }
+
+}
+
+class Main {
+
+    static void main()
+        //@ requires IO(?n);
+        //@ ensures false;
+        //@ terminates;
+    {
+        //@ produce_call_below_perm_();
+        //@ is_wf_int_lt();
+        //@ call_below_perm__elim_rec(1, {Beeper.class}, int_lt, n);
+        Beeper.beepForever();
+    }
+
+}
