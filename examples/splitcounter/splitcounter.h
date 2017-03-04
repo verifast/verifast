4454fcada5bfaaa03444034c8106edff792b4791
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2017-03-04 16:30:21 +0100

    Add CLH lock and split counter examples

---
+++ examples/splitcounter/splitcounter.h
@@ -0,0 +1,40 @@
+#ifndef SPLITCOUNTER_H
+#define SPLITCOUNTER_H
+
+struct counter;
+
+//@ predicate counter(struct counter *counter, predicate(int) inv);
+
+struct counter *create_counter();
+    //@ requires exists<predicate(int)>(?inv) &*& inv(0);
+    //@ ensures counter(result, inv);
+
+/*@
+
+typedef lemma void incr_ghop(predicate(int) inv, predicate() pre, predicate() post)();
+    requires inv(?value) &*& pre();
+    ensures inv(value + 1) &*& post();
+
+@*/
+
+void incr_left(struct counter *counter);
+    //@ requires [?f]counter(counter, ?inv) &*& is_incr_ghop(?ghop, inv, ?pre, ?post) &*& pre();
+    //@ ensures [f]counter(counter, inv) &*& is_incr_ghop(ghop, inv, pre, post) &*& post();
+
+void incr_right(struct counter *counter);
+    //@ requires [?f]counter(counter, ?inv) &*& is_incr_ghop(?ghop, inv, ?pre, ?post) &*& pre();
+    //@ ensures [f]counter(counter, inv) &*& is_incr_ghop(ghop, inv, pre, post) &*& post();
+
+/*@
+
+typedef lemma void read_ghop(predicate(int) inv, predicate() pre, predicate(int) post)();
+    requires inv(?value) &*& pre();
+    ensures inv(value) &*& post(value);
+
+@*/
+
+int read(struct counter *counter);
+    //@ requires [?f]counter(counter, ?inv) &*& is_read_ghop(?ghop, inv, ?pre, ?post) &*& pre();
+    //@ ensures [f]counter(counter, inv) &*& post(result);
+
+#endif
