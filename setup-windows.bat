876ca6800371efb7a803373074ede545cde46279
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2017-12-05 15:10:05 +0100

    Fix Windows build (missing tool 'conv')

--- setup-windows.bat
+++ setup-windows.bat
@@ -8,7 +8,7 @@
 c:\cygwin\bin\bash -lc "cygcheck -c -d" 
 
 bitsadmin.exe /transfer "cygwin" https://cygwin.com/setup-x86.exe %TEMP%\setup-cygwin-x86.exe || exit /b
-%TEMP%\setup-cygwin-x86.exe -B -qnNd -R c:/cygwin -l c:/cygwin/var/cache/setup -s http://ftp.inf.tu-dresden.de/software/windows/cygwin32/ -P p7zip curl -P make -P mingw64-i686-gcc-g++ -P mingw64-i686-gcc-core -P mingw64-i686-gcc -P patch -P rlwrap -P libreadline6 -P diffutils -P dos2unix -P mingw64-i686-binutils || exit /b
+%TEMP%\setup-cygwin-x86.exe -B -qnNd -R c:/cygwin -l c:/cygwin/var/cache/setup -s http://ftp.inf.tu-dresden.de/software/windows/cygwin32/ -P p7zip -P cygutils-extra -P make -P mingw64-i686-gcc-g++ -P mingw64-i686-gcc-core -P mingw64-i686-gcc -P patch -P rlwrap -P libreadline6 -P diffutils -P mingw64-i686-binutils || exit /b
 @rem Package 'wget' is required as well but is pre-installed on AppVeyor.
 
 @rem Add ",noacl" to prevent cygwin from messing with Windows file permissions
