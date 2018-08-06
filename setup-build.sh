0ff03ea30a6819ce8f5099eacf495afb5aff4172
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2018-08-06 19:03:00 +0200

    Fix Travis macOS build issue (second attempt) #135

--- setup-build.sh
+++ setup-build.sh
@@ -35,8 +35,6 @@ elif [ $(uname -s) = "Darwin" ]; then
   brewinstall gtksourceview
   brewinstall vala
   export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig
-  sudo mkdir /usr/local/include
-  sudo mkdir /usr/local/Frameworks
   sudo chown -R $(whoami):admin /usr/local/*
   cd /usr/local && curl -Lf http://people.cs.kuleuven.be/~bart.jacobs/verifast/vfdeps-17.12-el-capitan.txz | tar xj
   export PATH=/usr/local/vfdeps-17.12/bin:$PATH
