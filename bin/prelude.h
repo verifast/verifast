2763986b92aea3a2b65f478efbe850ca03717948
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2018-08-06 11:18:12 +0200

    Improve chars_to_uchars and uchars_to_chars spec

--- bin/prelude.h
+++ bin/prelude.h
@@ -306,14 +306,24 @@ lemma_auto void pointers_join(void **pp);
     requires [?f]pointers(pp, ?count1, ?ps1) &*& [f]pointers(pp + count1, ?count2, ?ps2);
     ensures [f]pointers(pp, count1 + count2, append(ps1, ps2));
 
+fixpoint char char_of_uchar(unsigned char c);
+fixpoint unsigned char uchar_of_char(char c);
+
+lemma_auto void map_uchar_of_char_char_of_uchar(list<unsigned char> ucs);
+    requires true;
+    ensures map(uchar_of_char, map(char_of_uchar, ucs)) == ucs;
+
+lemma_auto void map_char_of_uchar_uchar_of_char(list<char> cs);
+    requires true;
+    ensures map(char_of_uchar, map(uchar_of_char, cs)) == cs;
 
 lemma_auto void chars_to_uchars(void *p);
-    requires [?f]chars(p, ?n, _);
-    ensures [f]uchars(p, n, _);
+    requires [?f]chars(p, ?n, ?cs);
+    ensures [f]uchars(p, n, map(uchar_of_char, cs));
 
 lemma_auto void uchars_to_chars(void *p);
-    requires [?f]uchars(p, ?n, _);
-    ensures [f]chars(p, n, _);
+    requires [?f]uchars(p, ?n, ?ucs);
+    ensures [f]chars(p, n, map(char_of_uchar, ucs));
 
 lemma_auto void chars_to_ints(void *p, int n);
     requires [?f]chars(p, n * sizeof(int), _);
