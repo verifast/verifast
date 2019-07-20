9a3b32029f7b5bb7aab527afdeb5caee31b9293d
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2019-07-20 09:20:24 +0200

    Erratum to 9677305

--- build.sh
+++ build.sh
@@ -3,13 +3,14 @@
 set -e # Stop as soon as a command fails.
 set -x # Print what is being executed.
 
+. ./config.sh
+
 if [ $(uname -s) = "Linux" ]; then
 
-    export PATH=/tmp/vfdeps/bin:$PATH
+    export PATH=/tmp/$VFDEPS_NAME/bin:$PATH
 
 elif [ $(uname -s) = "Darwin" ]; then
 
-    . ./config.sh
     export PKG_CONFIG_PATH=/usr/local/opt/libffi/lib/pkgconfig
     export PATH=/usr/local/$VFDEPS_NAME/bin:$PATH
     export DYLD_LIBRARY_PATH=/usr/local/$VFDEPS_NAME/lib:$DYLD_LIBRARY_PATH
