9b1772995ac9cb712ed74f65cce31d6a7dafb863
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2019-10-25 21:52:20 +0200

    ghost_counters: strengthen specs

--- examples/lcset/ghost_counters.c
+++ examples/lcset/ghost_counters.c
@@ -1,62 +1,59 @@
+//@ #include <listex.gh>
 //@ #include "ghost_cells.gh"
 //@ #include "raw_ghost_lists.gh"
 //@ #include "ghost_counters.gh"
 
 /*@
 
-predicate foreach2<a, b>(list<a> xs, list<b> ys, predicate(a, b) p) =
+predicate foreach2<a, b>(list<a> xs, predicate(a; b) p; list<b> ys) =
     switch (xs) {
         case nil: return ys == nil;
-        case cons(x, xs0): return
-            switch (ys) {
-                case nil: return false;
-                case cons(y, ys0): return p(x, y) &*& foreach2(xs0, ys0, p);
-            };
+        case cons(x, xs0): return p(x, ?y) &*& foreach2(xs0, p, ?ys0) &*& ys == cons(y, ys0);
     };
 
 predicate foreach2_mem_results<a, b>(list<a> xs_before, list<a> xs_after, list<b> ys_before, b y, list<b> ys_after) = true;
 
 lemma void foreach2_mem<a, b>(a x)
-    requires foreach2<a, b>(?xs, ?ys, ?p) &*& mem(x, xs) == true;
+    requires foreach2<a, b>(?xs, ?p, ?ys) &*& mem(x, xs) == true;
     ensures
         foreach2_mem_results<a, b>(?xs0, ?xs1, ?ys0, ?y, ?ys1) &*&
         xs == append(xs0, cons(x, xs1)) &*& ys == append(ys0, cons(y, ys1)) &*&
         remove(x, xs) == append(xs0, xs1) &*&
-        foreach2(xs0, ys0, p) &*& p(x, y) &*& foreach2(xs1, ys1, p);
+        foreach2(xs0, p, ys0) &*& p(x, y) &*& foreach2(xs1, p, ys1);
 {
-    open foreach2(xs, ys, p);
+    open foreach2(xs, p, ys);
     switch (xs) {
         case nil:
         case cons(x0, xs0):
             if (x0 == x) {
                 close foreach2_mem_results(nil, xs0, nil, head(ys), tail(ys));
-                close foreach2(nil, nil, p);
+                close foreach2(nil, p, nil);
             } else {
                 foreach2_mem(x);
                 open foreach2_mem_results(?xs00, ?xs1, ?ys00, ?y, ?ys1);
                 close foreach2_mem_results(cons(x0, xs00), xs1, cons(head(ys), ys00), y, ys1);
-                close foreach2(cons(x0, xs00), cons(head(ys), ys00), p);
+                close foreach2(cons(x0, xs00), p, cons(head(ys), ys00));
             }
     }
 }
 
 lemma void foreach2_append<a, b>(list<a> xs0, list<a> xs1)
-    requires foreach2<a, b>(xs0, ?ys0, ?p) &*& foreach2<a, b>(xs1, ?ys1, p);
-    ensures foreach2<a, b>(append(xs0, xs1), append(ys0, ys1), p);
+    requires foreach2<a, b>(xs0, ?p, ?ys0) &*& foreach2<a, b>(xs1, p, ?ys1);
+    ensures foreach2<a, b>(append(xs0, xs1), p, append(ys0, ys1));
 {
-    open foreach2(xs0, ys0, p);
+    open foreach2(xs0, p, ys0);
     switch (xs0) {
         case nil:
         case cons(x0, xs00):
             foreach2_append(xs00, xs1);
-            close foreach2(append(xs0, xs1), append(ys0, ys1), p);
+            close foreach2(append(xs0, xs1), p, append(ys0, ys1));
     }
 }
 
 inductive info = info(int tokenCell, int list);
 inductive cinfo = cinfo(int contribCell, real tokenFrac);
 
-predicate_ctor contrib(int tokenCell)(pair<int, cinfo> e, int c) =
+predicate_ctor contrib(int tokenCell)(pair<int, cinfo> e; int c) =
     switch (e) {
         case pair(k, i): return
             switch (i) {
@@ -83,14 +80,28 @@ lemma void sum_append(list<int> xs, list<int> ys)
     }
 }
 
-predicate ghost_counter(int id, int count) =
+fixpoint bool nonneg(int x) { return 0 <= x; }
+
+predicate ghost_counter(int id; int count) =
     [_]ghost_cell<info>(id, ?info) &*&
     switch (info) {
         case info(tokenCell, list): return
             raw_ghost_list(list, _, ?es) &*&
-            foreach2(es, ?cs, contrib(tokenCell)) &*&
+            foreach2(es, contrib(tokenCell), ?cs) &*&
+            forall(cs, nonneg) == true &*&
             count == sum(cs);
     };
+
+lemma void nonneg_sum(list<int> xs)
+    requires forall(xs, nonneg) == true;
+    ensures 0 <= sum(xs);
+{
+    switch (xs) {
+    case nil:
+    case cons(x, xs0):
+        nonneg_sum(xs0);
+    }
+}
 
 predicate ghost_counter_zero_contrib(int id;) =
     [_]ghost_cell<info>(id, ?info) &*&
@@ -123,7 +134,7 @@ lemma int create_ghost_counter()
     int list = create_raw_ghost_list<cinfo>();
     int id = create_ghost_cell<info>(info(tokenCell, list));
     ghost_cell_leak<info>(id);
-    close foreach2(nil, nil, contrib(tokenCell));
+    close foreach2(nil, contrib(tokenCell), nil);
     close ghost_counter(id, 0);
     close ghost_counter_zero_contrib(id);
     return id;
@@ -153,11 +164,12 @@ lemma void ghost_counter_increment()
                 pair<int, cinfo> e = pair(n, cinfo(contribCell, f));
                 ghost_cell_fraction_info<unit>(tokenCell);
                 close contrib(tokenCell)(e, 0);
-                assert foreach2(es, ?cs, _);
-                close foreach2(nil, nil, contrib(tokenCell));
-                close foreach2(cons(e, nil), cons(0, nil), contrib(tokenCell));
+                assert foreach2(es, _, ?cs);
+                close foreach2(nil, contrib(tokenCell), nil);
+                close foreach2(cons(e, nil), contrib(tokenCell), cons(0, nil));
                 foreach2_append(es, cons(e, nil));
                 sum_append(cs, cons(0, nil));
+                forall_append(cs, cons(0, nil), nonneg);
             }
             assert raw_ghost_list_member_handle<cinfo>(list, ?k, ?cinfo);
             raw_ghost_list_match(list, k);
@@ -168,10 +180,12 @@ lemma void ghost_counter_increment()
                 case cinfo(contribCell, tokenFrac):
                     ghost_cell_mutate<int>(contribCell, contrib + 1);
                     close contrib(tokenCell)(pair(k, cinfo), c + 1);
-                    close foreach2(cons(pair(k, cinfo), es1), cons(c + 1, cs1), contrib(tokenCell));
+                    close foreach2(cons(pair(k, cinfo), es1), contrib(tokenCell), cons(c + 1, cs1));
                     foreach2_append(es0, cons(pair(k, cinfo), es1));
                     sum_append(cs0, cons(c + 1, cs1));
                     sum_append(cs0, cons(c, cs1));
+                    forall_append(cs0, cons(c, cs1), nonneg);
+                    forall_append(cs0, cons(c + 1, cs1), nonneg);
                     close ghost_counter(id, count + 1);
                     close ghost_counter_contrib(id, f, contrib + 1);
             }
@@ -180,7 +194,7 @@ lemma void ghost_counter_increment()
 
 lemma void ghost_counter_decrement()
     requires ghost_counter(?id, ?count) &*& ghost_counter_contrib(id, ?f, ?contrib) &*& 0 < contrib;
-    ensures ghost_counter(id, count - 1) &*& ghost_counter_contrib(id, f, contrib - 1);
+    ensures ghost_counter(id, count - 1) &*& ghost_counter_contrib(id, f, contrib - 1) &*& 0 < count;
 {
     open ghost_counter(id, count);
     open ghost_counter_contrib(id, f, contrib);
@@ -198,16 +212,22 @@ lemma void ghost_counter_decrement()
 		    if (contrib == 1) {
 		        raw_ghost_list_remove(list, k);
 		        foreach2_append(es0, es1);
+		        forall_append(cs0, cons(c, cs1), nonneg);
+		        forall_append(cs0, cs1, nonneg);
 		        sum_append(cs0, cons(c, cs1));
 		        sum_append(cs0, cs1);
+		        nonneg_sum(append(cs0, cs1));
 		        ghost_cell_leak(contribCell);
 		    } else {
 		        ghost_cell_mutate<int>(contribCell, c - 1);
 		        close contrib(tokenCell)(e, c - 1);
-		        close foreach2(cons(e, es1), cons(c - 1, cs1), contrib(tokenCell));
+		        close foreach2(cons(e, es1), contrib(tokenCell), cons(c - 1, cs1));
 		        foreach2_append(es0, cons(e, es1));
+		        forall_append(cs0, cons(c, cs1), nonneg);
+		        forall_append(cs0, cons(c - 1, cs1), nonneg);
 		        sum_append(cs0, cons(c, cs1));
 		        sum_append(cs0, cons(c - 1, cs1));
+		        nonneg_sum(append(cs0, cons(c - 1, cs1)));
 		    }
 		    close ghost_counter_contrib(id, f, contrib - 1);
 		    close ghost_counter(id, count - 1);
@@ -233,7 +253,7 @@ lemma void ghost_counter_match_zero_contrib()
     switch (info) {
         case info(tokenCell, list):
             assert raw_ghost_list<cinfo>(list, _, ?es);
-            open foreach2(es, ?cs, _);
+            open foreach2(es, _, ?cs);
             switch (es) {
                 case nil:
                 case cons(e, es0):
@@ -241,7 +261,7 @@ lemma void ghost_counter_match_zero_contrib()
                     ghost_cell_fraction_info(tokenCell);
                     assert false;
             }
-            close foreach2(nil, nil, contrib(tokenCell));
+            close foreach2(nil, contrib(tokenCell), nil);
     }
     close ghost_counter(id, count);
     close ghost_counter_zero_contrib(id);
