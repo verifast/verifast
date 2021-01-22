688fd1c96133cbed374764ace13fce737a182bdf
Niels Mommen <nielsmommen@hotmail.com>
2021-01-22 21:51:17 +0100

    pretty print json

--- src/json_tests.ml
+++ src/json_tests.ml
@@ -9,3 +9,25 @@ let () =
   assert(parse_json "\"\\u20AC\"" = S "€"); (* EURO SIGN *)
   assert(parse_json "\"\\uFFFD\"" = S "�"); (* REPLACEMENT CHARACTER *)
   assert(parse_json "\"\\uD83C\\uDF89\"" = S "🎉") (* PARTY POPPER *)
+
+let () =
+  let j = O ["key", S "value"; "arr", O ["nested_arr", A [A [Null; I 10; A []];S "string"; B true]; "empty object", O []]] in
+  let buf = Buffer.create 1024 in
+  begin
+    buffer_add_json_pp buf 2 j;
+    assert(Buffer.contents buf = {|{
+  "key": "value",
+  "arr": {
+    "nested_arr": [
+      [
+        null,
+        10,
+        []
+      ],
+      "string",
+      true
+    ],
+    "empty object": {}
+  }
+}|})
+  end
