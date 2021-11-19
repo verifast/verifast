7e2dc16a5db3e79cbfaebdf00d22bb06e8d05cee
Niels Mommen <nielsmommen@hotmail.com>
2021-11-19 16:15:54 +0100

    Fix dynamic linking to redundant libraries

--- setup-build.sh
+++ setup-build.sh
@@ -5,46 +5,58 @@
 # Suitable for home use and for continuous integration.
 #
 
-llvm_version=13
-
-dl_and_unzip() {
-  url="$1"
-  filename=$(basename "$url")
-  hash="$2"
-  curl -Lf -o "/tmp/$filename" "$url"
-  echo "$hash  /tmp/$filename" | shasum -a 224 -c || exit 1
-  tar xjf "/tmp/$filename"
-}
-
 set -e # Stop as soon as a command fails.
 set -x # Print what is being executed.
 
 . ./config.sh
 
+dl_and_unzip() {
+  url="$1"
+  filename=$(basename "$url")
+  hash="$2"
+  sha="$3"
+  filter="$4"
+  curl -Lf -o "/tmp/$filename" "$url"
+  echo "$hash  /tmp/$filename" | shasum -a "$sha" -c || exit 1
+  tar "x"$filter"f" "/tmp/$filename"
+}
+
+dl_and_unzip_vfdeps() {
+  url="$1"
+  hash="$2"
+  dl_and_unzip $url $hash 224 j
+}
+
+dl_and_unzip_llvm-clang() {
+  platform="$1"
+  hash="$2"
+  dl_and_unzip "https://github.com/NielsMommen/vf-llvm-clang-build/releases/download/v1.0.0/vf-llvm-clang-build$VF_LLVM_CLANG_BUILD_VERSION-$platform.tar.gz" $hash 256 z
+}
+
 script_dir=$(pwd)
 
 if [ $(uname -s) = "Linux" ]; then
-  wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-  sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-$llvm_version main"
   sudo apt-get update
   sudo apt-get install -y --no-install-recommends \
-       git wget ca-certificates make m4 \
-       gcc patch unzip libgtk2.0-dev \
+       git wget ca-certificates m4 \
+       patch unzip libgtk2.0-dev \
        valac libgtksourceview2.0-dev \
-       make cmake llvm-$llvm_version-dev clang-$llvm_version libclang-$llvm_version-dev
+       cmake build-essential
+  
   cd /tmp
-  dl_and_unzip https://vfdeps-cxx-linux.herokuapp.com/$VFDEPS_NAME-linux.txz c69e9bb1f058d827727d28922f3ebb6353f2fcbc8bd7dfe3ece54f94
-  cd ..
+  dl_and_unzip_llvm-clang Linux 1a48b859127a3574732b6fba735f410065f51a547d6ba8fe582e21f81b01ae31
+  dl_and_unzip_vfdeps https://vfdeps-cxx-linux.herokuapp.com/$VFDEPS_NAME-linux.txz c69e9bb1f058d827727d28922f3ebb6353f2fcbc8bd7dfe3ece54f94
+
   cd $script_dir/src/cxx_frontend/ast_exporter/build
-  CC=/usr/bin/clang-$llvm_version CXX=/usr/bin/clang++-$llvm_version cmake -DLLVM_INSTALL_DIR=/usr/lib/llvm-$llvm_version -DVFDEPS=/tmp/$VFDEPS_NAME -DCMAKE_BUILD_TYPE=Debug ..
+  cmake -DLLVM_INSTALL_DIR=/tmp/vf-llvm-clang-build$VF_LLVM_CLANG_BUILD_VERSION -DVFDEPS=/tmp/$VFDEPS_NAME -DCMAKE_BUILD_TYPE=Release ..
 
 
 elif [ $(uname -s) = "Darwin" ]; then
 
-  # if [ -z "$GITHUB_ACTIONS" ]; then
+  if [ -z "$GITHUB_ACTIONS" ]; then
       # No need to update when running in GitHub Actions; their brew is updated weekly.
       brew update
-  # fi
+  fi
 
   function brewinstall {
       if brew list $1 1>/dev/null 2>/dev/null; then
@@ -58,14 +70,17 @@ elif [ $(uname -s) = "Darwin" ]; then
   brewinstall gtksourceview
   brewinstall vala
   brewinstall cmake
-  brewinstall llvm@$llvm_version
   export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig
   sudo mkdir /usr/local/$VFDEPS_NAME
+  sudo mkdir /usr/local/vf-llvm-clang-build$VF_LLVM_CLANG_BUILD_VERSION
   sudo chown -R $(whoami):admin /usr/local/*
+
   cd /usr/local
-  dl_and_unzip https://vfdeps-cxx-macos.herokuapp.com/$VFDEPS_NAME-macos.txz 301bf548e6bdbaac79ef49f3c2eb787a37b8487c4c25de1aec92b6c5
+  dl_and_unzip_llvm-clang MacOS 5e1583e736450d490bfc31e62ba84a5138ac742275d3eb013c7f40f68c6317d2
+  dl_and_unzip_vfdeps https://vfdeps-cxx-macos.herokuapp.com/$VFDEPS_NAME-macos.txz 301bf548e6bdbaac79ef49f3c2eb787a37b8487c4c25de1aec92b6c5
+
   cd $script_dir/src/cxx_frontend/ast_exporter/build
-  cmake -DLLVM_INSTALL_DIR=$(brew --prefix llvm@$llvm_version) -DVFDEPS=/usr/local/$VFDEPS_NAME -DCMAKE_BUILD_TYPE=Debug ..
+  cmake -DLLVM_INSTALL_DIR=/usr/local/vf-llvm-clang-build$VF_LLVM_CLANG_BUILD_VERSION -DVFDEPS=/usr/local/$VFDEPS_NAME -DCMAKE_BUILD_TYPE=Release ..
   
 else
   echo "Your OS is not supported by this script."
