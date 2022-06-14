62b56f270f260eba8e0c15819a390f1158a20141
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2022-06-14 11:14:47 +0200

    fp_of_float/double, real_of_int(_of_decimal)

--- bin/vf__floating_point.h
+++ bin/vf__floating_point.h
@@ -1,6 +1,78 @@
 #ifndef VF__FLOATING_POINT_H
 #define VF__FLOATING_POINT_H
 
+// =========== real_of_int ===========
+
+/*@
+
+fixpoint bool is_decimal_digit(char c) {
+    return '0' <= c && c <= '9';
+}
+
+fixpoint int int_of_decimal_helper(int n, list<char> digits) {
+    switch (digits) {
+        case nil: return n;
+        case cons(d, ds): return int_of_decimal_helper(n * 10 + (d - '0'), ds);
+    }
+}
+
+fixpoint int int_of_decimal(list<char> digits) {
+    return head(digits) == '-' ?
+        -int_of_decimal_helper(0, tail(digits))
+    :
+        int_of_decimal_helper(0, digits);
+}
+
+fixpoint real real_of_decimal_digit(char d) {
+    return
+        d == '0' ? 0r :
+        d == '1' ? 1r :
+        d == '2' ? 2r :
+        d == '3' ? 3r :
+        d == '4' ? 4r :
+        d == '5' ? 5r :
+        d == '6' ? 6r :
+        d == '7' ? 7r :
+        d == '8' ? 8r :
+        9r;
+}
+
+fixpoint real real_of_decimal_helper(real n, list<char> digits) {
+    switch (digits) {
+        case nil: return n;
+        case cons(d, ds): return real_of_decimal_helper(n * 10 + real_of_decimal_digit(d), ds);
+    }
+}
+
+fixpoint real real_of_decimal(list<char> digits) {
+    return head(digits) == '-' ?
+        -real_of_decimal_helper(0, tail(digits))
+    :
+        real_of_decimal_helper(0, digits);
+}
+
+fixpoint real real_of_int(int x);
+
+lemma void real_of_int_of_decimal(list<char> digits);
+    requires digits == cons(?d0, ?ds) &*& d0 == '-' ? forall(ds, is_decimal_digit) == true : forall(digits, is_decimal_digit) == true;
+    ensures real_of_int(int_of_decimal(digits)) == real_of_decimal(digits);
+
+@*/
+
+// =========== floating point ===========
+
+/*@
+
+inductive fp = fp_real(real r) | fp_pos_inf | fp_neg_inf | fp_NaN;
+
+fixpoint fp fp_of_float(float f);
+
+fixpoint fp fp_of_double(double d);
+
+fixpoint fp fp_of_long_double(long double ld);
+
+@*/
+
 // VeriFast interprets floating-point operations as calls of the functions declared below.
 
 // A floating-point constant of type T is interpreted as a call of the T_of_real function.
@@ -19,15 +91,15 @@ long double vf__long_double_of_real(real x);
 
 float vf__float_of_int128(__int128 x);
     //@ requires true;
-    //@ ensures true;
+    //@ ensures -(1 << 24) <= x && x <= (1 << 24) ? fp_of_float(result) == fp_real(real_of_int(x)) : true;
 
 double vf__double_of_int128(__int128 x);
     //@ requires true;
-    //@ ensures true;
+    //@ ensures -(1 << 53) <= x && x <= (1 << 53) ? fp_of_double(result) == fp_real(real_of_int(x)) : true;
 
 long double vf__long_double_of_int128(__int128 x);
     //@ requires true;
-    //@ ensures true;
+    //@ ensures -(1 << 53) <= x && x <= (1 << 53) ? fp_of_long_double(result) == fp_real(real_of_int(x)) : true;
 
 float vf__float_of_long_long(long long x);
     //@ requires true;
--- tests/floating_point.c
+++ tests/floating_point.c
@@ -5,7 +5,10 @@ int main()
 //@ ensures true;
 {
   float f = 42;
-  double d = 0;
-
+  //@ real_of_int_of_decimal("42");
+  //@ assert fp_of_float(f) == fp_real(42);
+  double d = -127;
+  //@ real_of_int_of_decimal("-127");
+  //@ assert fp_of_double(d) == fp_real(-127r);
   return 0;
 }
