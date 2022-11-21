4828c849d5240112af1d21ff570f033e5fc6d788
Niels Mommen <nielsmommen@hotmail.com>
2022-11-21 16:02:13 +0100

    [C++] Add typid cases

--- tests/cxx/typeid.cpp
+++ tests/cxx/typeid.cpp
@@ -1,8 +1,16 @@
-struct A;
+struct S;
+union U;
 
 int main()
 //@ requires true;
 //@ ensures true;
 {
-  //@ std::type_info *t = &typeid(struct A);
+    //@ std::type_info *t = &typeid(unsigned long);
+    //@ t = &typeid(intptr_t);
+    //@ t = &typeid(struct S);
+    //@ t = &typeid(union U);
+    //@ t = &typeid(bool);
+    //@ t = &typeid(float);
+    //@ t = &typeid(double);
+    //@ t = &typeid(long double);
 }
