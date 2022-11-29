4b349daf5610a4648e199c399d85f9c3d300eacc
Niels Mommen <nielsmommen@hotmail.com>
2022-11-29 15:52:04 +0100

    Fix cygwin path

--- setup-windows.bat
+++ setup-windows.bat
@@ -10,7 +10,7 @@ bitsadmin.exe /transfer "cygwin" https://www.cygwin.com/setup-x86_64.exe %TEMP%\
 %TEMP%\setup-cygwin.exe -B -qnNd -R c:/cygwin64 -l c:/cygwin64/var/cache/setup -s http://ftp.inf.tu-dresden.de/software/windows/cygwin32/ -P p7zip -P cygutils-extra -P make -P mingw64-i686-gcc-g++ -P mingw64-i686-gcc-core -P mingw64-i686-gcc -P patch -P rlwrap -P libreadline6 -P diffutils -P mingw64-i686-binutils -P wget || exit /b
 
 @rem Add ",noacl" to prevent cygwin from messing with Windows file permissions
-echo none /cygdrive cygdrive binary,posix=0,user,noacl 0 0 > c:\cygwin\etc\fstab || exit /b
+echo none /cygdrive cygdrive binary,posix=0,user,noacl 0 0 > c:\cygwin64\etc\fstab || exit /b
 
 for /f "TOKENS=* USEBACKQ" %%f in (`cd`) do set VFWORKDIR=%%f
 
