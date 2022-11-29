967fbcb3a5cb6f2680d92f543dc8f5130ec51c2f
Niels Mommen <nielsmommen@hotmail.com>
2022-11-29 15:42:36 +0100

    Try to fix appveyor build

--- setup-windows.bat
+++ setup-windows.bat
@@ -7,7 +7,7 @@
 c:\cygwin\bin\bash -lc "cygcheck -c -d" 
 
 bitsadmin.exe /transfer "cygwin" https://www.cygwin.com/setup-x86_64.exe %TEMP%\setup-cygwin.exe || exit /b
-%TEMP%\setup-cygwin.exe -B -qnNd -R c:/cygwin -l c:/cygwin/var/cache/setup -s http://ftp.inf.tu-dresden.de/software/windows/cygwin32/ -P p7zip -P cygutils-extra -P make -P mingw64-i686-gcc-g++ -P mingw64-i686-gcc-core -P mingw64-i686-gcc -P patch -P rlwrap -P libreadline6 -P diffutils -P mingw64-i686-binutils -P wget || exit /b
+%TEMP%\setup-cygwin.exe -B -qnNd -R c:/cygwin64 -l c:/cygwin64/var/cache/setup -s http://ftp.inf.tu-dresden.de/software/windows/cygwin32/ -P p7zip -P cygutils-extra -P make -P mingw64-i686-gcc-g++ -P mingw64-i686-gcc-core -P mingw64-i686-gcc -P patch -P rlwrap -P libreadline6 -P diffutils -P mingw64-i686-binutils -P wget || exit /b
 
 @rem Add ",noacl" to prevent cygwin from messing with Windows file permissions
 echo none /cygdrive cygdrive binary,posix=0,user,noacl 0 0 > c:\cygwin\etc\fstab || exit /b
@@ -17,4 +17,4 @@ for /f "TOKENS=* USEBACKQ" %%f in (`cd`) do set VFWORKDIR=%%f
 @rem set MSVC environment variables
 call vcvarsall.bat x86
 
-c:\cygwin\bin\bash -lc "cd ""$VFWORKDIR"" && bash setup-windows.sh" || exit /b
+c:\cygwin64\bin\bash -lc "cd ""$VFWORKDIR"" && bash setup-windows.sh" || exit /b
