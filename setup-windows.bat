c52c9a2c2591179707e4ed4c8e3a4c31d9cc628d
NielsMommen <32457927+NielsMommen@users.noreply.github.com>
2022-11-29 16:57:02 +0100

    Cygwin x86_64 (#345)

--- setup-windows.bat
+++ setup-windows.bat
@@ -6,15 +6,15 @@
 @rem First, list pre-installed packages
 c:\cygwin\bin\bash -lc "cygcheck -c -d" 
 
-bitsadmin.exe /transfer "cygwin" https://cygwin.com/setup-x86.exe %TEMP%\setup-cygwin-x86.exe || exit /b
-%TEMP%\setup-cygwin-x86.exe -B -qnNd -R c:/cygwin -l c:/cygwin/var/cache/setup -s http://ftp.inf.tu-dresden.de/software/windows/cygwin32/ -P p7zip -P cygutils-extra -P make -P mingw64-i686-gcc-g++ -P mingw64-i686-gcc-core -P mingw64-i686-gcc -P patch -P rlwrap -P libreadline6 -P diffutils -P mingw64-i686-binutils -P wget || exit /b
+bitsadmin.exe /transfer "cygwin" https://www.cygwin.com/setup-x86_64.exe %TEMP%\setup-cygwin.exe || exit /b
+%TEMP%\setup-cygwin.exe -B -qnNd -R c:/cygwin64 -l c:/cygwin64/var/cache/setup -s http://ftp.inf.tu-dresden.de/software/windows/cygwin32/ -P p7zip -P cygutils-extra -P make -P mingw64-i686-gcc-g++ -P mingw64-i686-gcc-core -P mingw64-i686-gcc -P patch -P rlwrap -P libreadline6 -P diffutils -P mingw64-i686-binutils -P wget || exit /b
 
 @rem Add ",noacl" to prevent cygwin from messing with Windows file permissions
-echo none /cygdrive cygdrive binary,posix=0,user,noacl 0 0 > c:\cygwin\etc\fstab || exit /b
+echo none /cygdrive cygdrive binary,posix=0,user,noacl 0 0 > c:\cygwin64\etc\fstab || exit /b
 
 for /f "TOKENS=* USEBACKQ" %%f in (`cd`) do set VFWORKDIR=%%f
 
 @rem set MSVC environment variables
 call vcvarsall.bat x86
 
-c:\cygwin\bin\bash -lc "cd ""$VFWORKDIR"" && bash setup-windows.sh" || exit /b
+c:\cygwin64\bin\bash -lc "cd ""$VFWORKDIR"" && bash setup-windows.sh" || exit /b
