b709cfa80ff412453c44998cf3e94f3da95bf550
Niels Mommen <nielsmommen@hotmail.com>
2022-11-29 14:55:11 +0100

    Cygwin 64 bit

--- setup-windows.bat
+++ setup-windows.bat
@@ -6,8 +6,8 @@
 @rem First, list pre-installed packages
 c:\cygwin\bin\bash -lc "cygcheck -c -d" 
 
-bitsadmin.exe /transfer "cygwin" https://cygwin.com/setup-x86.exe %TEMP%\setup-cygwin-x86.exe || exit /b
-%TEMP%\setup-cygwin-x86.exe -B -qnNd -R c:/cygwin -l c:/cygwin/var/cache/setup -s http://ftp.inf.tu-dresden.de/software/windows/cygwin32/ -P p7zip -P cygutils-extra -P make -P mingw64-i686-gcc-g++ -P mingw64-i686-gcc-core -P mingw64-i686-gcc -P patch -P rlwrap -P libreadline6 -P diffutils -P mingw64-i686-binutils -P wget || exit /b
+bitsadmin.exe /transfer "cygwin" https://cygwin.com/setup.exe %TEMP%\setup-cygwin.exe || exit /b
+%TEMP%\setup-cygwin.exe -B -qnNd -R c:/cygwin -l c:/cygwin/var/cache/setup -s http://ftp.inf.tu-dresden.de/software/windows/cygwin32/ -P p7zip -P cygutils-extra -P make -P mingw64-i686-gcc-g++ -P mingw64-i686-gcc-core -P mingw64-i686-gcc -P patch -P rlwrap -P libreadline6 -P diffutils -P mingw64-i686-binutils -P wget || exit /b
 
 @rem Add ",noacl" to prevent cygwin from messing with Windows file permissions
 echo none /cygdrive cygdrive binary,posix=0,user,noacl 0 0 > c:\cygwin\etc\fstab || exit /b
