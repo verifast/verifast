02bbab1c2ed210418814de61bc6995f6c2e2a10a
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2023-04-12 09:03:41 +0200

    WIP ticketlock.c

--- examples/busywaiting/ticketlock/ticketlock.c
+++ examples/busywaiting/ticketlock/ticketlock.c
@@ -3,41 +3,95 @@
 #include "atomics.h"
 #include "ticketlock.h"
 
+/*@
+
+box_class growing_list(list<void *> xs) {
+  invariant true;
+  action noop();
+    requires true;
+    ensures xs == old_xs;
+  action add(void *elem);
+    requires true;
+    ensures xs == append(old_xs, {elem});
+  handle_predicate has_at(int k, void *x) {
+    invariant 0 <= k && k < length(xs) && nth(k, xs) == x;
+    preserved_by noop() {}
+    preserved_by add(elem) {
+      nth_append(old_xs, {elem}, k);
+    }
+  }
+}
+
+@*/
+
 typedef struct ticketlock {
+    //@ list<int> level;
+    //@ predicate(int, bool) inv_;
     unsigned long long next;
     unsigned long long owner;
+    //@ bool held;
+    //@ box signalsBox;
 } *ticketlock;
 
 /*@
 
-//predicate ticketlock(ticketlock lock; predicate(int, int) inv);
+lemma_auto void ticketlock_nb_level_dims_nonneg()
+    requires true;
+    ensures 0 <= ticketlock_nb_level_dims();
+{}
+
+predicate signals(list<void *> signals, list<int> baseLevel, int start) =
+    start == length(signals) ?
+        true
+    :
+        signal(nth(start, signals), append(baseLevel, {start}), false) &*& signals(signals, baseLevel, start + 1);
+
+predicate_ctor ticketlock_inv(ticketlock lock, list<int> level, predicate(int, bool) inv, box signalsBox)() =
+    growing_list(signalsBox, ?signals) &*&
+    lock->next |-> ?next &*& length(signals) == next &*&
+    [1/2]lock->owner |-> ?owner &*& 0 <= owner &*&
+    [1/2]lock->held |-> ?held &*&
+    signals(signals, level, held ? owner + 1 : owner) &*&
+    inv(owner, held) &*&
+    held ?
+        owner < next
+    :
+        [1/2]lock->owner |-> owner &*& [1/2]lock->held |-> held;
+
+predicate ticketlock(ticketlock lock; list<int> level, predicate(int, bool) inv) =
+    lock->level |-> level &*& lock->inv_ |-> inv &*& lock->signalsBox |-> ?signalsBox &*& malloc_block_ticketlock(lock) &*&
+    atomic_space(create_ticketlock, ticketlock_inv(lock, level, inv, signalsBox));
 
 @*/
 
+
 ticketlock create_ticketlock()
-//@ requires exists<predicate(int, int)>(?inv) &*& inv(0, 0);
-//@ ensures ticketlock(result, inv);
+/*@
+requires
+    exists<pair<list<int>, predicate(int, bool)> >(pair(?level, ?inv)) &*& inv(0, false) &*&
+    level == cons(?level_max_length, ?level0) &*& length(level0) + ticketlock_nb_level_dims <= level_max_length;
+@*/
+//@ ensures ticketlock(result, level, inv);
+//@ terminates;
 {
+    //@ open exists(_);
     ticketlock result = malloc(sizeof(struct ticketlock));
     if (result == 0) abort();
     result->next = 0;
     result->owner = 0;
+    //@ result->level = level;
+    //@ result->inv_ = inv;
+    //@ create_box signalsBox = growing_list({});
+    //@ result->signalsBox = signalsBox;
+    //@ result->held = false;
+    //@ close signals({}, level, 0);
+    //@ close ticketlock_inv(result, level, inv, signalsBox)();
+    //@ create_atomic_space(create_ticketlock, ticketlock_inv(result, level, inv, signalsBox));
     return result;
 }
 
-void ticketlock_acquire(ticketlock lock)
-/*@
-requires
-    [?f]ticketlock(lock, ?inv) &*&
-    is_ticketlock_take_ticket_ghost_op(?top, inv, ?pre, ?wait_inv, currentThread) &*&
-    is_ticketlock_wait_ghost_op(?wop, inv, wait_inv, currentThread) &*&
-    is_ticketlock_acquire_ghost_op(?aop, inv, wait_inv, ?post, currentThread) &*&
-    pre();
-@*/
-//@ ensures [f]ticketlock(lock, inv) &*& post();
+void ticketlock_acquire_helper(ticketlock lock, unsigned long long ticket)
 {
-    unsigned long long ticket;
-    ticket = atomic_fetch_and_increment_counter(&lock->next);
     for (;;)
     {
         unsigned long long owner;
@@ -47,9 +101,49 @@ requires
     }
 }
 
+void ticketlock_acquire(ticketlock lock)
+/*@
+requires
+    [?f]ticketlock(lock, ?level, ?inv) &*&
+    obs(?p, ?obs) &*& 
+    forall(map(snd, obs), (all_sublevels_lt)(ticketlock_nb_level_dims, level)) == true &*&
+    is_ticketlock_wait_ghost_op(?wop, p, level, inv, ?wait_inv, currentThread) &*&
+    is_ticketlock_acquire_ghost_op(?aop, p, level, inv, wait_inv, ?post, currentThread) &*&
+    wait_inv(-1, _);
+@*/
+//@ ensures [f]ticketlock(lock, level, inv) &*& post();
+//@ terminates;
+{
+    //@ box signalsBox = lock->signalsBox;
+    unsigned long long ticket;
+    {
+        /*@
+        predicate pre_() = [f]atomic_space(create_ticketlock, ticketlock_inv(lock, level, inv, signalsBox));
+        predicate post_(int result) = [f]atomic_space(create_ticketlock, ticketlock_inv(lock, level, inv, signalsBox));
+        @*/
+        /*@
+        produce_lemma_function_pointer_chunk atomic_fetch_and_increment_counter_ghost_op(&lock->next, pre_, post_, currentThread)() {
+            open pre_();
+            open_atomic_space(create_ticketlock, ticketlock_inv(lock, level, inv, signalsBox));
+            open ticketlock_inv(lock, level, inv, signalsBox)();
+            assert is_atomic_fetch_and_increment_counter_op(?op, &lock->next, ?P, ?Q);
+            assert lock->next |-> ?next;
+            open ticketlock_next(lock, _);
+            op();
+            close post_(next);
+        };
+        @*/
+        //@ close pre();
+        ticket = atomic_fetch_and_increment_counter(&lock->next);
+        //@ open post(_);
+    }
+    ticketlock_acquire_helper(lock, ticket);
+}
+
 void ticketlock_release(ticketlock lock)
-//@ requires [?f]ticketlock(lock, ?inv) &*& is_ticketlock_release_ghost_op(?ghop, inv, ?pre, ?post, currentThread) &*& pre();
-//@ ensures [f]ticketlock(lock, inv) &*& post();
+//@ requires [?f]ticketlock(lock, ?level, ?inv) &*& is_ticketlock_release_ghost_op(?ghop, inv, ?pre, ?post, currentThread) &*& pre();
+//@ ensures [f]ticketlock(lock, level, inv) &*& post();
+//@ terminates;
 {
     unsigned long long owner = load_counter(&lock->owner);
     atomic_store_counter(&lock->owner, owner + 1);
--- examples/busywaiting/ticketlock/ticketlock.h
+++ examples/busywaiting/ticketlock/ticketlock.h
@@ -1,6 +1,8 @@
 #ifndef TICKETLOCK_H
 #define TICKETLOCK_H
 
+#include "../busywaiting.h"
+
 struct ticketlock;
 typedef struct ticketlock *ticketlock;
 
@@ -17,24 +19,28 @@ predicate ticketlock(ticketlock lock; list<int> level, predicate(int, bool) inv)
 @*/
 
 ticketlock create_ticketlock();
-//@ requires exists<pair<list<int>, predicate(int, bool)> >(pair(?level, ?inv)) &*& inv(false) &*& level == cons(?level_max_length, ?level0) &*& length(level0) + ticketlock_nb_level_dims <= level_max_length;
+/*@
+requires
+    exists<pair<list<int>, predicate(int, bool)> >(pair(?level, ?inv)) &*& inv(0, false) &*&
+    level == cons(?level_max_length, ?level0) &*& length(level0) + ticketlock_nb_level_dims <= level_max_length;
+@*/
 //@ ensures ticketlock(result, level, inv);
 //@ terminates;
 
 /*@
 
-typedef lemma void ticketlock_wait_ghost_op(list<pathcomp> p, list<int> level, predicate(int, bool) inv, predicate(int) wait_inv, int callerThread)();
+typedef lemma void ticketlock_wait_ghost_op(list<pathcomp> p, list<int> level, predicate(int, bool) inv, predicate(int, void *) wait_inv, int callerThread)(void *f);
     requires
         obs(?p1, ?obs) &*& is_ancestor_of(p, p1) == true &*& forall(map(snd, obs), (level_lt)(level)) == true &*&
-        inv(?owner, true) &*& wait_inv(?owner0) &*& currentThread == callerThread &*&
-        owner0 == owner ? true : owner0 < owner &*& call_below_perm_(ticketlock_release);
+        inv(?owner, true) &*& 0 <= owner &*& wait_inv(?owner0, ?f0) &*& currentThread == callerThread &*& func_lt(f, ticketlock_acquire) == true &*&
+        owner == owner0 ? f == f0 : owner0 < owner &*& call_below_perm_(currentThread, ticketlock_acquire);
     ensures
-        obs(p1, obs) &*& inv(owner, true) &*& wait_inv(owner) &*& call_perm_(currentThread, ticketlock_acquire);
+        obs(p1, obs) &*& inv(owner, true) &*& wait_inv(owner, f) &*& call_perm_(currentThread, f);
 
-typedef lemma void ticketlock_acquire_ghost_op(list<pathcomp> p, list<int> level, predicate(int, bool) inv, predicate(int) wait_inv, predicate() post, int callerThread)();
+typedef lemma void ticketlock_acquire_ghost_op(list<pathcomp> p, list<int> level, predicate(int, bool) inv, predicate(int, void *) wait_inv, predicate() post, int callerThread)();
     requires
         obs(?p1, ?obs) &*& is_ancestor_of(p, p1) == true &*& forall(map(snd, obs), (all_sublevels_lt)(ticketlock_nb_level_dims, level)) == true &*&
-        inv(?owner, false) &*& wait_inv(_) &*& currentThread == callerThread;
+        inv(?owner, false) &*& wait_inv(_, _) &*& currentThread == callerThread;
     ensures inv(owner, true) &*& post();
 
 @*/
@@ -45,11 +51,12 @@ requires
     [?f]ticketlock(lock, ?level, ?inv) &*&
     obs(?p, ?obs) &*& 
     forall(map(snd, obs), (all_sublevels_lt)(ticketlock_nb_level_dims, level)) == true &*&
-    is_ticketlock_wait_ghost_op(?wop, p, level, inv, wait_inv, currentThread) &*&
+    is_ticketlock_wait_ghost_op(?wop, p, level, inv, ?wait_inv, currentThread) &*&
     is_ticketlock_acquire_ghost_op(?aop, p, level, inv, wait_inv, ?post, currentThread) &*&
-    wait_inv(0);
+    wait_inv(-1, _);
 @*/
 //@ ensures [f]ticketlock(lock, level, inv) &*& post();
+//@ terminates;
 
 /*@
 
@@ -62,5 +69,6 @@ typedef lemma void ticketlock_release_ghost_op(predicate(int, bool) inv, predica
 void ticketlock_release(ticketlock lock);
 //@ requires [?f]ticketlock(lock, ?level, ?inv) &*& is_ticketlock_release_ghost_op(?ghop, inv, ?pre, ?post, currentThread) &*& pre();
 //@ ensures [f]ticketlock(lock, level, inv) &*& post();
+//@ terminates;
 
 #endif
