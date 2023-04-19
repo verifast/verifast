14c230179e99d6dac58c8d2464f4080caba1abca
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2023-04-19 18:35:48 +0200

    ticketlock_classic spec

---
+++ examples/busywaiting/ticketlock/ticketlock_classic.h
@@ -0,0 +1,40 @@
+#ifndef TICKETLOCK_CLASSIC_H
+#define TICKETLOCK_CLASSIC_H
+
+#include "ticketlock.h" // Only needed for ticketlock_nb_level_dims, which should be hidden.
+
+#include "../busywaiting.h"
+
+struct ticketlock_classic;
+typedef struct ticketlock_classic *ticketlock_classic;
+
+/*@
+
+fixpoint int ticketlock_classic_nb_level_dims() { return ticketlock_nb_level_dims; } // TODO: Hide this fixpoint body from clients.
+
+predicate ticketlock_classic(ticketlock_classic lock; list<int> level, predicate() inv);
+
+@*/
+
+ticketlock_classic create_ticketlock_classic();
+//@ requires exists<pair<list<int>, predicate()> >(pair(?level, ?inv)) &*& level == cons(?level_max_length, ?level0) &*& length(level0) + ticketlock_classic_nb_level_dims >= level_max_length;
+//@ ensures ticketlock_classic(result, level, inv);
+
+/*@
+
+predicate ticketlock_classic_held(ticketlock_classic lock; list<int> level, predicate() inv, real frac, pair<void *, list<int> > ob);
+
+@*/
+void ticketlock_classic_acquire(ticketlock_classic lock);
+//@ requires obs(?p, ?obs) &*& [?f]ticketlock_classic(lock, ?level, ?inv) &*& forall(map(snd, obs), (all_sublevels_lt)(ticketlock_classic_nb_level_dims, level)) == true;
+//@ ensures obs(p, cons(?ob, obs)) &*& ticketlock_classic_held(lock, level, inv, f, ob) &*& inv() &*& level_lt(level, level_of(ob)) == true;
+
+void ticketlock_classic_release(ticketlock_classic lock);
+//@ requires obs(?p, ?obs) &*& ticketlock_classic_held(lock, ?level, ?inv, ?f, ?ob) &*& inv();
+//@ ensures obs(p, remove(ob, obs)) &*& [f]ticketlock_classic(lock, level, inv);
+
+void ticketlock_classic_dispose(ticketlock_classic lock);
+//@ requires ticketlock_classic(lock, ?level, ?inv);
+//@ ensures inv();
+
+#endif
