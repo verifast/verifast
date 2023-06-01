0c8804b22196245816acb775dc7ea36e9eed1c59
Niels Mommen <nielsmommen@hotmail.com>
2023-06-01 16:06:46 +0200

    Fix windows vfdeps download url

--- setup-windows.sh
+++ setup-windows.sh
@@ -22,7 +22,7 @@ script_dir=$(pwd)
 
 cd /cygdrive/c
 dl_and_unzip https://github.com/NielsMommen/vf-llvm-clang-build/releases/download/v2.0.3/vf-llvm-clang-build-$VF_LLVM_CLANG_BUILD_VERSION-Windows-MinGW-x86_64.tar.gz 0379947ECC2F475ECA7A876C858910DEAE507C2588BC8534935AE76C233AA49F 256 z
-dl_and_unzip https://github.com/verifast/vfdeps-win/releases/download/23.04/$VFDEPS_NAME-win.txz 63a593c235fbcb4d86c4cbe821aca1a943873daadfbbc1af37f0bb3f 224 j
+dl_and_unzip https://github.com/verifast/vfdeps-win/releases/download/23.04/vfdeps-e62a07d-win.txz 63a593c235fbcb4d86c4cbe821aca1a943873daadfbbc1af37f0bb3f 224 j
 
 cd $script_dir/src/cxx_frontend/ast_exporter
 cmake -S . -B build -G ninja -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_BUILD_TYPE=Release -DLLVM_INSTALL_DIR=/cygdrive/c/vf-llvm-clang-build-$VF_LLVM_CLANG_BUILD_VERSION -DVFDEPS=/cygdrive/c/vfdeps
