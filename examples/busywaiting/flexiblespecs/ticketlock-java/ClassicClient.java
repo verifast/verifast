164ea2fe1fc0da2eb491bc1e5e5787783f0bd5fe
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2023-11-09 11:56:48 +0100

    ClassicClient

---
+++ examples/busywaiting/flexiblespecs/ticketlock-java/ClassicClient.java
@@ -0,0 +1,39 @@
+final class Flag {
+  int value;
+}
+
+//@ predicate_ctor Flag_valid(Flag flag)() = flag.value |-> ?v &*& v == 0 || v == 1;
+
+final class Flipper implements Runnable {
+  Flag flag;
+  TicketlockClassic lock;
+  //@ predicate valid() = [_]flag |-> ?flag_ &*& [_]lock |-> ?lock_ &*& [_]lock_.valid(Flag_valid(flag_));
+  public void run()
+  //@ requires valid();
+  //@ ensures true;
+  {
+    lock.acquire();
+    //@ open Flag_valid(flag)();
+    flag.value = 1 - flag.value;
+    //@ close Flag_valid(flag)();
+    lock.release();
+  }
+}
+
+final class ClassicClient {
+  public static void main(String[] args)
+  //@ requires true;
+  //@ ensures true;
+  {
+    Flag flag = new Flag();
+    //@ close Flag_valid(flag)();
+    //@ close exists(Flag_valid(flag));
+    TicketlockClassic lock = new TicketlockClassic();
+    Flipper flipper = new Flipper();
+    flipper.flag = flag;
+    flipper.lock = lock;
+    //@ leak flipper.flag |-> _ &*& flipper.lock |-> _;
+    new Thread(flipper).start();
+    new Thread(flipper).start();
+  }
+}
