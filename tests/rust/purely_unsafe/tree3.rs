4766610df37025749365776fadc1bbc536eeb581
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2024-01-12 12:08:42 +0100

    [Rust] Improve the tree3.rs example

--- tests/rust/purely_unsafe/tree3.rs
+++ tests/rust/purely_unsafe/tree3.rs
@@ -29,6 +29,7 @@ unsafe fn assert(_b: bool)
 {}
 
 struct Tree {
+    data: *mut u8,
     left: *mut Tree,
     right: *mut Tree,
     parent: *mut Tree,
@@ -36,19 +37,26 @@ struct Tree {
 
 /*@
 
-inductive tree = empty(node: *mut Tree) | nonempty(node: *mut Tree, left: tree, right: tree);
+inductive tree = empty(node: *mut Tree) | nonempty(data: *mut u8, node: *mut Tree, left: tree, right: tree);
 
 fix node_count(tree: tree) -> i32 {
     match tree {
         empty(node) => 1,
-        nonempty(node, left, right) => 1 + node_count(left) + node_count(right)
+        nonempty(data, node, left, right) => 1 + node_count(left) + node_count(right)
+    }
+}
+
+fix tree_elems(tree: tree) -> list< *mut u8 > {
+    match tree {
+        empty(node) => [],
+        nonempty(data, node, left, right) => cons(data, append(tree_elems(left), tree_elems(right)))
     }
 }
 
 fix tree_root(tree: tree) -> *mut Tree {
     match tree {
         empty(node) => node,
-        nonempty(node, left, right) => node
+        nonempty(data, node, left, right) => node
     }
 }
 
@@ -58,7 +66,7 @@ lem_auto node_count_positive(tree: tree)
 {
     match tree {
         empty(node) => {}
-        nonempty(node, left, right) => {
+        nonempty(data, node, left, right) => {
             node_count_positive(left);
             node_count_positive(right);
         }
@@ -71,6 +79,7 @@ pred Tree(node: *mut Tree; parent: *mut Tree, shape: tree) =
     struct_Tree_padding(node) &*&
     node as usize & 1 == 0 &*&
     pointer_within_limits(node as *mut u8 + 1) == true &*&
+    (*node).data |-> ?data &*&
     (*node).left |-> ?left &*&
     (*node).right |-> ?right &*&
     (*node).parent |-> parent &*&
@@ -83,7 +92,7 @@ pred Tree(node: *mut Tree; parent: *mut Tree, shape: tree) =
         right != 0 &*&
         Tree(left, node, ?leftShape) &*&
         Tree(right, node, ?rightShape) &*&
-        shape == nonempty(node, leftShape, rightShape)
+        shape == nonempty(data, node, leftShape, rightShape)
     };
 
 lem_auto Tree_inv()
@@ -96,65 +105,65 @@ lem_auto Tree_inv()
     close Tree(node, parent, shape);
 }
 
-pred stack(parent: *mut Tree, current: *mut Tree, cShape: tree, root: *mut Tree, rootShape: tree, stepsLeft: i32, countTodo: i32) =
+pred stack(parent: *mut Tree, current: *mut Tree, cShape: tree, root: *mut Tree, rootShape: tree, stepsLeft: i32, elems_todo: list< *mut u8 >) =
     current != 0 &*&
     if parent == 0 {
         root == current &*&
         rootShape == cShape &*&
         stepsLeft == 0 &*&
-        countTodo == 0
+        elems_todo == []
     } else {
         alloc_block(parent, std::mem::size_of::<Tree>()) &*&
         struct_Tree_padding(parent) &*&
         parent as usize & 1 == 0 &*&
         pointer_within_limits(parent as *mut u8 + 1) == true &*&
+        (*parent).data |-> ?data &*&
         (*parent).left |-> ?left &*&
         (*parent).right |-> ?right &*&
         (*parent).parent |-> current &*&
         if right as usize & 1 != 0 {
             pointer_within_limits(right as *mut u8 - 1) == true &*&
             Tree(left, parent, ?rightShape) &*& left != 0 &*&
-            stack((right as *mut u8 - 1) as *mut Tree, parent, nonempty(parent, cShape, rightShape), root, rootShape, ?stepsLeft1, ?countTodo0) &*&
-            stepsLeft1 >= 0 &*& countTodo0 >= 0 &*&
+            stack((right as *mut u8 - 1) as *mut Tree, parent, nonempty(data, parent, cShape, rightShape), root, rootShape, ?stepsLeft1, ?elems_todo0) &*&
+            stepsLeft1 >= 0 &*&
             stepsLeft == node_count(rightShape) * 2 + 1 + stepsLeft1 &*&
-            countTodo == countTodo0 + node_count(rightShape)
+            elems_todo == append(tree_elems(rightShape), elems_todo0)
         } else {
             left as usize & 1 != 0 &*&
             pointer_within_limits(left as *mut u8 - 1) == true &*&
             Tree(right, parent, ?leftShape) &*& right != 0 &*&
-            stack((left as *mut u8 - 1) as *mut Tree, parent, nonempty(parent, leftShape, cShape), root, rootShape, ?stepsLeft1, countTodo) &*&
+            stack((left as *mut u8 - 1) as *mut Tree, parent, nonempty(data, parent, leftShape, cShape), root, rootShape, ?stepsLeft1, elems_todo) &*&
             stepsLeft1 >= 0 &*& stepsLeft == 1 + stepsLeft1
         }
     };
 
-pred inv_(x_is_new: bool, x: *mut Tree, root: *mut Tree, rootShape: tree, stepsLeft: i32, countTodo: i32) =
+pred inv_(x_is_new: bool, x: *mut Tree, root: *mut Tree, rootShape: tree, stepsLeft: i32, elems_todo: list< *mut u8 >) =
     if x_is_new {
-        Tree(x, ?parent, ?xShape) &*& stack(parent, x, xShape, root, rootShape, ?stepsLeft1, ?countTodo0) &*&
+        Tree(x, ?parent, ?xShape) &*& stack(parent, x, xShape, root, rootShape, ?stepsLeft1, ?elems_todo0) &*&
         stepsLeft1 >= 0 &*& stepsLeft == node_count(xShape) * 2 - 1 + stepsLeft1 &*&
-        countTodo0 >= 0 &*&
-        countTodo == node_count(xShape) + countTodo0
+        elems_todo == append(tree_elems(xShape), elems_todo0)
     } else {
-        stack(x, ?child, ?childShape, root, rootShape, stepsLeft, countTodo) &*& stepsLeft >= 0 &*& countTodo >= 0 &*&
+        stack(x, ?child, ?childShape, root, rootShape, stepsLeft, elems_todo) &*& stepsLeft >= 0 &*&
         Tree(child, x, childShape)
     };
 
-pred_fam TreeVisitor(idx: *std::type_info)(data: *mut u8, count_todo: i32, info: any);
+pred_fam TreeVisitor(idx: *std::type_info)(visitor_data: *mut u8, elems_todo: list< *mut u8 >, info: any);
 
 @*/
 
 trait TreeVisitor {
 
-    unsafe fn visit(data: *mut u8, node: *mut Tree);
-    //@ req TreeVisitor(typeid(Self))(data, ?countTodo, ?info) &*& 0 < countTodo;
-    //@ ens TreeVisitor(typeid(Self))(data, countTodo - 1, info);
+    unsafe fn visit(visitor_data: *mut u8, node_data: *mut u8);
+    //@ req TreeVisitor(typeid(Self))(visitor_data, cons(node_data, ?elems), ?info);
+    //@ ens TreeVisitor(typeid(Self))(visitor_data, elems, info);
 
 }
 
 impl Tree {
 
-    unsafe fn new_with_children(left: *mut Tree, right: *mut Tree) -> *mut Tree
+    unsafe fn new_nonempty(data: *mut u8, left: *mut Tree, right: *mut Tree) -> *mut Tree
     //@ req Tree(left, _, ?leftShape) &*& Tree(right, _, ?rightShape);
-    //@ ens Tree(result, 0, nonempty(result, leftShape, rightShape));
+    //@ ens Tree(result, 0, nonempty(data, result, leftShape, rightShape));
     {
         let result = std::alloc::alloc(std::alloc::Layout::new::<Tree>()) as *mut Tree;
         if result.is_null() {
@@ -162,6 +171,7 @@ impl Tree {
         }
         //@ assume(result as usize & 1 == 0);
         //@ close_struct(result);
+        (*result).data = data;
         (*result).left = left;
         (*left).parent = result;
         (*result).right = right;
@@ -170,7 +180,7 @@ impl Tree {
         result
     }
 
-    unsafe fn new() -> *mut Tree
+    unsafe fn new_empty() -> *mut Tree
     //@ req true;
     //@ ens Tree(result, 0, empty(result));
     {
@@ -180,6 +190,7 @@ impl Tree {
         }
         //@ assume(result as usize & 1 == 0);
         //@ close_struct(result);
+        (*result).data = std::ptr::null_mut();
         (*result).left = std::ptr::null_mut();
         (*result).right = std::ptr::null_mut();
         (*result).parent = std::ptr::null_mut();
@@ -187,32 +198,30 @@ impl Tree {
     }
 
     unsafe fn visit<V: TreeVisitor>(root: *mut Tree, visitor_data: *mut u8)
-    //@ req Tree(root, 0, ?shape) &*& TreeVisitor(typeid(V))(visitor_data, node_count(shape), ?info);
-    //@ ens Tree(root, 0, shape) &*& TreeVisitor(typeid(V))(visitor_data, 0, info);
+    //@ req Tree(root, 0, ?shape) &*& TreeVisitor(typeid(V))(visitor_data, tree_elems(shape), ?info);
+    //@ ens Tree(root, 0, shape) &*& TreeVisitor(typeid(V))(visitor_data, [], info);
     {
         Self::visit0::<V>(root, true, visitor_data)
     }
 
     unsafe fn visit0<V: TreeVisitor>(mut x: *mut Tree, mut x_is_new: bool, visitor_data: *mut u8)
-    //@ req Tree(?root, 0, ?rootShape) &*& x == root &*& x_is_new &*& TreeVisitor(typeid(V))(visitor_data, node_count(rootShape), ?info);
-    //@ ens Tree(root, 0, rootShape) &*& TreeVisitor(typeid(V))(visitor_data, 0, info);
+    //@ req Tree(?root, 0, ?rootShape) &*& x == root &*& x_is_new &*& TreeVisitor(typeid(V))(visitor_data, tree_elems(rootShape), ?info);
+    //@ ens Tree(root, 0, rootShape) &*& TreeVisitor(typeid(V))(visitor_data, [], info);
     {
         //@ Tree_inv();
-        //@ close stack(0, root, rootShape, root, rootShape, 0, 0);
+        //@ close stack(0, root, rootShape, root, rootShape, 0, []);
         //@ close inv_(true, x, root, rootShape, _, _);
         loop {
-            //@ inv inv_(x_is_new, x, root, rootShape, ?stepsLeft, ?countTodo) &*& x != 0 &*& TreeVisitor(typeid(V))(visitor_data, countTodo, info);
+            //@ inv inv_(x_is_new, x, root, rootShape, ?stepsLeft, ?elems_todo) &*& x != 0 &*& TreeVisitor(typeid(V))(visitor_data, elems_todo, info);
             //@ open inv_(_, _, _, _, _, _);
             //@ if x_is_new == false { open stack(x, _, _, _, _, _, _); }
-            if x_is_new {
-                V::visit(visitor_data, x);
-            }
             if (*x).left.is_null() {
                 x = (*x).parent;
                 x_is_new = false;
                 //@ close inv_(false, x, root, rootShape, _, _);
             } else {
                 if x_is_new {
+                    V::visit(visitor_data, (*x).data);
                     //@ set_lsb((*x).parent as *mut u8);
                     (*x).parent = ((*x).parent as *mut u8).offset(1) as *mut Tree;
                 }
@@ -228,6 +237,8 @@ impl Tree {
                 /*@
                 if x_is_new {
                     assert Tree(y, x, ?leftShape);
+                    assert stack(_, x, nonempty(_, _, _, ?rightShape), root, rootShape, _, ?elems_todo0);
+                    append_assoc(tree_elems(leftShape), tree_elems(rightShape), elems_todo0);
                     close stack(x, y, leftShape, root, rootShape, ?stepsLeft1, _);
                     close inv_(true, y, root, rootShape, _, _);
                 } else {
@@ -265,32 +276,36 @@ struct CountingTreeVisitor;
 
 inductive wrap<t> = wrap(t);
 
-pred_fam_inst TreeVisitor(typeid(CountingTreeVisitor))(data: *mut u8, countTodo: i32, maxCount: wrap<i32>) =
+pred_fam_inst TreeVisitor(typeid(CountingTreeVisitor))(visitor_data: *mut u8, elems_todo: list< *mut u8 >, maxCount: wrap<i32>) =
     maxCount == wrap(?m) &*& m <= 0x7fffffff &*&
-    *(data as *mut i32) |-> m - countTodo;
+    *(visitor_data as *mut i32) |-> m - length(elems_todo);
 
 @*/
 
 impl TreeVisitor for CountingTreeVisitor {
 
-    unsafe fn visit(data: *mut u8, _node: *mut Tree)
-    //@ req TreeVisitor(typeid(CountingTreeVisitor))(data, ?countTodo, ?info) &*& 0 < countTodo;
-    //@ ens TreeVisitor(typeid(CountingTreeVisitor))(data, countTodo - 1, info);
+    unsafe fn visit(visitor_data: *mut u8, _node_data: *mut u8)
+    //@ req TreeVisitor(typeid(CountingTreeVisitor))(visitor_data, cons(_node_data, ?elems_todo), ?info);
+    //@ ens TreeVisitor(typeid(CountingTreeVisitor))(visitor_data, elems_todo, info);
     {
-        //@ open TreeVisitor(typeid(CountingTreeVisitor))(data, countTodo, wrap(?maxCount));
-        let count = *(data as *mut i32);
+        //@ open TreeVisitor(typeid(CountingTreeVisitor))(visitor_data, _, wrap(?maxCount));
+        let count = *(visitor_data as *mut i32);
         //@ produce_limits(count);
-        *(data as *mut i32) = count + 1;
-        //@ close TreeVisitor(typeid(CountingTreeVisitor))(data, countTodo - 1, wrap(maxCount));
+        *(visitor_data as *mut i32) = count + 1;
+        //@ close TreeVisitor(typeid(CountingTreeVisitor))(visitor_data, elems_todo, wrap(maxCount));
     }
 
 }
 
 fn main() {
     unsafe {
-        let left_child = Tree::new();
-        let right_child = Tree::new();
-        let root = Tree::new_with_children(left_child, right_child);
+        let empty1 = Tree::new_empty();
+        let empty2 = Tree::new_empty();
+        let nonempty1 = Tree::new_nonempty(1 as *mut u8, empty1, empty2);
+        let empty3 = Tree::new_empty();
+        let empty4 = Tree::new_empty();
+        let nonempty2 = Tree::new_nonempty(2 as *mut u8, empty3, empty4);
+        let root = Tree::new_nonempty(3 as *mut u8, nonempty1, nonempty2);
 
         let count = std::alloc::alloc(std::alloc::Layout::new::<i32>()) as *mut i32;
         if count.is_null() {
@@ -299,9 +314,9 @@ fn main() {
         //@ u8s__to_integer__(count, 4, true);
         *count = 0;
 
-        //@ close TreeVisitor(typeid(CountingTreeVisitor))(count as *u8, 3, wrap(3));
+        //@ close TreeVisitor(typeid(CountingTreeVisitor))(count as *mut u8, [3 as *u8, 1 as *u8, 2 as *u8], wrap(3));
         Tree::visit::<CountingTreeVisitor>(root, count as *mut u8);
-        //@ open TreeVisitor(typeid(CountingTreeVisitor))(count as *u8, 0, wrap(3));
+        //@ open TreeVisitor(typeid(CountingTreeVisitor))(count as *mut u8, [], wrap(3));
         assert(*count == 3);
         
         //@ integer__to_u8s(count, 4, true);
