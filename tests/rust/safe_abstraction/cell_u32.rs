a6a86f452c65fe9af57bc28b2ba0a9b5f2bc8df6
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2024-01-17 15:34:30 +0100

    [Rust] Adds support for thread local masks

--- tests/rust/safe_abstraction/cell_u32.rs
+++ tests/rust/safe_abstraction/cell_u32.rs
@@ -7,7 +7,7 @@ pub struct CellU32 {
 // Interpretation
 // `OWN` for Cell<u32>
 // [[cell(tau)]].OWN(t, vs) = [[tau]].OWN(t, vs)
-pred CellU32_own(t: thread_id_t, v: u32) = true; // The `v` parameter type carries the info
+pred CellU32_own(t: thread_id_t, v: u32;) = true; // The `v` parameter type carries the info
 
 /* A note on `|= cell(tau) copy` judgement:
 In RustBelt `|= tau copy => |= cell(tau) copy` but it is not the case in Rust as it is prohibited
@@ -18,12 +18,12 @@ However, we do not need `|= u32 copy` explicitly because VeriFast allows copying
 i.e. in VeriFast the `[[u32]].OWN` is implicitly persistent.
 */
 
-pred_ctor CellU32_nonatomic_borrow_content(l: *CellU32, t: thread_id_t)() =
+pred_ctor CellU32_nonatomic_borrow_content(l: *CellU32, t: thread_id_t)(;) =
   CellU32_v(l, ?v) &*& struct_CellU32_padding(l) &*& CellU32_own(t, v);
 
 // `SHR` for Cell<u32>
 pred CellU32_share(k: lifetime_t, t: thread_id_t, l: *CellU32) =
-  [_]nonatomic_borrow(k, t, CellU32_nonatomic_borrow_content(l, t));
+  [_]nonatomic_borrow(k, t, MaskNshrSingle(l), CellU32_nonatomic_borrow_content(l, t));
 
 // Proof obligations
 lem CellU32_share_mono(k: lifetime_t, k1: lifetime_t, t: thread_id_t, l: *CellU32)
@@ -31,7 +31,8 @@ lem CellU32_share_mono(k: lifetime_t, k1: lifetime_t, t: thread_id_t, l: *CellU3
   ens [_]CellU32_share(k1, t, l);
 {
   open CellU32_share(k, t, l);
-  nonatomic_borrow_mono(k, k1, t, CellU32_nonatomic_borrow_content(l, t));
+  assert [_]nonatomic_borrow(k, t, ?m, _);
+  nonatomic_borrow_mono(k, k1, t, m, CellU32_nonatomic_borrow_content(l, t));
   close CellU32_share(k1, t, l);
   leak CellU32_share(k1, t, l);
 }
@@ -51,7 +52,7 @@ lem CellU32_share_full(k: lifetime_t, t: thread_id_t, l: *CellU32)
       full_borrow_implies(k, CellU32_full_borrow_content(t, l), CellU32_nonatomic_borrow_content(l, t));
     }
   }
-  full_borrow_into_nonatomic_borrow(k, t, CellU32_nonatomic_borrow_content(l, t));
+  full_borrow_into_nonatomic_borrow(k, t, MaskNshrSingle(l), CellU32_nonatomic_borrow_content(l, t));
   close CellU32_share(k, t, l);
   leak CellU32_share(k, t, l);
 }
@@ -59,7 +60,9 @@ lem CellU32_share_full(k: lifetime_t, t: thread_id_t, l: *CellU32)
 
 impl CellU32 {
     fn new(u: u32) -> CellU32 {
-        let c = CellU32 { v: std::cell::UnsafeCell::new(u) };
+        let c = CellU32 {
+            v: std::cell::UnsafeCell::new(u),
+        };
         //@ close CellU32_own(_t, u);
         c
     }
@@ -67,15 +70,18 @@ impl CellU32 {
     /* VeriFast generates the contract of the safe functions based on the function's semantic type */
     fn get<'a>(&'a self) -> u32 {
         //@ open CellU32_share(a, _t, self);
-        //@ open_nonatomic_borrow(a, _t, _q_a);
-        //@ open CellU32_nonatomic_borrow_content(self, _t)();
+        //@ assert [_]nonatomic_borrow(a, _t, ?_m, _);
+        //@ open thread_token(_t);
+        //@ thread_token_split(_t, MaskTop, _m);
+        //@ open_nonatomic_borrow(a, _t, _m, _q_a);
         let v = unsafe { *self.v.get() };
-        //@ close CellU32_nonatomic_borrow_content(self, _t)();
         //@ close_nonatomic_borrow();
+        //@ thread_token_merge(_t, mask_diff(MaskTop, _m), _m);
+        //@ close thread_token(_t);
         v
     }
 
-    /* User can also write the contract of public functions to have it explicit.
+    /* User can also write the contract of safe functions to have it explicit.
     Verifast would check the compatibility of the contract with the function semantic type. */
     fn set<'a>(&'a self, u: u32)
     //@ req thread_token(?t) &*& [?q]lifetime_token(?a) &*& CellU32_share(a, t, self);
@@ -83,14 +89,43 @@ impl CellU32 {
     {
         let p = self.v.get();
         //@ open CellU32_share(a, t, self);
-        //@ open_nonatomic_borrow(a, t, q);
-        //@ open CellU32_nonatomic_borrow_content(self, t)();
+        //@ assert [_]nonatomic_borrow(a, t, ?m, _);
+        //@ open thread_token(t);
+        //@ thread_token_split(t, MaskTop, m);
+        //@ open_nonatomic_borrow(a, t, m, q);
         unsafe {
             *p = u;
         }
-        //@ open CellU32_own(t, _);
-        //@ close CellU32_own(t, u);
-        //@ close CellU32_nonatomic_borrow_content(self, t)();
         //@ close_nonatomic_borrow();
+        //@ thread_token_merge(t, mask_diff(MaskTop, m), m);
+        //@ close thread_token(t);
+    }
+
+    fn swap<'a>(&'a self, other: &'a Self) {
+        //@ open CellU32_share(a, _t, self);
+        //@ open CellU32_share(a, _t, other);
+        if self as *const CellU32 == other as *const CellU32 {
+            return;
+        }
+        //@ assert [_]nonatomic_borrow(a, _t, ?ms, CellU32_nonatomic_borrow_content(self, _t));
+        //@ assert [_]nonatomic_borrow(a, _t, ?mo, CellU32_nonatomic_borrow_content(other, _t));
+        //@ open thread_token(_t);
+        //@ thread_token_split(_t, MaskTop, ms);
+        //@ thread_token_split(_t, mask_diff(MaskTop, ms), mo);
+        //@ open_nonatomic_borrow(a, _t, ms, _q_a/2);
+        //@ open_nonatomic_borrow(a, _t, mo, _q_a/2);
+        let ps = self.v.get();
+        let po = other.v.get();
+        unsafe {
+            let tmp = *po;
+            *po = *ps;
+            *ps = tmp;
+        }
+        //@ assert partial_thread_token(_t, ?rem);
+        //@ close_nonatomic_borrow();
+        //@ close_nonatomic_borrow();
+        //@ thread_token_merge(_t, rem, mo);
+        //@ thread_token_merge(_t, mask_diff(MaskTop, ms), ms);
+        //@ close thread_token(_t);
     }
 }
