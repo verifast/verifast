53a2a27310bf44755c6b745c7e01c2f59ed83505
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2024-04-02 11:05:27 +0200

    dune-project: Add (implicit_transitive_deps false)
    Also adds rust_fe.mli. Rebuilding after working on the MIR translator
    is now much faster.
    
    Unfortunately, I could not figure out how to keep the automatic
    fallback version of 'linemarks', so now the default is hardcoded.

---
+++ src/rust_frontend/rust_fe.mli
@@ -0,0 +1,20 @@
+module type RUST_FE_ARGS =
+  sig
+    val data_model_opt : Ast.data_model option
+    val report_should_fail : string -> Ast.loc0 -> unit
+    val report_range : Lexer.range_kind -> Ast.loc0 -> unit
+    val report_macro_call : Ast.loc0 -> Ast.loc0 -> unit
+    val verbose_flags : string list
+  end
+module Make :
+  functor (_ : RUST_FE_ARGS) ->
+    sig
+      exception RustFrontend of string
+      val parse_rs_file :
+        string list ->
+        string list ->
+        string ->
+        (Ast.loc * (Lexer.include_kind * string * string) * 'a list *
+         Ast.package list)
+        list * Ast.package list * Verifast0.debug_info option
+    end
