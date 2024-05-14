6a4c87ae868e5d7f330431c38807e366154d7203
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2024-05-14 18:59:35 +0200

    WIP

--- tests/rust/safe_abstraction/rc_u32.rs
+++ tests/rust/safe_abstraction/rc_u32.rs
@@ -1,6 +1,25 @@
 #![feature(negative_impls)]
 use std::{cell::UnsafeCell, process::abort, ptr::NonNull};
 
+/*@
+pred nonatomic_inv(t: thread_id_t, m: mask_t, P: pred());
+fix add<T>(x: real, y: real) -> real { x + y }
+pred_ctor RcBoxU32_na_inv_cnt(ptr: *RcBoxU32)() = (*ptr).strong |-> ?strong &*& [?qv](*ptr).value |-> ?value &*& exists(?qs) &*& length(qs) == strong - 1 &*& fold_left(0 as real, add, qs) + qv == 1;
+
+pred RcU32_own(t: thread_id_t, ptr: std::ptr::NonNull<RcBoxU32>) = [_]nonatomic_inv(t, MaskNshrSingle(std::ptr::NonNull_ptr(ptr)), RcBoxU32_na_inv_cnt(std::ptr::NonNull_ptr(ptr)));
+pred RcU32_share(k: lifetime_t, t: thread_id_t, l: *RcU32) = true;
+lem RcU32_share_mono(k: lifetime_t, k1: lifetime_t, t: thread_id_t, l: *RcU32)
+    req lifetime_inclusion(k1, k) == true &*& [_]RcU32_share(k, t, l);
+    ens [_]RcU32_share(k1, t, l);
+{}
+lem RcU32_share_full(k: lifetime_t, t: thread_id_t, l: *RcU32)
+    req atomic_mask(Nlft) &*& full_borrow(k, RcU32_full_borrow_content(t, l)) &*& [?q]lifetime_token(k);
+    ens atomic_mask(Nlft) &*& [_]RcU32_share(k, t, l) &*& [q]lifetime_token(k);
+{
+    
+}
+@*/
+
 pub struct RcU32 {
     ptr: NonNull<RcBoxU32>,
 }
@@ -24,7 +43,10 @@ impl RcU32 {
         }
     }
 
-    unsafe fn from_inner(ptr: NonNull<RcBoxU32>) -> Self {
+    unsafe fn from_inner(ptr: NonNull<RcBoxU32>) -> Self
+    //@ req true;
+    //@ ens true;
+    {
         Self { ptr }
     }
 
@@ -32,7 +54,7 @@ impl RcU32 {
         unsafe { self.ptr.as_ref() }
     }
 }
-
+/*
 impl std::ops::Deref for RcU32 {
     type Target = u32;
 
@@ -69,3 +91,4 @@ impl Drop for RcU32 {
         }
     }
 }
+*/
