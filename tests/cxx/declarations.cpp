400ff961d7badb574c2a5fd60626ac33081b43d6
Christian Geldermann <git@peoplez.org>
2024-09-22 00:45:28 +0200

    [C++] Added support for default values of function parameters

--- tests/cxx/declarations.cpp
+++ tests/cxx/declarations.cpp
@@ -1,3 +1,18 @@
 void multiple_unnamed_arguments(int, int, long);
 //@ requires true;
 //@ ensures true;
+
+int default_valued_arguments(int j = 0, int k = 42)
+//@ requires true;
+//@ ensures result == j + k;
+{
+    return j + k;
+}
+
+int main()
+//@ requires true;
+//@ ensures true;
+{
+    int res = default_valued_arguments(3);
+    //@ assert(res == 45);
+}
