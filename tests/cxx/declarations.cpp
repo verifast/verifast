05129731c8cb6596e5e533fee8b5fdff98c0e455
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2024-09-22 05:15:33 +0200

    Slight cleanup

--- tests/cxx/declarations.cpp
+++ tests/cxx/declarations.cpp
@@ -3,7 +3,7 @@ void multiple_unnamed_arguments(int, int, long);
 //@ ensures true;
 
 int default_valued_arguments(int j = 0, int k = 42)
-//@ requires true;
+//@ requires -10000 <= j + k &*& j + k <= 10000;
 //@ ensures result == j + k;
 {
     return j + k;
