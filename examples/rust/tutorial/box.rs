4a27f76bbda18a863ae7c91ca67262728cc13e5a
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2024-10-27 18:04:40 +0100

    WIP

--- examples/rust/tutorial/box.rs
+++ examples/rust/tutorial/box.rs
@@ -5,44 +5,55 @@ pub struct Box<T> {
 }
 
 /*@
-pred<T> <Box<T>>.own(t, b) = *b.ptr |-> ?v &*& <T>.own(t, v);
-pred_ctor frac_borrow_content<T>(l: *Box<T>, ptr: *T)(;) = (*l).ptr |-> ptr;
-pred<T> <Box<T>>.share(k, t, l) = [_]exists(?ptr) &*& [_]frac_borrow(k, frac_borrow_content(l, ptr)) &*& [_](<T>.share)(k, t, ptr);
+pred<T> <Box<T>>.own(t, b) = *b.ptr |-> ?v &*& <T>.own(t, v) &*& std::alloc::alloc_block(b.ptr as *u8, std::alloc::Layout::new_::<T>());
+pred_ctor field_ptr_chunk<T>(l: *Box<T>, ptr: *T)(;) = (*l).ptr |-> ptr;
+pred<T> <Box<T>>.share(k, t, l) = [_]exists(?p) &*& [_]frac_borrow(k, field_ptr_chunk(l, p)) &*& [_](<T>.share)(k, t, p);
 
 lem Box_share_mono<T>(k: lifetime_t, k1: lifetime_t, t: thread_id_t, l: *Box<T>)
     req type_interp::<T>() &*& lifetime_inclusion(k1, k) == true &*& [_](<Box<T>>.share)(k, t, l);
     ens type_interp::<T>() &*& [_](<Box<T>>.share)(k1, t, l);
 {
+    // We use the monotonicity of `frac_borrow` and `<T>.share` predicate
     open Box_share::<T>(k, t, l);
     assert [_]exists(?ptr);
-    frac_borrow_mono(k, k1, frac_borrow_content(l, ptr));
+    frac_borrow_mono(k, k1, field_ptr_chunk(l, ptr));
     share_mono(k, k1, t, ptr);
     close Box_share::<T>(k1, t, l);
     leak Box_share(k1, t, l);
 }
 
 lem Box_share_full<T>(k: lifetime_t, t: thread_id_t, l: *Box<T>)
-    req atomic_mask(Nlft) &*& [?q]lifetime_token(k) &*& full_borrow(k, Box_full_borrow_content(t, l));
-    ens atomic_mask(Nlft) &*& [q]lifetime_token(k) &*& [_](<Box<T>>.share)(k, t, l);
+    req type_interp::<T>() &*& atomic_mask(Nlft) &*& [?q]lifetime_token(k) &*& full_borrow(k, Box_full_borrow_content(t, l));
+    ens type_interp::<T>() &*& atomic_mask(Nlft) &*& [q]lifetime_token(k) &*& [_](<Box<T>>.share)(k, t, l);
 {
-    let klong = open_full_borrow_strong_m(k, BoxU8_full_borrow_content(t, l), q);
-    open BoxU8_full_borrow_content(t, l)();
-    open BoxU8_own(t, ?ptr);
-    close sep(Box_ptr(l, ptr), u8_full_borrow_content(t, ptr))();
-    produce_lem_ptr_chunk full_borrow_convert_strong(ctx(l, ptr), sep(Box_ptr(l, ptr), u8_full_borrow_content(t, ptr)), klong, BoxU8_full_borrow_content(t, l))() {
-        open sep(Box_ptr(l, ptr), u8_full_borrow_content(t, ptr))();
-        close BoxU8_own(t, ptr);
-        close BoxU8_full_borrow_content(t, l)();
+    // We convert `full_borrow(k, Box_full_borrow_content(t, t))` to `full_borrow(k, (*l).ptr |-> p &*& <T>.full_borrow_content(t, p));
+    let klong = open_full_borrow_strong_m(k, Box_full_borrow_content(t, l), q);
+    open Box_full_borrow_content::<T>(t, l)();
+    open Box_own::<T>(t, ?b);
+    close_full_borrow_content::<T>(t, b.ptr);
+    close field_ptr_chunk::<T>(l, b.ptr)();
+    close sep(field_ptr_chunk(l, b.ptr), (<T>.full_borrow_content)(t, b.ptr))();
+    {
+    pred ctx(;) = std::alloc::alloc_block(b.ptr as *u8, std::alloc::Layout::new_::<T>());
+    close ctx();
+    produce_lem_ptr_chunk full_borrow_convert_strong(ctx, sep(field_ptr_chunk(l, b.ptr), (<T>.full_borrow_content)(t, b.ptr)), klong, (<Box<T>>.full_borrow_content)(t, l))() {
+        open ctx();
+        open sep(field_ptr_chunk(l, b.ptr), (<T>.full_borrow_content)(t, b.ptr))();
+        open field_ptr_chunk::<T>(l, b.ptr)();
+        open_full_borrow_content::<T>(t, b.ptr);
+        close Box_own::<T>(t, b);
+        close Box_full_borrow_content::<T>(t, l)();
     }{
-        close_full_borrow_strong_m(klong, BoxU8_full_borrow_content(t, l), sep(Box_ptr(l, ptr), u8_full_borrow_content(t, ptr)));
+        close_full_borrow_strong_m(klong, (<Box<T>>.full_borrow_content)(t, l), sep(field_ptr_chunk(l, b.ptr), (<T>.full_borrow_content)(t, b.ptr)));
     }
-    full_borrow_mono(klong, k, sep(Box_ptr(l, ptr), u8_full_borrow_content(t, ptr)));
-    full_borrow_split_m(k, Box_ptr(l, ptr), u8_full_borrow_content(t, ptr));
-    full_borrow_into_frac_m(k, Box_ptr(l, ptr));
-    full_borrow_into_frac_m(k, u8_full_borrow_content(t, ptr));
-    leak exists(ptr);
-    close BoxU8_share(k, t, l);
-    leak BoxU8_share(k, t, l);
+    }//ctx
+    full_borrow_mono(klong, k, sep(field_ptr_chunk(l, b.ptr), (<T>.full_borrow_content)(t, b.ptr)));
+    full_borrow_split_m(k, field_ptr_chunk(l, b.ptr), (<T>.full_borrow_content)(t, b.ptr));
+    full_borrow_into_frac_m(k, field_ptr_chunk(l, b.ptr));
+    share_full_borrow_m::<T>(k, t, b.ptr);
+    leak exists(b.ptr);
+    close Box_share::<T>(k, t, l);
+    leak Box_share::<>(k, t, l);
 }
 @*/
 
@@ -67,12 +78,14 @@ impl<T> Box<T> {
 impl<T> Drop for Box<T> {
     fn drop<'a>(&'a mut self)
     //@ req thread_token(?t) &*& Box_full_borrow_content::<T>(t, self)();
-    //@ ens thread_token(t) &*& (*self).ptr |-> ?_ &*& struct_Box_padding(self);
+    //@ ens thread_token(t) &*& (*self).ptr |-> ?_;
     {
         unsafe {
-            //@ open BoxU8_full_borrow_content(t, self)();
-            //@ assert (*self).ptr |-> ?ptr;
-            //@ to_u8s_(ptr);
+            //@ open Box_full_borrow_content::<T>(t, self)();
+            //@ open Box_own::<T>(t, *self);
+            //@ close_full_borrow_content::<T>(t, (*self).ptr);
+            std::ptr::drop_in_place(self.ptr);
+            //@ to_u8s_((*self).ptr);
             dealloc(self.ptr as *mut u8, Layout::new::<T>());
         }
     }
