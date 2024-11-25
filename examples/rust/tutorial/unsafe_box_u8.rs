36670770610e843437926f41ff4aefd0ca68f193
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2024-11-25 21:00:29 +0100

    [Rust] Auto open/close_points_to in unsafe_box_u8 (This message is not needed anymore in final PR)

--- examples/rust/tutorial/unsafe_box_u8.rs
+++ examples/rust/tutorial/unsafe_box_u8.rs
@@ -7,35 +7,62 @@ pub struct BoxU8 {
 impl BoxU8 {
     pub unsafe fn new(v: u8) -> BoxU8
     //@ req true;
-    //@ ens *result.ptr |-> v &*& std::alloc::alloc_block(result.ptr, std::alloc::Layout::new_::<u8>()) &*& object_pointer_within_limits(result.ptr, 1) == true;
+    //@ ens *result.ptr |-> v &*& std::alloc::alloc_block(result.ptr, std::alloc::Layout::new_::<u8>());
     {
         let l = Layout::new::<u8>();
         let p = alloc(l);
         if p.is_null() {
-            handle_alloc_error(l)
+            handle_alloc_error(l);
         }
         *p = v;
         Self { ptr: p }
     }
 
     pub unsafe fn drop(this: BoxU8)
-    //@ req *this.ptr |-> ?v &*& std::alloc::alloc_block(this.ptr, std::alloc::Layout::new_::<u8>()) &*& object_pointer_within_limits(this.ptr, 1) == true;
+    //@ req *this.ptr |-> _ &*& std::alloc::alloc_block(this.ptr, std::alloc::Layout::new_::<u8>());
     //@ ens true;
     {
+        //@ to_u8s_(this.ptr);
         dealloc(this.ptr, Layout::new::<u8>());
     }
 
     pub unsafe fn from_raw(raw: *mut u8) -> BoxU8
-    //@ req *raw |-> ?v &*& std::alloc::alloc_block(raw, std::alloc::Layout::new_::<u8>()) &*& object_pointer_within_limits(raw, 1) == true;
-    //@ ens *result.ptr |-> v &*& std::alloc::alloc_block(result.ptr, std::alloc::Layout::new_::<u8>()) &*& object_pointer_within_limits(result.ptr, 1) == true;
+    //@ req *raw |-> ?v &*& std::alloc::alloc_block(raw, std::alloc::Layout::new_::<u8>());
+    //@ ens *result.ptr |-> v &*& std::alloc::alloc_block(result.ptr, std::alloc::Layout::new_::<u8>());
     {
         Self { ptr: raw }
     }
 
     pub unsafe fn into_raw(this: BoxU8) -> *mut u8
-    //@ req *this.ptr |-> ?v &*& std::alloc::alloc_block(this.ptr, std::alloc::Layout::new_::<u8>()) &*& object_pointer_within_limits(this.ptr, 1) == true;
-    //@ ens *result |-> v &*& std::alloc::alloc_block(result, std::alloc::Layout::new_::<u8>()) &*& object_pointer_within_limits(result, 1) == true;
+    //@ req *this.ptr |-> ?v &*& std::alloc::alloc_block(this.ptr, std::alloc::Layout::new_::<u8>());
+    //@ ens *result |-> v &*& std::alloc::alloc_block(result, std::alloc::Layout::new_::<u8>());
     {
         this.ptr
     }
+
+    pub unsafe fn get(p: *const BoxU8) -> u8
+    //@ req [?qb](*p |-> ?b) &*& [?qv](*b.ptr |-> ?v);
+    //@ ens [qb](*p |-> b) &*& [qv](*b.ptr |-> v) &*& result == v;
+    {
+        *(*p).ptr
+    }
+
+    pub unsafe fn set(p: *mut BoxU8, new: u8)
+    //@ req [?qb](*p |-> ?b) &*& *b.ptr |-> _;
+    //@ ens [qb](*p |-> b) &*& *b.ptr |-> new;
+    {
+        *(*p).ptr = new;
+    }
+}
+
+unsafe fn main()
+//@ req true;
+//@ ens true;
+{
+    let mut b = BoxU8::new(0);
+    let p = &mut b as *mut BoxU8;
+    assert!(BoxU8::get(p) == 0);
+    BoxU8::set(p, 42);
+    assert!(BoxU8::get(p) == 42);
+    BoxU8::drop(b);
 }
