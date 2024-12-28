11df1d0300ec456ae06561d297f27c37bc1d81aa
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2024-12-28 20:14:31 +0000

    Seems to work

--- setenv.sh
+++ setenv.sh
@@ -1,10 +1,18 @@
+WORKSPACEFOLDER="`cd $(dirname "${BASH_SOURCE[0]}") && pwd`"
 if [[ -z "$VFDEPS_NAME" ]]; then
-    . `dirname "${BASH_SOURCE[0]}"`/config.sh
+    . "$WORKSPACEFOLDER/config.sh"
 fi
 case ":${PATH}:" in
     *:"/tmp/$VFDEPS_NAME/bin":*)
         ;;
     *)
         export PATH="/tmp/$VFDEPS_NAME/bin:$PATH"
+        ;;
+esac
+case ":$WORKSPACEFOLDER/bin:" in
+    *:"/$WORKSPACEFOLDER/bin":*)
+        ;;
+    *)
+        export PATH="$WORKSPACEFOLDER/bin:$PATH"
         ;;
 esac
--- setup-devcontainer.sh
+++ setup-devcontainer.sh
@@ -2,6 +2,15 @@ set -e
 set -x
 
 ./setup-build.sh
+
+# The toolchain for building VeriFast is installed by ./setup-build.sh (not using opam), but
+# we now use opam to install ocaml-lsp-server.
+sudo apt-get install -y --no-install-recommends opam
+opam init -n -c 4.14.0
+opam install -y dune.3.14.2 ocaml-lsp-server ocamlformat
+
 WORKSPACEFOLDER=`pwd`
 echo ". $WORKSPACEFOLDER/setenv.sh" >> ~/.bashrc
-sudo apt-get install -y --no-install-recommends opam
+
+. ./setenv.sh
+cd src && make build
