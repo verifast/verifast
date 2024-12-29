86c232c37efe66d3f7935db8ce38c5cf41a573cb
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2024-12-29 01:07:02 +0100

    install-vfdeps.sh: fix vfdeps path

--- install-vfdeps.sh
+++ install-vfdeps.sh
@@ -1,2 +1,2 @@
 ln -sf /vf-llvm-clang-build-$VF_LLVM_CLANG_BUILD_VERSION /tmp/vf-llvm-clang-build-$VF_LLVM_CLANG_BUILD_VERSION
-ln -sf /vfdeps-$VFDEPS_NAME /tmp/vfdeps-$VFDEPS_NAME
+ln -sf /$VFDEPS_NAME /tmp/$VFDEPS_NAME
