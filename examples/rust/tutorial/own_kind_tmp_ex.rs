28ce4217aa81676c77833e417a22c734f3f60d3e
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2025-02-06 01:26:28 +0100

    WIP

--- examples/rust/tutorial/own_kind_tmp_ex.rs
+++ examples/rust/tutorial/own_kind_tmp_ex.rs
@@ -1,11 +1,12 @@
-fn inc<'a>(b: &'a mut Box<u8>) {
-    **b = **b + 1
-}
+fn inc<'a>(b: &'a mut Box<u8>) { **b = **b + 1; }
+
 fn main() {
     let mut x = Box::new(42);
-    //======================= κ begins
+    //====================================== κ begins
     let r = &mut x; // borrow under κ
+    // assert_eq!(*x, 42); // is not allowed by compoiler
     inc(r);
-    //======================= κ ends
+    //====================================== κ ends => x unfreez
     assert_eq!(*x, 43);
+    // x drop
 }
