3017cf4aa5de948d8c5bb5600df7ac10da3fde78
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2025-02-07 22:59:07 +0100

    refinement-checker: check that outside the fns, the files match
    Apart from functions checked by the function refinement checker, the crate source files and (nested) module source files must be identical after collapsing whitespace.

--- tests/rust/refinement_checker/identical_verified.rs
+++ tests/rust/refinement_checker/identical_verified.rs
@@ -1,9 +1,10 @@
-use std::boxed::Box;
+use std::boxed::Box; // This comment is ignored
+use std::option::Option;
 fn foo(x: Box<i32>) -> i32 { *x }
 fn bar(x: i32) -> i32
 {
     let b = Box::new(x);
     let r = foo(b);
     r
-}
+} /* It's okay to have extra comments /* and even nested ones */ */
 fn baz(x: Option<i32>) -> i32 { x.map_or_else(|| 42, |v| v / 2) }
