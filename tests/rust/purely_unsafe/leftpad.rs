a77fbfd1249731141119868338a8ea1b4277a40b
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2025-03-31 16:51:54 +0200

    [Rust] leftpad_vec (#761)

--- tests/rust/purely_unsafe/leftpad.rs
+++ tests/rust/purely_unsafe/leftpad.rs
@@ -1,12 +1,16 @@
+// verifast_options{ignore_unwind_paths}
+
 pub unsafe fn leftpad(c: u8, n: usize, ns: usize, s: *mut u8) -> usize
 /*@
 req s[..ns] |-> ?cs &*&
     if ns < n { (s + ns)[..n - ns] |-> _ } else { true };
 @*/
 /*@
-ens s[..result - ns] |-> repeat(nat_of_int(result - ns), c) &*&
+ens result == (if n < ns { ns } else { n }) &*&
+    s[..result - ns] |-> repeat(nat_of_int(result - ns), c) &*&
     (s + result - ns)[..ns] |-> cs;
 @*/
+//@ on_unwind_ens false;
 //@ terminates;
 {
     if ns < n {
@@ -19,4 +23,51 @@ ens s[..result - ns] |-> repeat(nat_of_int(result - ns), c) &*&
     } else {
         ns
     }
+}
+
+/*@
+
+lem foreach_own_u8(t: thread_id_t, elems: list<u8>)
+    req true;
+    ens foreach(elems, own::<u8>(t));
+{
+    match elems {
+        nil => {
+            close foreach(elems, own::<u8>(t));
+        }
+        cons(elem, elems0) => {
+            close u8_own(t, elem);
+            close own::<u8>(t)(elem);
+            foreach_own_u8(t, elems0);
+            close foreach(elems, own::<u8>(t));
+        }
+    }
+}
+
+@*/
+
+pub fn leftpad_vec(c: u8, n: usize, v: &mut Vec<u8>) {
+    //@ std::vec::own_to_Vec(*v);
+    //@ let v_ref = precreate_ref(v);
+    //@ std::vec::init_ref_Vec(v_ref);
+    let ns = v.len();
+    //@ std::vec::end_ref_Vec(v_ref);
+    if ns < n {
+        let p = n - ns;
+        v.reserve(p);
+        //@ std::vec::Vec_separate_buffer(*v);
+        let buf = v.as_mut_ptr();
+        //@ array__split(buf + ns, n - ns);
+        unsafe {
+            leftpad(c, n, ns, buf);
+            v.set_len(n);
+        }
+        //@ assert buf[..p] |-> ?cspad &*& buf[p..n] |-> ?cs;
+        //@ array_join(buf);
+        //@ std::vec::Vec_unseparate_buffer(*v);
+        //@ foreach_own_u8(_t, cspad);
+        //@ foreach_append(cspad, cs);
+        //@ std::vec::Vec_to_own(*v);
+    }
+    //@ if ns >= n { std::vec::Vec_to_own(*v); }
 }
