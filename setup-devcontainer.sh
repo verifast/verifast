90684a2bdc4326c7f6b7b6e5001f0066e46b365e
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2025-06-23 18:05:14 +0200

    Fix devcontainer
    It had rotted because
    - the GitHub Codespaces base images had grown, causing the
      devcontainer setup to run out of disk space
    - some OPAM packages have been moved to
      opam-repository-archive

--- setup-devcontainer.sh
+++ setup-devcontainer.sh
@@ -10,7 +10,8 @@ set -x
 # The toolchain for building VeriFast is installed by ./setup-build.sh (not using opam), but
 # we now use opam to install ocaml-lsp-server.
 sudo apt-get install -y --no-install-recommends opam
-opam init -n -c 4.14.0
+opam init --disable-sandboxing -y -n -c 4.14.0
+opam repo add archive git+https://github.com/ocaml/opam-repository-archive
 opam install -y dune.3.14.2 ocaml-lsp-server ocamlformat
 
 WORKSPACEFOLDER=`pwd`
