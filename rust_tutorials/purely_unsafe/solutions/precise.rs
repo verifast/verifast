be07c90531b4769574e0463b3d8ddcac8369c4d6
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2025-10-10 13:22:47 +0200

    Rust unsafe tutorial: tweaked threading examples (#926)

--- rust_tutorials/purely_unsafe/solutions/precise.rs
+++ rust_tutorials/purely_unsafe/solutions/precise.rs
@@ -1,71 +1,103 @@
-// verifast_options{ignore_unwind_paths extern:../../../tests/rust/unverified/platform disable_overflow_check}
+// verifast_options{ignore_unwind_paths}
+#![allow(unsafe_op_in_unsafe_fn)]
 
-use std::alloc::{Layout, alloc, handle_alloc_error};
-//use platform::threading::Thread;
-//@ use std::alloc::{Layout, alloc_block};
-//@ use platform::threading::{Thread, thread_run_joinable};
+use std::{alloc::{alloc, handle_alloc_error, Layout, dealloc}, thread::JoinHandle};
+//@ use std::{alloc::Layout, thread::JoinHandle};
 
-unsafe fn random_int(max: i32) -> i32
-//@ req 0 < max;
-//@ ens 0 <= result && result < max;
+/*@
+
+fn_type Spawnee<A, R>(pre: pred(A, pred(R))) = unsafe fn(arg: A) -> R;
+    req pre(arg, ?post);
+    ens post(result);
+
+pred JoinHandle<R>(h: JoinHandle<Sendable<R>>, post: pred(R));
+
+@*/
+
+type Spawnee<A, R> = unsafe fn(arg: A) -> R;
+
+struct Sendable<T> { payload: T }
+unsafe impl<T> Send for Sendable<T> {}
+
+unsafe fn spawn<A, R>(f: Spawnee<A, R>, arg: A) -> JoinHandle<Sendable<R>>
+where A: 'static, R: 'static
+//@ req [_]is_Spawnee::<A, R>(f, ?pre) &*& pre(arg, ?post);
+//@ ens JoinHandle(result, post);
+//@ assume_correct
 {
-    max - 1 // TODO: Replace by an actual random number generator
+    let package = Sendable { payload: arg };
+    std::thread::spawn(move || {
+        let package_moved = package;
+        Sendable { payload: f(package_moved.payload) }
+    })
 }
 
-unsafe fn fac(mut x: i32) -> i32
+unsafe fn join<R>(h: JoinHandle<Sendable<R>>) -> R
+//@ req JoinHandle(h, ?post);
+//@ ens post(result);
+//@ assume_correct
+{
+    h.join().unwrap().payload
+}
+
+unsafe fn wrapping_fib(n: u16) -> u64
 //@ req true;
 //@ ens true;
 {
-    let mut result = 1;
-    loop {
-        //@ inv true;
-        if x == 1 {
-            return result;
+    if n <= 1 {
+        1
+    } else {
+        let mut k: u16 = 2;
+        let mut fib_k_minus_1: u64 = 1;
+        let mut fib_k: u64 = 1;
+        loop {
+            //@ inv k <= n;
+            
+            if k == n { break; }
+            
+            let fib_k_plus_1 = fib_k_minus_1.wrapping_add(fib_k);
+            
+            k += 1;
+            fib_k_minus_1 = fib_k;
+            fib_k = fib_k_plus_1;
         }
-        result *= x;
-        x -= 1;
+        fib_k
     }
 }
 
 struct Tree {
     left: *mut Tree,
     right: *mut Tree,
-    value: i32,
+    value: u16,
 }
 
 /*@
 
-pred Tree(t: *mut Tree; depth: i32) =
+pred Tree(t: *mut Tree; depth: u8) =
     if t == 0 {
         depth == 0
     } else {
-        (*t).left |-> ?left &*& Tree(left, ?childDepth) &*&
-        (*t).right |-> ?right &*& Tree(right, childDepth) &*&
+        (*t).left |-> ?left &*& Tree(left, ?depth0) &*& depth == depth0 + 1 &*&
+        (*t).right |-> ?right &*& Tree(right, depth0) &*&
         (*t).value |-> ?value &*&
-        alloc_block_Tree(t) &*&
-        depth == childDepth + 1
+        alloc_block_Tree(t)
     };
-
-fn_type FoldFunction() = unsafe fn(acc: i32, x: i32) -> i32;
-    req true;
-    ens true;
 
 @*/
 
-type FoldFunction = unsafe fn(acc: i32, x: i32) -> i32;
-
 impl Tree {
 
-    unsafe fn make(depth: i32) -> *mut Tree
+    unsafe fn make(depth: u8) -> *mut Tree
     //@ req true;
     //@ ens Tree(result, depth);
     {
         if depth == 0 {
+            //@ close Tree(0, 0);
             std::ptr::null_mut()
         } else {
             let left = Self::make(depth - 1);
             let right = Self::make(depth - 1);
-            let value = random_int(5);
+            let value = 5000; // TODO: Use a random number here
             let t = alloc(Layout::new::<Tree>()) as *mut Tree;
             if t.is_null() {
                 handle_alloc_error(Layout::new::<Tree>());
@@ -73,14 +105,48 @@ impl Tree {
             (*t).left = left;
             (*t).right = right;
             (*t).value = value;
+            //@ close Tree(t, depth);
             t
         }
     }
 
+    unsafe fn compute_sum_fibs(tree: *mut Tree) -> u64
+    //@ req [?frac]Tree(tree, ?depth);
+    //@ ens [frac]Tree(tree, depth);
+    {
+        if tree.is_null() {
+            0
+        } else {
+            //@ open [frac]Tree(tree, depth);
+            let left_sum = Self::compute_sum_fibs((*tree).left);
+            let f = wrapping_fib((*tree).value);
+            let right_sum = Self::compute_sum_fibs((*tree).right);
+            //@ close [frac]Tree(tree, depth);
+            left_sum.wrapping_add(f).wrapping_add(right_sum)
+        }
+    }
+    
+    unsafe fn compute_product_fibs(tree: *mut Tree) -> u64
+    //@ req [?frac]Tree(tree, ?depth);
+    //@ ens [frac]Tree(tree, depth);
+    {
+        if tree.is_null() {
+            1
+        } else {
+            //@ open [frac]Tree(tree, depth);
+            let left_product = Self::compute_product_fibs((*tree).left);
+            let f = wrapping_fib((*tree).value);
+            let right_product = Self::compute_product_fibs((*tree).right);
+            //@ close [frac]Tree(tree, depth);
+            left_product.wrapping_mul(f).wrapping_mul(right_product)
+        }
+    }
+    
     unsafe fn dispose(tree: *mut Tree)
     //@ req Tree(tree, _);
     //@ ens true;
     {
+        //@ open Tree(tree, _);
         if !tree.is_null() {
             Self::dispose((*tree).left);
             Self::dispose((*tree).right);
@@ -88,101 +154,9 @@ impl Tree {
         }
     }
 
-    unsafe fn fold(tree: *mut Tree, f: FoldFunction, mut acc: i32) -> i32
-    //@ req [?frac]Tree(tree, ?depth) &*& [_]is_FoldFunction(f);
-    //@ ens [frac]Tree(tree, depth);
-    {
-        if tree.is_null() {
-            acc
-        } else {
-            acc = Self::fold((*tree).left, f, acc);
-            acc = Self::fold((*tree).right, f, acc);
-            acc = f(acc, (*tree).value);
-            acc
-        }
-    }
-
 }
 
-struct FoldData {
-    thread: platform::threading::Thread,
-    tree: *mut Tree,
-    f: FoldFunction,
-    acc: i32,
-}
-
-/*@
-
-pred_ctor folder_post(data: *FoldData)() =
-    [1/2](*data).tree |-> ?tree &*& [1/2]Tree(tree, _) &*&
-    (*data).f |-> ?f &*& [_]is_FoldFunction(f) &*&
-    (*data).acc |-> ?acc;
-pred folder_pre(data: *FoldData, post: pred()) =
-    [1/2](*data).tree |-> ?tree &*& [1/2]Tree(tree, _) &*&
-    (*data).f |-> ?f &*& [_]is_FoldFunction(f) &*&
-    (*data).acc |-> ?acc &*&
-    post == folder_post(data);
-
-@*/
-
-unsafe fn folder(data: *mut FoldData)
-//@ req folder_pre(data, ?post);
-//@ ens post();
-{
-    //@ open folder_pre(data, _);
-    let acc = Tree::fold((*data).tree, (*data).f, (*data).acc);
-    (*data).acc = acc;
-    //@ close folder_post(data)();
-}
-
-unsafe fn start_fold_thread(tree: *mut Tree, f: FoldFunction, acc: i32) -> *mut FoldData
-//@ req [1/2]Tree(tree, _) &*& [_]is_FoldFunction(f);
-//@ ens [1/2](*result).tree |-> tree &*& (*result).thread |-> ?t &*& Thread(t, folder_post(result)) &*& alloc_block_FoldData(result);
-{
-    let data = alloc(Layout::new::<FoldData>()) as *mut FoldData;
-    if data.is_null() {
-        handle_alloc_error(Layout::new::<FoldData>());
-    }
-    (*data).tree = tree;
-    (*data).f = f;
-    (*data).acc = acc;
-    //@ close folder_pre(data, folder_post(data));
-    /*@
-    produce_fn_ptr_chunk thread_run_joinable<*FoldData>(folder)(folder_pre)(data_) {
-        call();
-    }
-    @*/
-    let t = platform::threading::fork_joinable(folder, data);
-    (*data).thread = t;
-    data
-}
-
-unsafe fn join_fold_thread(data: *mut FoldData) -> i32
-//@ req [1/2](*data).tree |-> ?tree &*& (*data).thread |-> ?t &*& Thread(t, folder_post(data)) &*& alloc_block_FoldData(data);
-//@ ens [1/2]Tree(tree, _);
-{
-    platform::threading::join((*data).thread);
-    //@ open folder_post(data)();
-    let result = (*data).acc;
-    dealloc(data as *mut u8, Layout::new::<FoldData>());
-    result
-}
-
-unsafe fn sum_function(acc: i32, x: i32) -> i32
-//@ req true;
-//@ ens true;
-{
-    acc + fac(x)
-}
-
-unsafe fn xor_function(acc: i32, x: i32) -> i32
-//@ req true;
-//@ ens true;
-{
-    acc ^ fac(x)
-}
-
-unsafe fn print_i32(value: i32)
+unsafe fn print_u64(value: u64)
 //@ req true;
 //@ ens true;
 //@ assume_correct
@@ -190,16 +164,47 @@ unsafe fn print_i32(value: i32)
     println!("{}", value);
 }
 
-fn main() {
+//@ pred_ctor inspect_tree_post(tree: *mut Tree, depth: i32)(result: u64) = [1/2]Tree(tree, depth);
+//@ pred inspect_tree_pre(tree: *mut Tree, post: pred(u64)) = [1/2]Tree(tree, ?depth) &*& post == inspect_tree_post(tree, depth);
+
+fn main()
+//@ req true;
+//@ ens true;
+{
     unsafe {
-        let tree = Tree::make(21);
-        //@ produce_fn_ptr_chunk FoldFunction(sum_function)()(acc, x) { call(); }
-        let sum_data = start_fold_thread(tree, sum_function, 0);
-        //@ produce_fn_ptr_chunk FoldFunction(xor_function)()(acc, x) { call(); }
-        let xor_data = start_fold_thread(tree, xor_function, 0);
-        let sum = join_fold_thread(sum_data);
-        let xor = join_fold_thread(xor_data);
+        let tree = Tree::make(22);
+        
+        /*@
+        produce_fn_ptr_chunk Spawnee<*mut Tree, u64>(Tree::compute_sum_fibs)(inspect_tree_pre)(arg) {
+            open inspect_tree_pre(arg, _);
+            assert [1/2]Tree(arg, ?depth);
+            let result = call();
+            close inspect_tree_post(arg, depth)(result);
+        }
+        @*/
+        //@ close inspect_tree_pre(tree, _);
+        let sum_join_handle = spawn(Tree::compute_sum_fibs, tree);
+        
+        /*@
+        produce_fn_ptr_chunk Spawnee<*mut Tree, u64>(Tree::compute_product_fibs)(inspect_tree_pre)(arg) {
+            open inspect_tree_pre(arg, _);
+            assert [1/2]Tree(arg, ?depth);
+            let result = call();
+            close inspect_tree_post(arg, depth)(result);
+        }
+        @*/
+        //@ close inspect_tree_pre(tree, _);
+        let product_join_handle = spawn(Tree::compute_product_fibs, tree);
+        
+        let sum = join(sum_join_handle);
+        //@ open inspect_tree_post(tree, 22)(_);
+        
+        let product = join(product_join_handle);
+        //@ open inspect_tree_post(tree, 22)(_);
+        
         Tree::dispose(tree);
-        print_i32(sum - xor);
+        
+        print_u64(sum);
+        print_u64(product);
     }
 }
