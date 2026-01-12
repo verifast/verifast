3b6bd8b8633d0bcc9ba63825c9953f6e7877f233
Yuichi Nishiwaki <yuichi.nishiwaki@icloud.com>
2026-01-13 01:23:16 +0900

    fix "Expressing ownership of &str values is not yet supported" error (#983)
    * fix build
    
    * add a failing test for "Expressing ownership of &str values is not yet supported"
    
    * add <str>.share
    
    * fix "Expressing ownership of &str values is not yet supported"
    
    * Better test case
    
    Also: adds support for calling len() on a raw slice pointer.
    
    * Resolve review comments
    
    * Add comment on sysctl in Codex sandbox
    
    ---------
    
    Co-authored-by: Bart Jacobs <bart.jacobs@cs.kuleuven.be>

---
+++ tests/rust/safe_abstraction/str.rs
@@ -0,0 +1,29 @@
+fn test_str<'a>(s: &'a str) -> &'a str
+//@ req [?qa]lifetime_token('a) &*& [_](<str>.share('a, currentThread, s));
+//@ ens [qa]lifetime_token('a) &*& [_](<str>.share('a, currentThread, result));
+//@ on_unwind_ens false;
+{
+    let s_ptr = s as *const str;
+    if (s_ptr as *mut [u8]).len() > 0 {
+        //@ open str_share('a, currentThread, s);
+        //@ assert [_]exists(?bs);
+        //@ open_frac_borrow('a, mk_array(s as *u8, s.len(), bs), qa);
+        //@ open [?f]mk_array::<u8>(s as *u8, s.len(), bs)();
+        //@ assert bs == cons(?b0, ?rest0);
+        /*@
+        if 0x80 <= b0 {
+            assert rest0 == cons(?b1, ?rest1);
+            if 0xE0 <= b0 {
+                assert rest1 == cons(?b2, ?rest2);
+                if 0xF0 <= b0 {
+                    assert rest2 == cons(?b3, ?rest3);
+                }
+            }
+        }
+        @*/
+        unsafe { std::hint::assert_unchecked(*(s_ptr as *const u8) <= 0x7F || 0x80 <= *(s_ptr as *const u8).add(1)); }
+        //@ close [f]mk_array::<u8>(s as *u8, s.len(), bs)();
+        //@ close_frac_borrow(f, mk_array(s as *u8, s.len(), bs));
+    }
+    s
+}
