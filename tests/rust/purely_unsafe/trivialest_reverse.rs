2f03762acf77ba713a9318c4100c5332b83613e4
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2026-01-19 12:52:36 +0100

    trivialest_reverse.rs: Improve names (#984)

--- tests/rust/purely_unsafe/trivialest_reverse.rs
+++ tests/rust/purely_unsafe/trivialest_reverse.rs
@@ -1,36 +1,36 @@
 /*@
 
-pred Nodes(n: *mut u8) =
-    if n == 0 {
+pred llist(head: *mut u8) =
+    if head == 0 {
         true
     } else {
-        *(n as *mut *mut u8) |-> ?next &*& Nodes(next)
+        *(head as *mut *mut u8) |-> ?next &*& llist(next)
     };
 
 @*/
 
-unsafe fn reverse_iter(n: *mut u8, m: *mut u8) -> *mut u8
-//@ req Nodes(n) &*& Nodes(m);
-//@ ens Nodes(result);
+unsafe fn reverse_iter(original: *mut u8, reversed: *mut u8) -> *mut u8
+//@ req llist(original) &*& llist(reversed);
+//@ ens llist(result);
 //@ on_unwind_ens false;
 {
-    //@ open Nodes(n);
-    if n.is_null() {
-        return m;
+    //@ open llist(original);
+    if original.is_null() {
+        return reversed;
     }
-    let k = *(n as *mut *mut u8);
-    *(n as *mut *mut u8) = m;
-    //@ close Nodes(n);
-    reverse_iter(k, n)
+    let next = *(original as *mut *mut u8);
+    *(original as *mut *mut u8) = reversed;
+    //@ close llist(original);
+    reverse_iter(next, original)
 }
 
-unsafe fn reverse(mut n: *mut u8) -> *mut u8
-//@ req Nodes(n);
-//@ ens Nodes(result);
+unsafe fn reverse(mut list: *mut u8) -> *mut u8
+//@ req llist(list);
+//@ ens llist(result);
 //@ on_unwind_ens false;
 {
-    //@ close Nodes(0);
-    reverse_iter(n, std::ptr::null_mut())
+    //@ close llist(0);
+    reverse_iter(list, std::ptr::null_mut())
 }
 
 fn main()
@@ -38,12 +38,14 @@ fn main()
 //@ ens true;
 {
     unsafe {
-        //@ close Nodes(0);
+        //@ close llist(0);
         let mut node1: *mut u8 = std::ptr::null_mut();
-        //@ close Nodes(&node1 as *mut u8);
-        let mut node2: *mut u8 = &raw mut node1 as *mut u8;
-        //@ close Nodes(&node2 as *mut u8);
-        let reversed = reverse(&raw mut node2 as *mut u8);
+        //@ close llist(&node1 as *mut u8);
+        let mut node2 = &raw mut node1 as *mut u8;
+        //@ close llist(&node2 as *mut u8);
+        let mut node3 = &raw mut node2 as *mut u8;
+        //@ close llist(&node3 as *mut u8);
+        let reversed = reverse(&raw mut node3 as *mut u8);
         reverse(reversed);
         std::process::abort();
     }
--- tests/rust/purely_unsafe/trivialest_reverse_expected.v
+++ tests/rust/purely_unsafe/trivialest_reverse_expected.v
@@ -6,14 +6,14 @@ Definition bodies := [
         output := RawPtr (Uint U8);
         local_decls := [
             ("$_0", {| mutability := Mut; ty := RawPtr (Uint U8) |});
-            ("n", {| mutability := Not; ty := RawPtr (Uint U8) |});
-            ("m", {| mutability := Not; ty := RawPtr (Uint U8) |});
+            ("original", {| mutability := Not; ty := RawPtr (Uint U8) |});
+            ("reversed", {| mutability := Not; ty := RawPtr (Uint U8) |});
             ("$_3", {| mutability := Not; ty := Tuple0 |});
             ("$_4", {| mutability := Not; ty := Tuple0 |});
             ("$_5", {| mutability := Mut; ty := Bool |});
             ("$_6", {| mutability := Mut; ty := RawPtr (Uint U8) |});
             ("$_7", {| mutability := Mut; ty := Never |});
-            ("k", {| mutability := Not; ty := RawPtr (Uint U8) |});
+            ("next", {| mutability := Not; ty := RawPtr (Uint U8) |});
             ("$_9", {| mutability := Mut; ty := RawPtr (RawPtr (Uint U8)) |});
             ("$_10", {| mutability := Mut; ty := RawPtr (Uint U8) |});
             ("$_11", {| mutability := Mut; ty := RawPtr (Uint U8) |});
@@ -41,7 +41,7 @@ Definition bodies := [
                     StorageLive "$_4";
                     StorageLive "$_5";
                     StorageLive "$_6";
-                    Assign ("$_6", []) (Use (Copy ("n", [])))
+                    Assign ("$_6", []) (Use (Copy ("original", [])))
                 ];
                 terminator := Call {|
                     args := [Move ("$_6", [])];
@@ -57,7 +57,7 @@ Definition bodies := [
             ("3", {|
                 statements := [
                     StorageDead "$_6";
-                    Assign ("$_0", []) (Use (Copy ("m", [])));
+                    Assign ("$_0", []) (Use (Copy ("reversed", [])));
                     StorageDead "$_5";
                     StorageDead "$_4"
                 ];
@@ -69,19 +69,19 @@ Definition bodies := [
                     Assign ("$_4", []) (Use (Constant (Val ZeroSized Tuple0)));
                     StorageDead "$_5";
                     StorageDead "$_4";
-                    StorageLive "k";
+                    StorageLive "next";
                     StorageLive "$_9";
                     StorageLive "$_10";
-                    Assign ("$_10", []) (Use (Copy ("n", [])));
+                    Assign ("$_10", []) (Use (Copy ("original", [])));
                     Assign ("$_9", []) (Cast PtrToPtr (Move ("$_10", [])) (RawPtr (RawPtr (Uint U8))));
                     StorageDead "$_10";
-                    Assign ("k", []) (Use (Copy ("$_9", [Deref])));
+                    Assign ("next", []) (Use (Copy ("$_9", [Deref])));
                     StorageDead "$_9";
                     StorageLive "$_11";
-                    Assign ("$_11", []) (Use (Copy ("m", [])));
+                    Assign ("$_11", []) (Use (Copy ("reversed", [])));
                     StorageLive "$_12";
                     StorageLive "$_13";
-                    Assign ("$_13", []) (Use (Copy ("n", [])));
+                    Assign ("$_13", []) (Use (Copy ("original", [])));
                     Assign ("$_12", []) (Cast PtrToPtr (Move ("$_13", [])) (RawPtr (RawPtr (Uint U8))));
                     StorageDead "$_13";
                     Assign ("$_12", [Deref]) (Use (Move ("$_11", [])));
@@ -100,9 +100,9 @@ Definition bodies := [
                 statements := [
                     StorageDead "$_14";
                     StorageLive "$_15";
-                    Assign ("$_15", []) (Use (Copy ("k", [])));
+                    Assign ("$_15", []) (Use (Copy ("next", [])));
                     StorageLive "$_16";
-                    Assign ("$_16", []) (Use (Copy ("n", [])))
+                    Assign ("$_16", []) (Use (Copy ("original", [])))
                 ];
                 terminator := Call {|
                     args := [Move ("$_15", []); Move ("$_16", [])];
@@ -115,7 +115,7 @@ Definition bodies := [
                 statements := [
                     StorageDead "$_16";
                     StorageDead "$_15";
-                    StorageDead "k"
+                    StorageDead "next"
                 ];
                 terminator := Goto "7"
             |});
@@ -130,7 +130,7 @@ Definition bodies := [
         output := RawPtr (Uint U8);
         local_decls := [
             ("$_0", {| mutability := Mut; ty := RawPtr (Uint U8) |});
-            ("n", {| mutability := Mut; ty := RawPtr (Uint U8) |});
+            ("list", {| mutability := Mut; ty := RawPtr (Uint U8) |});
             ("$_2", {| mutability := Not; ty := Tuple0 |});
             ("$_3", {| mutability := Mut; ty := RawPtr (Uint U8) |});
             ("$_4", {| mutability := Mut; ty := RawPtr (Uint U8) |})
@@ -151,7 +151,7 @@ Definition bodies := [
                 statements := [
                     StorageDead "$_2";
                     StorageLive "$_3";
-                    Assign ("$_3", []) (Use (Copy ("n", [])));
+                    Assign ("$_3", []) (Use (Copy ("list", [])));
                     StorageLive "$_4"
                 ];
                 terminator := Call {|
@@ -191,12 +191,15 @@ Definition bodies := [
             ("node2", {| mutability := Mut; ty := RawPtr (Uint U8) |});
             ("$_6", {| mutability := Mut; ty := RawPtr (RawPtr (Uint U8)) |});
             ("$_7", {| mutability := Not; ty := Tuple0 |});
+            ("node3", {| mutability := Mut; ty := RawPtr (Uint U8) |});
+            ("$_9", {| mutability := Mut; ty := RawPtr (RawPtr (Uint U8)) |});
+            ("$_10", {| mutability := Not; ty := Tuple0 |});
             ("reversed", {| mutability := Not; ty := RawPtr (Uint U8) |});
-            ("$_9", {| mutability := Mut; ty := RawPtr (Uint U8) |});
-            ("$_10", {| mutability := Mut; ty := RawPtr (RawPtr (Uint U8)) |});
-            ("$_11", {| mutability := Not; ty := RawPtr (Uint U8) |});
             ("$_12", {| mutability := Mut; ty := RawPtr (Uint U8) |});
-            ("$_13", {| mutability := Not; ty := Never |})
+            ("$_13", {| mutability := Mut; ty := RawPtr (RawPtr (Uint U8)) |});
+            ("$_14", {| mutability := Not; ty := RawPtr (Uint U8) |});
+            ("$_15", {| mutability := Mut; ty := RawPtr (Uint U8) |});
+            ("$_16", {| mutability := Not; ty := Never |})
         ];
         basic_blocks := [
             ("0", {|
@@ -253,44 +256,61 @@ Definition bodies := [
             ("4", {|
                 statements := [
                     StorageDead "$_7";
-                    StorageLive "reversed";
+                    StorageLive "node3";
                     StorageLive "$_9";
-                    StorageLive "$_10";
-                    Assign ("$_10", []) (RawPtr_ ("node2", []));
-                    Assign ("$_9", []) (Cast PtrToPtr (Move ("$_10", [])) (RawPtr (Uint U8)));
-                    StorageDead "$_10"
+                    Assign ("$_9", []) (RawPtr_ ("node2", []));
+                    Assign ("node3", []) (Cast PtrToPtr (Move ("$_9", [])) (RawPtr (Uint U8)));
+                    StorageDead "$_9";
+                    StorageLive "$_10"
                 ];
                 terminator := Call {|
-                    args := [Move ("$_9", [])];
-                    func := Constant (Val ZeroSized (FnDef "reverse" GALNil));
-                    destination := ("reversed", []);
+                    args := [];
+                    func := Constant (Val ZeroSized (FnDef "VeriFast_ghost_command" GALNil));
+                    destination := ("$_10", []);
                     target := Some "5"
                 |}
             |});
             ("5", {|
                 statements := [
-                    StorageDead "$_9";
-                    StorageLive "$_11";
+                    StorageDead "$_10";
+                    StorageLive "reversed";
                     StorageLive "$_12";
-                    Assign ("$_12", []) (Use (Copy ("reversed", [])))
+                    StorageLive "$_13";
+                    Assign ("$_13", []) (RawPtr_ ("node3", []));
+                    Assign ("$_12", []) (Cast PtrToPtr (Move ("$_13", [])) (RawPtr (Uint U8)));
+                    StorageDead "$_13"
                 ];
                 terminator := Call {|
                     args := [Move ("$_12", [])];
                     func := Constant (Val ZeroSized (FnDef "reverse" GALNil));
-                    destination := ("$_11", []);
+                    destination := ("reversed", []);
                     target := Some "6"
                 |}
             |});
             ("6", {|
                 statements := [
                     StorageDead "$_12";
-                    StorageDead "$_11";
-                    StorageLive "$_13"
+                    StorageLive "$_14";
+                    StorageLive "$_15";
+                    Assign ("$_15", []) (Use (Copy ("reversed", [])))
+                ];
+                terminator := Call {|
+                    args := [Move ("$_15", [])];
+                    func := Constant (Val ZeroSized (FnDef "reverse" GALNil));
+                    destination := ("$_14", []);
+                    target := Some "7"
+                |}
+            |});
+            ("7", {|
+                statements := [
+                    StorageDead "$_15";
+                    StorageDead "$_14";
+                    StorageLive "$_16"
                 ];
                 terminator := Call {|
                     args := [];
                     func := Constant (Val ZeroSized (FnDef "std::process::abort" GALNil));
-                    destination := ("$_13", []);
+                    destination := ("$_16", []);
                     target := None
                 |}
             |})
@@ -301,24 +321,24 @@ Definition bodies := [
 From VeriFast Require Export Annotations.
 
 Definition preds := [
-    ("Nodes", {|
-        params := [("n", RawPtr (Uint U8))];
-        body := IfAsn (Eq (Var "n") NullPtr) (BoolAsn (BoolLit true)) (SepAsn (PointsToAsn (RawPtr (Uint U8)) (Var "n") (VarPat "next")) (PredAsn "Nodes" [LitPat (Var "next")]))
+    ("llist", {|
+        params := [("head", RawPtr (Uint U8))];
+        body := IfAsn (Eq (Var "head") NullPtr) (BoolAsn (BoolLit true)) (SepAsn (PointsToAsn (RawPtr (Uint U8)) (Var "head") (VarPat "next")) (PredAsn "llist" [LitPat (Var "next")]))
     |})
 ].
 
 Definition specs := [
     ("reverse_iter", {|
-        spec_params := [("n", RawPtr (Uint U8)); ("m", RawPtr (Uint U8))];
+        spec_params := [("original", RawPtr (Uint U8)); ("reversed", RawPtr (Uint U8))];
         spec_output := RawPtr (Uint U8);
-        pre := SepAsn (PredAsn "Nodes" [LitPat (Var "n")]) (PredAsn "Nodes" [LitPat (Var "m")]);
-        post := PredAsn "Nodes" [LitPat (Var "result")]
+        pre := SepAsn (PredAsn "llist" [LitPat (Var "original")]) (PredAsn "llist" [LitPat (Var "reversed")]);
+        post := PredAsn "llist" [LitPat (Var "result")]
     |});
     ("reverse", {|
-        spec_params := [("n", RawPtr (Uint U8))];
+        spec_params := [("list", RawPtr (Uint U8))];
         spec_output := RawPtr (Uint U8);
-        pre := PredAsn "Nodes" [LitPat (Var "n")];
-        post := PredAsn "Nodes" [LitPat (Var "result")]
+        pre := PredAsn "llist" [LitPat (Var "list")];
+        post := PredAsn "llist" [LitPat (Var "result")]
     |});
     ("main", {|
         spec_params := [];
@@ -332,15 +352,15 @@ Open Scope annot_scope.
 
 Definition symex_trees := [
     (Verifying "reverse_iter", 
-        ParamAddrTaken "n" false;;
-        ParamAddrTaken "m" false;;
+        ParamAddrTaken "original" false;;
+        ParamAddrTaken "reversed" false;;
         LocalAddrTaken "$_0" false;;
         LocalAddrTaken "$_3" true;;
         LocalAddrTaken "$_4" true;;
         LocalAddrTaken "$_5" false;;
         LocalAddrTaken "$_6" false;;
         LocalAddrTaken "$_7" true;;
-        LocalAddrTaken "k" false;;
+        LocalAddrTaken "next" false;;
         LocalAddrTaken "$_9" false;;
         LocalAddrTaken "$_10" false;;
         LocalAddrTaken "$_11" false;;
@@ -350,11 +370,11 @@ Definition symex_trees := [
         LocalAddrTaken "$_15" false;;
         LocalAddrTaken "$_16" false;;
         Open;;
-        (* h = [[1]Nodes<>(m); [1]Nodes<>(n)] *)
+        (* h = [[1]llist<>(reversed); [1]llist<>(original)] *)
         ConsumeChunk 1%nat;;
         Fork (
             Fork (
-                (* h = [[1]Nodes<>(m)] *)
+                (* h = [[1]llist<>(reversed)] *)
                 ConsumeChunk 0%nat;;
                 Done
             ) (
@@ -364,43 +384,43 @@ Definition symex_trees := [
             Fork (
                 Done
             ) (
-                (* h = [[1]Nodes<>(next); [1]points_to<*u8>(n, next); [1]Nodes<>(m)] *)
+                (* h = [[1]llist<>(next); [1]points_to<*u8>(original, next); [1]llist<>(reversed)] *)
                 ConsumeChunk 1%nat;;
-                (* h = [[1]points_to<*u8>(n, next); [1]Nodes<>(next); [1]Nodes<>(m)] *)
+                (* h = [[1]points_to<*u8>(original, next); [1]llist<>(next); [1]llist<>(reversed)] *)
                 AutoOpen 0%nat;;
-                (* h = [[1]points_to_<*u8>(n, some(next)); [1]Nodes<>(next); [1]Nodes<>(m)] *)
+                (* h = [[1]points_to_<*u8>(original, some(next)); [1]llist<>(next); [1]llist<>(reversed)] *)
                 ConsumeChunk 0%nat;;
-                Close (RealLit 1%Q) "Nodes" [] [LitPat (Var "n")];;
+                Close (RealLit 1%Q) "llist" [] [LitPat (Var "original")];;
                 Fork (
                     Done
                 ) (
-                    (* h = [[1]points_to<*u8>(n, m); [1]Nodes<>(next); [1]Nodes<>(m)] *)
+                    (* h = [[1]points_to<*u8>(original, reversed); [1]llist<>(next); [1]llist<>(reversed)] *)
                     ConsumeChunk 0%nat;;
-                    (* h = [[1]Nodes<>(next); [1]Nodes<>(m)] *)
+                    (* h = [[1]llist<>(next); [1]llist<>(reversed)] *)
                     ConsumeChunk 1%nat;;
-                    (* h = [[1]Nodes<>(n); [1]Nodes<>(next)] *)
+                    (* h = [[1]llist<>(original); [1]llist<>(next)] *)
                     ConsumeChunk 1%nat;;
-                    (* h = [[1]Nodes<>(n)] *)
+                    (* h = [[1]llist<>(original)] *)
                     ConsumeChunk 0%nat;;
-                    (* h = [[1]Nodes<>($_0)] *)
+                    (* h = [[1]llist<>($_0)] *)
                     ConsumeChunk 0%nat;;
                     Done
                 )
             )
         ));
     (Verifying "reverse", 
-        ParamAddrTaken "n" false;;
+        ParamAddrTaken "list" false;;
         LocalAddrTaken "$_0" false;;
         LocalAddrTaken "$_2" true;;
         LocalAddrTaken "$_3" false;;
         LocalAddrTaken "$_4" false;;
-        Close (RealLit 1%Q) "Nodes" [] [LitPat NullPtr];;
+        Close (RealLit 1%Q) "llist" [] [LitPat NullPtr];;
         Fork (
-            (* h = [[1]Nodes<>(null_pointer); [1]Nodes<>(n)] *)
+            (* h = [[1]llist<>(null_pointer); [1]llist<>(list)] *)
             ConsumeChunk 1%nat;;
-            (* h = [[1]Nodes<>(null_pointer)] *)
+            (* h = [[1]llist<>(null_pointer)] *)
             ConsumeChunk 0%nat;;
-            (* h = [[1]Nodes<>($_0)] *)
+            (* h = [[1]llist<>($_0)] *)
             ConsumeChunk 0%nat;;
             Done
         ) (
@@ -415,39 +435,53 @@ Definition symex_trees := [
         LocalAddrTaken "node2" true;;
         LocalAddrTaken "$_6" false;;
         LocalAddrTaken "$_7" true;;
-        LocalAddrTaken "reversed" false;;
+        LocalAddrTaken "node3" true;;
         LocalAddrTaken "$_9" false;;
-        LocalAddrTaken "$_10" false;;
-        LocalAddrTaken "$_11" false;;
+        LocalAddrTaken "$_10" true;;
+        LocalAddrTaken "reversed" false;;
         LocalAddrTaken "$_12" false;;
-        LocalAddrTaken "$_13" true;;
-        Close (RealLit 1%Q) "Nodes" [] [LitPat NullPtr];;
+        LocalAddrTaken "$_13" false;;
+        LocalAddrTaken "$_14" false;;
+        LocalAddrTaken "$_15" false;;
+        LocalAddrTaken "$_16" true;;
+        Close (RealLit 1%Q) "llist" [] [LitPat NullPtr];;
         Fork (
-            (* h = [[1]Nodes<>(null_pointer); [1]points_to_<*u8>(node2_addr, dummy0); [1]points_to_<*u8>(node1_addr, dummy)] *)
-            ConsumeChunk 2%nat;;
-            Close (RealLit 1%Q) "Nodes" [] [LitPat (Var "node1")];;
+            (* h = [[1]llist<>(null_pointer); [1]points_to_<*u8>(node3_addr, dummy1); [1]points_to_<*u8>(node2_addr, dummy0); [1]points_to_<*u8>(node1_addr, dummy)] *)
+            ConsumeChunk 3%nat;;
+            Close (RealLit 1%Q) "llist" [] [LitPat (Var "node1")];;
             Fork (
                 Done
             ) (
-                (* h = [[1]points_to<*u8>(node1_addr, null_pointer); [1]points_to_<*u8>(node2_addr, dummy0); [1]Nodes<>(null_pointer)] *)
+                (* h = [[1]points_to<*u8>(node1_addr, null_pointer); [1]points_to_<*u8>(node2_addr, dummy0); [1]points_to_<*u8>(node3_addr, dummy1); [1]llist<>(null_pointer)] *)
                 ConsumeChunk 0%nat;;
-                (* h = [[1]points_to_<*u8>(node2_addr, dummy0); [1]Nodes<>(null_pointer)] *)
-                ConsumeChunk 1%nat;;
-                (* h = [[1]Nodes<>(node1_addr); [1]points_to_<*u8>(node2_addr, dummy0)] *)
-                ConsumeChunk 1%nat;;
-                Close (RealLit 1%Q) "Nodes" [] [LitPat (Var "node2")];;
+                (* h = [[1]points_to_<*u8>(node2_addr, dummy0); [1]points_to_<*u8>(node3_addr, dummy1); [1]llist<>(null_pointer)] *)
+                ConsumeChunk 2%nat;;
+                (* h = [[1]llist<>(node1_addr); [1]points_to_<*u8>(node3_addr, dummy1); [1]points_to_<*u8>(node2_addr, dummy0)] *)
+                ConsumeChunk 2%nat;;
+                Close (RealLit 1%Q) "llist" [] [LitPat (Var "node2")];;
                 Fork (
                     Done
                 ) (
-                    (* h = [[1]points_to<*u8>(node2_addr, node1_addr); [1]Nodes<>(node1_addr)] *)
+                    (* h = [[1]points_to<*u8>(node2_addr, node1_addr); [1]points_to_<*u8>(node3_addr, dummy1); [1]llist<>(node1_addr)] *)
                     ConsumeChunk 0%nat;;
-                    (* h = [[1]Nodes<>(node1_addr)] *)
-                    ConsumeChunk 0%nat;;
-                    (* h = [[1]Nodes<>(node2_addr)] *)
-                    ConsumeChunk 0%nat;;
-                    (* h = [[1]Nodes<>(reversed)] *)
-                    ConsumeChunk 0%nat;;
-                    Done
+                    (* h = [[1]points_to_<*u8>(node3_addr, dummy1); [1]llist<>(node1_addr)] *)
+                    ConsumeChunk 1%nat;;
+                    (* h = [[1]llist<>(node2_addr); [1]points_to_<*u8>(node3_addr, dummy1)] *)
+                    ConsumeChunk 1%nat;;
+                    Close (RealLit 1%Q) "llist" [] [LitPat (Var "node3")];;
+                    Fork (
+                        Done
+                    ) (
+                        (* h = [[1]points_to<*u8>(node3_addr, node2_addr); [1]llist<>(node2_addr)] *)
+                        ConsumeChunk 0%nat;;
+                        (* h = [[1]llist<>(node2_addr)] *)
+                        ConsumeChunk 0%nat;;
+                        (* h = [[1]llist<>(node3_addr)] *)
+                        ConsumeChunk 0%nat;;
+                        (* h = [[1]llist<>(reversed)] *)
+                        ConsumeChunk 0%nat;;
+                        Done
+                    )
                 )
             )
         ) (
