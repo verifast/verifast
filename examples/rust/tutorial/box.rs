7a5723de262c98ff83d4d9abf1a572cdb42d36c4
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2026-01-26 22:58:06 +0100

    WIP

--- examples/rust/tutorial/box.rs
+++ examples/rust/tutorial/box.rs
@@ -1,59 +1,96 @@
 use std::alloc::{alloc, dealloc, handle_alloc_error, Layout};
 //@ use std::alloc_block_;
 
-pub struct Box<T> {
-    ptr: *mut T,
-}
+pub struct Box<T> { ptr: *mut T }
 
 /*@
 pred<T> <Box<T>>.own(t, b) = *b.ptr |-> ?v &*& <T>.own(t, v) &*& alloc_block_(b.ptr);
-
-pred_ctor field_ptr_chunk<T>(l: *Box<T>, p: *T)(;) = (*l).ptr |-> p;
-pred<T> <Box<T>>.share(k, t, l) = [_]exists(?p) &*& [_]frac_borrow(k, field_ptr_chunk(l, p)) &*& [_](<T>.share)(k, t, p);
+pred_ctor Box_ptr_field<T>(l: *Box<T>, p: *T)(;) = (*l).ptr |-> p;
+pred<T> <Box<T>>.share(k, t, l) = [_]exists(?p) &*&
+    [_]frac_borrow(k, Box_ptr_field(l, p)) &*& [_](<T>.share(k, t, p));
 
 lem Box_share_mono<T>(k: lifetime_t, k1: lifetime_t, t: thread_id_t, l: *Box<T>)
-    req type_interp::<T>() &*& lifetime_inclusion(k1, k) == true &*& [_](<Box<T>>.share)(k, t, l);
-    ens type_interp::<T>() &*& [_](<Box<T>>.share)(k1, t, l);
+    req type_interp::<T>() &*& lifetime_inclusion(k1, k) == true &*&
+        [_](<Box<T>>.share(k, t, l));
+    ens type_interp::<T>() &*& [_](<Box<T>>.share(k1, t, l));
 {
-    // We use the monotonicity of `frac_borrow` and `<T>.share` predicate
-    open <Box<T>>.share(k, t, l);
-    assert [_]exists(?p);
-    frac_borrow_mono(k, k1, field_ptr_chunk(l, p));
-    share_mono(k, k1, t, p);
+    open <Box<T>>.share(k, t, l); assert [_]exists(?p);
+    /*|\begin{vfHeap}
+    \vfResAdd{type_interp::<T>()}, \vfResAdd{[_]exists(p)}, \vfResAdd{[_]fcbor(k, Box_ptr_field(l, p))},
+    \vfResAdd{[_](<T>.share(k, t, p))}
+    \end{vfHeap}|*/
+    frac_borrow_mono(k, k1, Box_ptr_field(l, p));
+    /*|\begin{vfHeap}
+    type_interp::<T>(), [_]exists(p), [_](<T>.share(k, t, p)),
+    \vfResRm{[_]fcbor(k, Box_ptr_field(l, p))}, \vfResAdd{[_]fcbor(k1, Box_ptr_field(l, p))}
+    \end{vfHeap}|*/
+    share_mono(k, k1, t, p); //| Requires and ensures \verb|type_interp::<T>()|
+    /*|\begin{vfHeap}
+    \vfResAssert{type_interp::<T>()}, [_]exists(p), [_]fcbor(k1, Box_ptr_field(l, p)),
+    \vfResRm{[_](<T>.share(k, t, p))}, \vfResAdd{[_](<T>.share(k1, t, p))}
+    \end{vfHeap}|*/
     close <Box<T>>.share(k1, t, l);
     leak <Box<T>>.share(k1, t, l);
 }
 
 lem Box_share_full<T>(k: lifetime_t, t: thread_id_t, l: *Box<T>)
-    req type_interp::<T>() &*& atomic_mask(MaskTop) &*& [?q]lifetime_token(k) &*& full_borrow(k, <Box<T>>.full_borrow_content(t, l));
-    ens type_interp::<T>() &*& atomic_mask(MaskTop) &*& [q]lifetime_token(k) &*& [_](<Box<T>>.share)(k, t, l);
+    req type_interp::<T>() &*& atomic_mask(MaskTop) &*& [?q]lifetime_token(k) &*&
+        full_borrow(k, <Box<T>>.full_borrow_content(t, l));
+    ens type_interp::<T>() &*& atomic_mask(MaskTop) &*& [q]lifetime_token(k) &*&
+        [_](<Box<T>>.share(k, t, l));
 {
-    // We convert `full_borrow(k, <Box<T>>.full_borrow_content(t, l))` to `full_borrow(k, (*l).ptr |-> p &*& <T>.full_borrow_content(t, p));
-    let klong = open_full_borrow_strong_m(k, <Box<T>>.full_borrow_content(t, l), q);
+    open_full_borrow_strong_m_(k, <Box<T>>.full_borrow_content(t, l));
     open <Box<T>>.full_borrow_content(t, l)();
-    open Box_own::<T>(t, ?b);
+    open <Box<T>>.own(t, ?b);
     let p = b.ptr;
-    close_full_borrow_content::<T>(t, p);
-    close field_ptr_chunk::<T>(l, p)();
-    close sep(field_ptr_chunk(l, p), (<T>.full_borrow_content)(t, p))();
     {
-    pred ctx(;) = alloc_block_(p);
-    close ctx();
-    produce_lem_ptr_chunk full_borrow_convert_strong(ctx, sep(field_ptr_chunk(l, p), (<T>.full_borrow_content)(t, p)), klong, <Box<T>>.full_borrow_content(t, l))() {
-        open ctx();
-        open sep(field_ptr_chunk(l, p), (<T>.full_borrow_content)(t, p))();
-        open field_ptr_chunk::<T>(l, p)();
-        open_full_borrow_content::<T>(t, p);
-        close <Box<T>>.own(t, b);
-        close <Box<T>>.full_borrow_content(t, l)();
-    }{
-        close_full_borrow_strong_m(klong, <Box<T>>.full_borrow_content(t, l), sep(field_ptr_chunk(l, p), (<T>.full_borrow_content)(t, p)));
+        pred ctx(;) = alloc_block_(p);
+        produce_lem_ptr_chunk restore_full_borrow_(ctx,
+            sep(Box_ptr_field(l, p), <T>.full_borrow_content(t, p)),
+            <Box<T>>.full_borrow_content(t, l))()
+        {
+            open ctx();
+            open sep(Box_ptr_field(l, p), <T>.full_borrow_content(t, p))();
+            open Box_ptr_field::<T>(l, p)();
+            /*|\begin{vfHeap}
+            \vfResAdd{alloc_block_(p)}, \vfResAdd{(*l).ptr |-> p}, \vfResAdd{<T>.fbc(t, p)()}
+            \end{vfHeap}|*/
+            open_full_borrow_content::<T>(t, p);
+            /*|\begin{vfHeap}
+            alloc_block_(p), (*l).ptr |-> p, \vfResRm{<T>.fbc(t, p)()},
+            \vfResAdd{*p |-> v}, \vfResAdd{<T>.own(t, v)}
+            \end{vfHeap}|*/
+            close <Box<T>>.own(t, b);
+            close <Box<T>>.full_borrow_content(t, l)();
+        }{
+            close ctx();
+            /*|\begin{vfHeap}
+            ..., \vfResAdd{*p |-> v}, \vfResAdd{<T>.own(t, v)}
+            \end{vfHeap}|*/
+            close_full_borrow_content::<T>(t, p);
+            /*|\begin{vfHeap}
+            ..., \vfResRm{*p |-> v}, \vfResRm{<T>.own(t, v)}, \vfResAdd{<T>.fbc(t, p)()}
+            \end{vfHeap}|*/
+            close Box_ptr_field::<T>(l, p)();
+            close sep(Box_ptr_field(l, p), <T>.full_borrow_content(t, p))();
+            close_full_borrow_strong_m_();
+        }
     }
-    }//ctx
-    full_borrow_mono(klong, k, sep(field_ptr_chunk(l, p), (<T>.full_borrow_content)(t, p)));
-    full_borrow_split_m(k, field_ptr_chunk(l, p), (<T>.full_borrow_content)(t, p));
-    full_borrow_into_frac_m(k, field_ptr_chunk(l, p));
-    share_full_borrow_m::<T>(k, t, p);
+    full_borrow_split_m(k, Box_ptr_field(l, p), <T>.full_borrow_content(t, p));
+    /*|\begin{vfHeap}
+    ..., type_interp::<T>(), \vfResAdd{[q]lft(k)},
+    \vfResAdd{fbor(k, Box_ptr_field(l, p))}, \vfResAdd{fbor(k, <T>.fbc(t, p))}
+    \end{vfHeap}|*/
+    full_borrow_into_frac_m(k, Box_ptr_field(l, p));
+    /*|\begin{vfHeap}
+    ..., type_interp::<T>(), [q]lft(k), fbor(k, <T>.fbc(t, p)),
+    \vfResRm{fbor(k, Box_ptr_field(l, p))}, \vfResAdd{[_]fcbor(k, Box_ptr_field(l, p))}
+    \end{vfHeap}|*/
+    share_full_borrow_m::<T>(k, t, p); //| Requires and ensures \verb|type_interp::<T>() &*& [q]lft(k)|
+    /*|\begin{vfHeap}
+    ..., \vfResAssert{type_interp::<T>()}, \vfResAssert{[q]lft(k)}, [_]fcbor(k, Box_ptr_field(l, p)),
+    \vfResRm{fbor(k, <T>.fbc(t, p))}, \vfResAdd{[_](<T>.share(k, t, p))}
+    \end{vfHeap}|*/
     leak exists(p);
     close <Box<T>>.share(k, t, l);
     leak <Box<T>>.share(k, t, l);
@@ -85,6 +122,14 @@ impl<T> Box<T> {
             r
         }
     }
+
+    pub fn set(& mut self, new: T) {
+        unsafe {
+            //@ open <Box<T>>.own(_t, ?b);
+            *self.ptr = new;
+            //@ close <Box<T>>.own(_t, b);
+        }
+    }
 }
 
 impl<T> Drop for Box<T> {
@@ -104,16 +149,16 @@ impl<T> Drop for Box<T> {
 impl<T> std::ops::Deref for Box<T> {
     type Target = T;
     fn deref<'a>(&'a self) -> &'a T
-    //@ req thread_token(?_t) &*& [?_q_a]lifetime_token('a) &*& [_](<Box<T>>.share)('a, _t, self);
+//@ req thread_token(?_t) &*& [?_q_a]lifetime_token('a) &*& [_](<Box<T>>.share)('a, _t, self);
     //@ ens thread_token(_t) &*& [_q_a]lifetime_token('a) &*& [_](<T>.share('a, _t, result));
     {
         //@ open <Box<T>>.share('a, _t, self);
         //@ assert [_]exists(?p);
-        //@ open_frac_borrow('a, field_ptr_chunk(self, p), _q_a);
-        //@ open [?qp]field_ptr_chunk::<T>(self, p)();
+        //@ open_frac_borrow('a, Box_ptr_field(self, p), _q_a);
+        //@ open [?qp]Box_ptr_field::<T>(self, p)();
         let r = unsafe { &*self.ptr };
-        //@ close [qp]field_ptr_chunk::<T>(self, p)();
-        //@ close_frac_borrow(qp, field_ptr_chunk(self, p));
+        //@ close [qp]Box_ptr_field::<T>(self, p)();
+        //@ close_frac_borrow(qp, Box_ptr_field(self, p));
         r
     }
 }
